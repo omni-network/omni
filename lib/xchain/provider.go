3725cce59b7bb87a51254a23ec121aaa520c6e21
corver <corver@omni.network>
2024-01-08 11:47:02 +0200

    refactor(lib/xchain): rename xtypes to xchain (#39)
    Add the `Provider` interface to the cross chain types package. Also
    rename the package from `xtypes` to `xchain` since that is more specific
    and conveys the "cross chain" concept more clearly and explicitly.
    
    task: none

---
+++ lib/xchain/provider.go
@@ -0,0 +1,20 @@
+package xchain
+
+import "context"
+
+// ProviderCallback is the callback function signature that will be called with every finalized.
+type ProviderCallback func(context.Context, Block) error
+
+// Provider abstracts connecting to any supported source chain and streaming finalized
+// XBlocks from a specific height. It provides exactly once-delivery guarantees for the callback function.
+// It will exponentially backoff and retry forever while the callback function returns an error.
+type Provider interface {
+	// Subscribe registers a callback function that will be called with every finalized
+	// XBlock (as they become available) on the source chain from the provided height (inclusive).
+	Subscribe(
+		ctx context.Context,
+		chainID uint64,
+		fromHeight uint64,
+		callback ProviderCallback,
+	)
+}
