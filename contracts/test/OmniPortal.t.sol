cd335d0b9d444e25335ae0360a06792271cab64b
Kevin Halliday <halliday.kevin7@gmail.com>
2024-01-08 17:50:38 -0500

    Change license to GPL-3.0-or-later

--- contracts/test/OmniPortal.t.sol
+++ contracts/test/OmniPortal.t.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: No License (None)
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.23;
 
 import {OmniPortal} from "src/OmniPortal.sol";
--- contracts/test/common/CommonTest.sol
+++ contracts/test/common/CommonTest.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: No License (None)
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.23;
 
 import {Test} from "forge-std/Test.sol";
