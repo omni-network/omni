46ecb00858a72e7b37f31bc3656e24de2208dfcb
Kevin Halliday <halliday.kevin7@gmail.com>
2024-01-08 12:21:59 -0500

    Add OmniPortal xcall tests, introduce test structure

---
+++ contracts/test/common/CommonTest.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: No License (None)
+pragma solidity 0.8.23;
+
+import {Test} from "forge-std/Test.sol";
+import {OmniPortal} from "src/OmniPortal.sol";
+import {Events} from "test/common/Events.sol";
+
+/**
+ * @title CommonTest
+ * @dev An extension of forge Test that includes Omni specifc setup, utils, and events.
+ */
+contract CommonTest is Test, Events {
+    uint64 constant sourceChainId = 1;
+
+    address deployer;
+    address xcaller;
+
+    OmniPortal portal;
+
+    function setUp() public {
+        deployer = makeAddr("deployer");
+        xcaller = makeAddr("xcaller");
+
+        vm.prank(deployer);
+        portal = new OmniPortal(sourceChainId);
+    }
+}
---
+++ contracts/test/common/Events.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: No License (None)
+pragma solidity 0.8.23;
+
+/**
+ * @title Events
+ * @dev Contains all events that are tested against. Required until solc allows
+ *      for referencing events defined in other contracts.
+ */
+contract Events {
+    event XMsg(
+        uint64 indexed destChainId, uint64 indexed streamOffset, address sender, address to, bytes data, uint64 gasLimit
+    );
+}
