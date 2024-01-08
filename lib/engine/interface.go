9d66c103d4e9d9e4df21803353e4eeeb986d0957
corver <corver@omni.network>
2024-01-08 16:18:46 +0200

    feat(lib/engine): implement engine rpc client

---
+++ lib/engine/interface.go
@@ -0,0 +1,33 @@
+package engine
+
+import (
+	"context"
+
+	"github.com/ethereum/go-ethereum/beacon/engine"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+// API defines the execution chain' Engine API.
+type API interface {
+	// NewPayloadV2 creates an Eth1 block, inserts it in the chain, and returns the status of the chain.
+	NewPayloadV2(ctx context.Context, params engine.ExecutableData) (engine.PayloadStatusV1, error)
+	// NewPayloadV3 creates an Eth1 block, inserts it in the chain, and returns the status of the chain.
+	NewPayloadV3(ctx context.Context, params engine.ExecutableData, versionedHashes []common.Hash,
+		beaconRoot *common.Hash) (engine.PayloadStatusV1, error)
+
+	// ForkchoiceUpdatedV2 has several responsibilities:
+	//  - It sets the chain the head.
+	//  - And/or it sets the chain's finalized block hash.
+	//  - And/or it starts assembling (async) a block with the payload attributes.
+	ForkchoiceUpdatedV2(ctx context.Context, update engine.ForkchoiceStateV1,
+		payloadAttributes *engine.PayloadAttributes) (engine.ForkChoiceResponse, error)
+
+	// ForkchoiceUpdatedV3 is equivalent to V2 with the addition of parent beacon block root in the payload attributes.
+	ForkchoiceUpdatedV3(ctx context.Context, update engine.ForkchoiceStateV1,
+		payloadAttributes *engine.PayloadAttributes) (engine.ForkChoiceResponse, error)
+
+	// GetPayloadV2 returns a cached payload by id.
+	GetPayloadV2(ctx context.Context, payloadID engine.PayloadID) (*engine.ExecutionPayloadEnvelope, error)
+	// GetPayloadV3 returns a cached payload by id.
+	GetPayloadV3(ctx context.Context, payloadID engine.PayloadID) (*engine.ExecutionPayloadEnvelope, error)
+}
---
+++ lib/engine/methods.go
@@ -0,0 +1,18 @@
+package engine
+
+const (
+	// newPayloadV1 = "engine_newPayloadV1".
+	newPayloadV2 = "engine_newPayloadV2"
+	newPayloadV3 = "engine_newPayloadV3"
+
+	// forkchoiceUpdatedV1 = "engine_forkchoiceUpdatedV1".
+	forkchoiceUpdatedV2 = "engine_forkchoiceUpdatedV2"
+	forkchoiceUpdatedV3 = "engine_forkchoiceUpdatedV3"
+
+	// getPayloadBodiesByHashV1  = "engine_getPayloadBodiesByHashV1"
+	// getPayloadBodiesByRangeV1 = "engine_getPayloadBodiesByRangeV1".
+
+	getPayloadV1 = "engine_getPayloadV1"
+	getPayloadV2 = "engine_getPayloadV2"
+	getPayloadV3 = "engine_getPayloadV3"
+)
