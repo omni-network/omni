d42c1b043321f465651768f7be6cd14c4e8fdefc
Kevin Halliday <halliday.kevin7@gmail.com>
2024-01-08 19:20:59 -0500

    Explicitly add foundryup to path.

--- scripts/install_foundry.sh
+++ scripts/install_foundry.sh
@@ -3,39 +3,41 @@
 
 set -e
 
+BASE_DIR=${XDG_CONFIG_HOME:-$HOME}
+
+# tells https://foundry.paradigm.xyz where to install foundryup
+# this way we can explicity add FOUNDRY_BIN_DIR to PATH
+export FOUNDRY_DIR=${FOUNDRY_DIR-"$BASE_DIR/.foundry"}
+export FOUNDRY_BIN_DIR="$FOUNDRY_DIR/bin"
+
 # foundryup installs versions by tag (https://github.com/foundry-rs/foundry/tags)
 TAG="nightly-6fc74638b797b8e109452d3df8e26758f86f31fe"
 
 # output of forge --version, to check $TAG installed
 VERSION="forge 0.2.0 (6fc7463 2024-01-05T00:17:41.668342000Z)"
 
+# If not running interactively (like in GithubActions), add FOUNDRY_BIN_DIR to PATH
+case $- in
+  *i*) ;;
+  *) [[ ":$PATH:" != *":${FOUNDRY_BIN_DIR}:"* ]] && PATH="${PATH}:${FOUNDRY_BIN_DIR}";;
+esac
+
+
+# If foundryup not installed, install it
 if ! which foundryup 1>/dev/null; then
   echo "Installing foundryup"
   curl -L https://foundry.paradigm.xyz | bash
 
-  case $SHELL in
-    */zsh)
-      PROFILE=${ZDOTDIR-"$HOME"}/.zshenv
+  # again if not running interactively, add FOUNDRY_BIN_DIR to PATH
+  case $- in
+    *i*)
       ;;
-    */bash)
-      PROFILE=$HOME/.bashrc
-      ;;
-    *)
-      echo "Unknown shell: $SHELL"
-      exit 1
-      ;;
+    *) [[ ":$PATH:" != *":${FOUNDRY_BIN_DIR}:"* ]] && PATH="${PATH}:${FOUNDRY_BIN_DIR}";;
   esac
-
-  echo "Sourcing $PROFILE"
-  source $PROFILE
-  echo "$PATH"
-  export PATH="$PATH:/home/runner/.config/.foundry/bin"
-  echo "$PATH"
-  echo "which foundryup: $(which foundryup)"
 fi
 
+
+# If correct version not installed, install it
 if ! which forge 1>/dev/null || [[ $(forge --version) != $VERSION ]]; then
-  echo "Installing $VERSION"
-  echo "which foundryup: $(which foundryup)"
   foundryup --version $TAG
 fi
