6af045db232d840c4bf54a61c599b375fe46b84a
corver <corver@omni.network>
2024-01-09 12:05:45 +0200

    feat(lib/engine): implement engine rpc client (#43)
    Implement the engine API client and interface in `lib/engine`
    
    task: none

---
+++ lib/engine/client.go
@@ -0,0 +1,111 @@
+package engine
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/omni-network/omni/lib/errors"
+
+	"github.com/ethereum/go-ethereum/beacon/engine"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/rpc"
+)
+
+const defaultRPCHTTPTimeout = time.Second * 30
+
+var _ API = Client{}
+
+// Client implements the Engine API using JSON-RPC.
+type Client struct {
+	client *rpc.Client
+}
+
+// NewClient returns a new Engine API JSON-RPC client.
+func NewClient(ctx context.Context, urlAddr string, jwtSecret []byte) (Client, error) {
+	transport := http.DefaultTransport
+	if len(jwtSecret) > 0 {
+		transport = newJWTRoundTripper(http.DefaultTransport, jwtSecret)
+	}
+
+	client := &http.Client{Timeout: defaultRPCHTTPTimeout, Transport: transport}
+
+	rpcClient, err := rpc.DialOptions(ctx, urlAddr, rpc.WithHTTPClient(client))
+	if err != nil {
+		return Client{}, errors.Wrap(err, "rpc dial")
+	}
+
+	return Client{
+		client: rpcClient,
+	}, nil
+}
+
+func (c Client) NewPayloadV2(ctx context.Context, params engine.ExecutableData) (engine.PayloadStatusV1, error) {
+	var resp engine.PayloadStatusV1
+	err := c.client.CallContext(ctx, &resp, newPayloadV2, params)
+	if err != nil {
+		return engine.PayloadStatusV1{}, errors.Wrap(err, "rpc new payload v2")
+	}
+
+	return resp, nil
+}
+
+func (c Client) NewPayloadV3(ctx context.Context, params engine.ExecutableData, versionedHashes []common.Hash,
+	beaconRoot *common.Hash,
+) (engine.PayloadStatusV1, error) {
+	var resp engine.PayloadStatusV1
+	err := c.client.CallContext(ctx, &resp, newPayloadV3, params, versionedHashes, beaconRoot)
+	if err != nil {
+		return engine.PayloadStatusV1{}, errors.Wrap(err, "rpc new payload v3")
+	}
+
+	return resp, nil
+}
+
+func (c Client) ForkchoiceUpdatedV2(ctx context.Context, update engine.ForkchoiceStateV1,
+	payloadAttributes *engine.PayloadAttributes,
+) (engine.ForkChoiceResponse, error) {
+	var resp engine.ForkChoiceResponse
+	err := c.client.CallContext(ctx, &resp, forkchoiceUpdatedV2, update, payloadAttributes)
+	if err != nil {
+		return engine.ForkChoiceResponse{}, errors.Wrap(err, "rpc forkchoice updated v2")
+	}
+
+	return resp, nil
+}
+
+func (c Client) ForkchoiceUpdatedV3(ctx context.Context, update engine.ForkchoiceStateV1,
+	payloadAttributes *engine.PayloadAttributes,
+) (engine.ForkChoiceResponse, error) {
+	var resp engine.ForkChoiceResponse
+	err := c.client.CallContext(ctx, &resp, forkchoiceUpdatedV3, update, payloadAttributes)
+	if err != nil {
+		return engine.ForkChoiceResponse{}, errors.Wrap(err, "rpc forkchoice updated v3")
+	}
+
+	return resp, nil
+}
+
+func (c Client) GetPayloadV2(ctx context.Context, payloadID engine.PayloadID) (
+	*engine.ExecutionPayloadEnvelope, error,
+) {
+	var resp engine.ExecutionPayloadEnvelope
+	err := c.client.CallContext(ctx, &resp, getPayloadV2, payloadID)
+	if err != nil {
+		return nil, errors.Wrap(err, "rpc get payload v2")
+	}
+
+	return &resp, nil
+}
+
+func (c Client) GetPayloadV3(ctx context.Context, payloadID engine.PayloadID) (
+	*engine.ExecutionPayloadEnvelope, error,
+) {
+	var resp engine.ExecutionPayloadEnvelope
+	err := c.client.CallContext(ctx, &resp, getPayloadV3, payloadID)
+	if err != nil {
+		return nil, errors.Wrap(err, "rpc get payload v3")
+	}
+
+	return &resp, nil
+}
---
+++ lib/engine/jwt.go
@@ -0,0 +1,60 @@
+package engine
+
+import (
+	"encoding/hex"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/omni-network/omni/lib/errors"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type jwtRoundTripper struct {
+	underlyingTransport http.RoundTripper
+	jwtSecret           []byte
+}
+
+func newJWTRoundTripper(transport http.RoundTripper, jwtSecret []byte) *jwtRoundTripper {
+	return &jwtRoundTripper{
+		underlyingTransport: transport,
+		jwtSecret:           jwtSecret,
+	}
+}
+
+func (t *jwtRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"iat": time.Now().Unix(),
+	})
+
+	tokenString, err := token.SignedString(t.jwtSecret)
+	if err != nil {
+		return nil, errors.Wrap(err, "jwt token string")
+	}
+
+	req.Header.Set("Authorization", "Bearer "+tokenString)
+
+	resp, err := t.underlyingTransport.RoundTrip(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "round trip")
+	}
+
+	return resp, nil
+}
+
+// LoadJWTHexFile loads a hex encoded JWT secret from the provided file.
+func LoadJWTHexFile(file string) ([]byte, error) {
+	jwtHex, err := os.ReadFile(file)
+	if err != nil {
+		return nil, errors.Wrap(err, "read jwt file")
+	}
+
+	jwtBytes, err := hex.DecodeString(strings.TrimPrefix(string(jwtHex), "0x"))
+	if err != nil {
+		return nil, errors.Wrap(err, "decode jwt file")
+	}
+
+	return jwtBytes, nil
+}
