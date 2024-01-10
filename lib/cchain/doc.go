ee0cac35aa43088adde9d56216feda540a94015c
corver <corver@omni.network>
2024-01-10 11:40:31 +0200

    feat(lib/capi): define cprovider interface (#51)
    Defines the `CProvider` that abstracts streaming of approved
    attestations from halo. This will initially be used by the relayer.
    
    The package name `cchain` aligns with the `xchain` name.
    
    task: none

---
+++ lib/cchain/doc.go
@@ -0,0 +1,2 @@
+// Package cchain defines the API to interact with the omni consensus chain.
+package cchain
---
+++ lib/cchain/provider.go
@@ -0,0 +1,23 @@
+package cchain
+
+import (
+	"context"
+
+	"github.com/omni-network/omni/lib/xchain"
+)
+
+// ProviderCallback is the callback function signature that will be called with all approved attestation per
+// source chain block.
+type ProviderCallback func(ctx context.Context, height uint64, approved []xchain.AggAttestation) error
+
+// Provider abstracts connecting to the omni consensus chain and streaming approved
+// aggregate attestations for each source chain block from a specific height.
+//
+// It provides exactly once-delivery guarantees for the callback function.
+// It will exponentially backoff and retry forever while the callback function returns an error.
+type Provider interface {
+	// Subscribe registers a callback function that will be called with all approved aggregate
+	// attestations (as they become available per source chain block) on the consensus chain from
+	// the provided source chain ID and height (inclusive).
+	Subscribe(ctx context.Context, sourceChainID uint64, sourceHeight uint64, callback ProviderCallback)
+}
