411f6e67f02d74d5c2ad1f2a6474f2fbb83fb01e
corver <corver@omni.network>
2024-01-10 09:20:59 +0200

    cleanup

--- lib/cchain/provider.go
+++ lib/cchain/provider.go
@@ -10,13 +10,13 @@ import (
 // consensus block.
 type ProviderCallback func(ctx context.Context, height uint64, approved []xchain.AggAttestation) error
 
-// Provider abstracts connecting to the omni consensus chainand streaming approved
-// aggregate attestations from a specific height.
+// Provider abstracts connecting to the omni consensus chain and streaming approved
+// aggregate attestations for each block from a specific height.
 //
 // It provides exactly once-delivery guarantees for the callback function.
 // It will exponentially backoff and retry forever while the callback function returns an error.
 type Provider interface {
 	// Subscribe registers a callback function that will be called with all approved aggregate
-	// attestations (as they become available) on the consensus chain from the provided height (inclusive).
+	// attestations (as they become available per block) on the consensus chain from the provided height (inclusive).
 	Subscribe(ctx context.Context, height uint64, callback ProviderCallback)
 }
