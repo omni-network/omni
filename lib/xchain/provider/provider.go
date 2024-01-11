3f09ff39d6391b89b0a1f6c6ae64c0ab019d0c21
Zahoor Mohamed <zahoor@omni.network>
2024-01-11 17:46:52 +0530

    feat(xchain/provider): add initial wrapper for the provider and streamer (#55)
    This PR implements the Xprovider
    - Provider constructor with chainConfig
    - Provider subscription
    - Streamer which triggers a rpcClient and implements a producer consumer
    of blocks
    - Need to implement actual construction of XBlocks from event logs
    (`checkForBlockInBatch()`).
    
    task: https://app.asana.com/0/1206208509925075/1206208496796894/f

---
+++ lib/xchain/provider/provider.go
@@ -0,0 +1,89 @@
+// Package provider is the implementation of the Provider interface.
+// it abstracts connecting to different rollup chains and collecting
+// XMsgs and XReceipts to construct XBlock and deliver them to the
+// calling module.
+package provider
+
+import (
+	"context"
+
+	"github.com/omni-network/omni/lib/errors"
+	"github.com/omni-network/omni/lib/ethclient"
+	"github.com/omni-network/omni/lib/log"
+	"github.com/omni-network/omni/lib/xchain"
+)
+
+// ChainConfig is the configuration parameters for all the chains
+// that needs to be managed by the provider.
+type ChainConfig struct {
+	name      string           // name of the rollup chain
+	id        uint64           // network id of the chain
+	minHeight uint64           // minimum configured height from which blocks should be fetched
+	rpcClient ethclient.Client // the rpc client to get the information from the chain
+}
+
+// Provider stores the source chain configuration and the global quit channel.
+type Provider struct {
+	config []*ChainConfig // store config for every chain ID
+	quitC  chan struct{}  // to stop all operations of the provider
+}
+
+// New instantiates the provider instance which will be ready to accept
+// subscriptions for respective destination XBlocks.
+func New(chains []*ChainConfig) *Provider {
+	return &Provider{
+		config: chains,
+		quitC:  make(chan struct{}),
+	}
+}
+
+// Subscribe to the XBlock from a given destination chain.
+func (p *Provider) Subscribe(
+	ctx context.Context,
+	chainID uint64,
+	fromHeight uint64,
+	callback xchain.ProviderCallback,
+) error {
+	log.Info(ctx, "Subscribing to provider ", "id", chainID, "fromHeight", fromHeight)
+
+	// retrieve the respective config
+	config, err := p.getConfig(chainID)
+	if err != nil {
+		return err
+	}
+
+	// Start streaming from chain's minimum height as per config.
+	if fromHeight < config.minHeight {
+		fromHeight = config.minHeight
+	}
+
+	// run the XBlock stream for this chain
+	go p.runStreamer(ctx, config, fromHeight, callback)
+
+	return nil
+}
+
+// startStreamer creates a new XBlock streamer for the given chain and kicks tarts its operation.
+func (p *Provider) runStreamer(ctx context.Context,
+	config *ChainConfig,
+	minHeight uint64,
+	callback xchain.ProviderCallback,
+) {
+	// instantiate a new streamer for this chain
+	streamer := NewStreamer(config, minHeight, callback, p.quitC)
+
+	// start the streaming process
+	streamer.streamBlocks(ctx, minHeight)
+}
+
+// getConfig provides the configuration of the given chainID.
+func (p *Provider) getConfig(chainID uint64) (*ChainConfig, error) {
+	// check if the config for this chain ID is present
+	for _, config := range p.config {
+		if config.id == chainID {
+			return config, nil
+		}
+	}
+
+	return nil, errors.New("config for chain id is not found")
+}
---
+++ lib/xchain/provider/streamer.go
@@ -0,0 +1,117 @@
+package provider
+
+import (
+	"context"
+	"sync/atomic"
+	"time"
+
+	"github.com/omni-network/omni/lib/log"
+	"github.com/omni-network/omni/lib/xchain"
+)
+
+const (
+	BlockFetchInterval = 1 * time.Second // time interval between each block fetch
+)
+
+// Streamer maintains the config and the destination for each chain.
+type Streamer struct {
+	chainConfig *ChainConfig            // the chain config which also has the subscription information
+	minHeight   uint64                  // the minimum height to start the getting the xchain messages
+	callback    xchain.ProviderCallback // the callback to call on receiving a xblock
+	quitC       chan struct{}           // the quit channel to stop all the streamer operations
+}
+
+// NewStreamer manages the rpc client to collect xblocks and delivers it to the
+// subscriber through callback.
+func NewStreamer(config *ChainConfig,
+	minHeight uint64,
+	callback xchain.ProviderCallback,
+	quitC chan struct{},
+) *Streamer {
+	// initialize the streamer structure with the received configuration
+	stream := &Streamer{
+		chainConfig: config,
+		minHeight:   minHeight,
+		callback:    callback,
+		quitC:       quitC,
+	}
+
+	return stream
+}
+
+func (s *Streamer) streamBlocks(ctx context.Context, currentHeight uint64) {
+	// produce blocks on every BlockFetchInterval
+	ticker := time.NewTicker(BlockFetchInterval)
+	defer ticker.Stop()
+
+	var locker uint32 // variable to take channel backpressure
+	for {
+		select {
+		case <-ctx.Done():
+			log.Info(ctx, "Stopping to produce blocks",
+				"chainName", s.chainConfig.name,
+				"chainID", s.chainConfig.id,
+				"height", currentHeight)
+
+			return
+
+		case <-s.quitC:
+			log.Info(ctx, "Stopping to produce blocks",
+				"chainName", s.chainConfig.name,
+				"chainID", s.chainConfig.id,
+				"height", currentHeight)
+
+			return
+
+		case <-ticker.C:
+			// if the previous xblocks are not consumed yet, then skip this interval
+			if !atomic.CompareAndSwapUint32(&locker, 0, 1) {
+				continue
+			}
+
+			// get the message and receipts from the chain for this block if any
+			xBlock, err := s.chainConfig.rpcClient.GetBlock(ctx, currentHeight)
+			if err != nil {
+				log.Error(ctx, "Could not get cross chain block from rpc client", err,
+					"chainName", s.chainConfig.name,
+					"chainID", s.chainConfig.id,
+					"height", currentHeight)
+
+				continue
+			}
+
+			// ignore of there is no messages in this block
+			if xBlock == nil {
+				log.Info(ctx, "No cross chain block in this height",
+					"chainName", s.chainConfig.name,
+					"chainID", s.chainConfig.id,
+					"height", currentHeight)
+
+				continue
+			}
+
+			// deliver the block
+			callbackErr := s.callback(ctx, xBlock)
+			if callbackErr != nil {
+				log.Error(ctx, "Error while delivering xblock", callbackErr,
+					"chainName", s.chainConfig.name,
+					"chainID", s.chainConfig.id,
+					"blockHeight", currentHeight)
+
+				continue
+			}
+			log.Info(ctx, "Delivered xBlock",
+				"sourceChainID", xBlock.SourceChainID,
+				"blockHeight", xBlock.BlockHeight,
+				"blockHash", xBlock.BlockHash,
+				"noOfMsgs", len(xBlock.Msgs),
+				"noOfReceipts", len(xBlock.Receipts))
+
+			// move to the next block
+			currentHeight++
+
+			// release the lock to accept new xblocks
+			atomic.StoreUint32(&locker, 0)
+		}
+	}
+}
