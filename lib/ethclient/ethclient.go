2a53a4e80c3e8d9aca86a3a8e246dd6a01307488
Zahoor Mohamed Jawad <zahoor@omni.network>
2024-01-13 19:45:51 +0400

    bell and whistles fix
    Signed-off-by: Zahoor Mohamed Jawad <zahoor@omni.network>

--- lib/ethclient/ethclient.go
+++ lib/ethclient/ethclient.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/omni-network/omni/contracts/bindings"
 	"github.com/omni-network/omni/lib/errors"
-	"github.com/omni-network/omni/lib/log"
 	"github.com/omni-network/omni/lib/xchain"
 
 	"github.com/ethereum/go-ethereum"
@@ -18,10 +17,13 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
+const (
+	xMsgSigString = "XMsg(uint64,uint64,address,address,bytes,uint64)"
+)
+
 // EthClient is the configuration for the rpc client to connect and get information.
 type EthClient struct {
 	chainID       uint64
-	rpcURL        string
 	portalAddress common.Address
 	contractABI   abi.ABI
 	xMsgSigHash   common.Hash
@@ -30,18 +32,11 @@ type EthClient struct {
 
 // NewEthClient is the client implementation of the json rpc interface to the rollup chain.
 func NewEthClient(
-	ctx context.Context,
 	chainID uint64,
-	rpcURL string,
 	portalAddress common.Address,
+	rpcClient *ethclient.Client,
 ) (*EthClient, error) {
 	// TODO(jmozah): validate chainID , portalAddress etc
-
-	// connect to rpc if not connected
-	eClient, err := connect(ctx, chainID, rpcURL)
-	if err != nil {
-		return nil, err
-	}
 
 	// construct the omni portal contract ABI from the bindings
 	contractAbi, err := abi.JSON(strings.NewReader(bindings.OmniPortalMetaData.ABI))
@@ -50,15 +45,14 @@ func NewEthClient(
 	}
 
 	// create the method signatures
-	xMsgSig := []byte("XMsg(uint64,uint64,address,address,bytes,uint64)")
+	xMsgSig := []byte(xMsgSigString)
 
 	return &EthClient{
 		chainID:       chainID,
-		rpcURL:        rpcURL,
 		portalAddress: portalAddress,
 		contractABI:   contractAbi,
 		xMsgSigHash:   crypto.Keccak256Hash(xMsgSig),
-		rpcClient:     eClient,
+		rpcClient:     rpcClient,
 	}, nil
 }
 
@@ -83,31 +77,32 @@ func (e *EthClient) GetBlock(ctx context.Context, height uint64) (xchain.Block, 
 	}
 
 	// select the logs based on the required event signature
-	selectedLogs := make([]types.Log, 0)
+	// TODO(jmozah): extract receipts logs too
+	selectedMsgLogs := make([]types.Log, 0)
 	for _, vLog := range logs {
 		switch vLog.Topics[0].Hex() {
 		case e.xMsgSigHash.Hex():
-			selectedLogs = append(selectedLogs, vLog)
+			selectedMsgLogs = append(selectedMsgLogs, vLog)
 		default:
 		}
 	}
 
 	// construct an xblock only if some cross chain events are found
-	if len(selectedLogs) > 0 {
-		xBlock = e.constructXBlocks(selectedLogs)
+	if len(selectedMsgLogs) > 0 {
+		xBlock = e.constructXBlock(selectedMsgLogs)
 	}
 
 	return xBlock, true, nil
 }
 
 // constructXBlocks assembles the xBlock using the XMsgs and XReceipts found in the block height.
-func (e *EthClient) constructXBlocks(selectedLogs []types.Log) xchain.Block {
+func (e *EthClient) constructXBlock(selectedMsgLogs []types.Log) xchain.Block {
 	var header xchain.BlockHeader
 	messages := make([]xchain.Msg, 0)
 	var block xchain.Block
 
 	// construct the block based on cross chain message or receipts that are found
-	for _, vLog := range selectedLogs {
+	for _, vLog := range selectedMsgLogs {
 		// create the BlockHeader once
 		if header.SourceChainID != e.chainID {
 			header = xchain.BlockHeader{
@@ -144,17 +139,4 @@ func (e *EthClient) constructXBlocks(selectedLogs []types.Log) xchain.Block {
 	}
 
 	return block
-}
-
-// connect initiates a connection if it is not already dialed in.
-func connect(ctx context.Context, chainID uint64, rpcURL string) (*ethclient.Client, error) {
-	eClient, err := ethclient.Dial(rpcURL)
-	if err != nil {
-		return nil, errors.Wrap(err, "could not connect to chain")
-	}
-	log.Info(ctx, "Connected to chain. ",
-		"chainId", chainID,
-		"rpcURL", rpcURL)
-
-	return eClient, nil
 }
