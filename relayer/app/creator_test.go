22af12823107a61603cd9a1161599abae4d54a3b
lazar955 <lukic.lazar@yahoo.com>
2024-01-22 12:45:31 +0000

    tests grouping by chain id for multiple subs

--- relayer/app/creator_test.go
+++ relayer/app/creator_test.go
@@ -1,8 +1,11 @@
 package relayer_test
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 
+	"github.com/cometbft/cometbft/crypto"
 	"github.com/omni-network/omni/halo/attest"
 	"github.com/omni-network/omni/lib/xchain"
 	relayer "github.com/omni-network/omni/relayer/app"
@@ -24,8 +27,19 @@ func TestCreatorService_CreateSubmissions(t *testing.T) {
 
 	privKey := k1.GenPrivKey()
 
+	fuzzer := fuzz.New().NilChance(0).Funcs(
+		func(e *xchain.Msg, c fuzz.Continue) {
+			e.DestChainID = uint64(randomBetween(1, 5))
+			e.SourceChainID = uint64(randomBetween(1, 5))
+			e.StreamOffset = uint64(randomBetween(1, 10))
+			e.DestAddress = [20]byte(crypto.CRandBytes(20))
+			e.SourceMsgSender = [20]byte(crypto.CRandBytes(20))
+			e.Data = crypto.CRandBytes(100)
+		},
+	)
+
 	var block xchain.Block
-	fuzz.New().NilChance(0).NumElements(1, 64).Fuzz(&block)
+	fuzzer.NilChance(0).NumElements(1, 64).Fuzz(&block)
 
 	att, err := attest.CreateAttestation(privKey, block)
 	require.NoError(t, err)
@@ -64,7 +78,15 @@ func TestCreatorService_CreateSubmissions(t *testing.T) {
 			for _, g := range got {
 				require.NotNil(t, g.AttestationRoot)
 				require.Equal(t, g.AttestationRoot, att.BlockRoot)
+				require.NotNil(t, g.Proof)
+				require.NotNil(t, g.ProofFlags)
+				require.NotNil(t, g.Signatures)
 			}
 		})
 	}
+}
+
+func randomBetween(min, max int) int {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+	return rand.Intn(max-min+1) + min
 }
