dfca2719021bc965a3c8bacd8fd01afba84a59ec
Kevin Halliday <halliday.kevin7@gmail.com>
2024-01-22 13:58:12 -0500

    test(contracts): refactor test XMsgs (#141)
    Refactor test XMsg factories
    
    - use XMsg struct in tests
    - introduce test Counter contract
    - move test XMsg factories to CommonTest
    
    
    task: none

--- contracts/test/OmniPortal.t.sol
+++ contracts/test/OmniPortal.t.sol
@@ -1,64 +1,58 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.23;
 
-import { OmniPortal } from "src/OmniPortal.sol";
 import { CommonTest } from "test/common/CommonTest.sol";
+import { XChain } from "src/libraries/XChain.sol";
 
 contract OmniPortal_Test is CommonTest {
     /// @dev Test that xcall with default gas limit emits XMsg event and increments outXStreamOffset
     function test_xcall_defaultGasLimit_succeeds() public {
-        (uint64 destChainId, uint64 offset, address to, bytes memory data) = _xfoo();
-        uint64 gasLimit = portal.XMSG_DEFAULT_GAS_LIMIT();
+        XChain.Msg memory xmsg = _outbound_increment();
 
         // check XMsg event is emitted
         vm.expectEmit();
-        emit XMsg(destChainId, offset, xcaller, to, data, gasLimit);
+        emit XMsg(xmsg.destChainId, xmsg.streamOffset, xcaller, xmsg.to, xmsg.data, xmsg.gasLimit);
 
         // make xcall
         vm.prank(xcaller);
-        portal.xcall(destChainId, to, data);
+        portal.xcall(xmsg.destChainId, xmsg.to, xmsg.data);
 
         // check outXStreamOffset is incremented
-        assertEq(portal.outXStreamOffset(destChainId), offset + 1);
+        assertEq(portal.outXStreamOffset(xmsg.destChainId), xmsg.streamOffset + 1);
     }
 
     /// @dev Test that xcall with explicit gas limit emits XMsg event and increments outXStreamOffset
     function test_xcall_explicitGasLimit_succeeds() public {
-        (uint64 destChainId, uint64 offset, address to, bytes memory data) = _xfoo();
-        uint64 gasLimit = portal.XMSG_DEFAULT_GAS_LIMIT() + 1;
+        XChain.Msg memory xmsg = _outbound_increment();
+        xmsg.gasLimit = portal.XMSG_DEFAULT_GAS_LIMIT() + 1;
 
         // check XMsg event is emitted
         vm.expectEmit();
-        emit XMsg(destChainId, offset, xcaller, to, data, gasLimit);
+        emit XMsg(xmsg.destChainId, xmsg.streamOffset, xcaller, xmsg.to, xmsg.data, xmsg.gasLimit);
 
         // make xcall
         vm.prank(xcaller);
-        portal.xcall(destChainId, to, data, gasLimit);
+        portal.xcall(xmsg.destChainId, xmsg.to, xmsg.data, xmsg.gasLimit);
 
         // check outXStreamOffset is incremented
-        assertEq(portal.outXStreamOffset(destChainId), offset + 1);
+        assertEq(portal.outXStreamOffset(xmsg.destChainId), xmsg.streamOffset + 1);
     }
 
     /// @dev Test that xcall with too-low gas limit reverts
     function test_xcall_gasLimitTooLow_reverts() public {
-        (uint64 destChainId,, address to, bytes memory data) = _xfoo();
-        uint64 gasLimit = portal.XMSG_MIN_GAS_LIMIT() - 1;
+        XChain.Msg memory xmsg = _outbound_increment();
+        xmsg.gasLimit = portal.XMSG_MIN_GAS_LIMIT() - 1;
 
-        vm.expectRevert();
-        portal.xcall(destChainId, to, data, gasLimit);
+        vm.expectRevert("OmniPortal: gasLimit too low");
+        portal.xcall(xmsg.destChainId, xmsg.to, xmsg.data, xmsg.gasLimit);
     }
 
     /// @dev Test that xcall with too-high gas limit reverts
     function test_xcall_gasLimitTooHigh_reverts() public {
-        (uint64 destChainId,, address to, bytes memory data) = _xfoo();
-        uint64 gasLimit = portal.XMSG_MAX_GAS_LIMIT() + 1;
+        XChain.Msg memory xmsg = _outbound_increment();
+        xmsg.gasLimit = portal.XMSG_MAX_GAS_LIMIT() + 1;
 
-        vm.expectRevert();
-        portal.xcall(destChainId, to, data, gasLimit);
-    }
-
-    /// @dev Get test foo() xcall params
-    function _xfoo() private returns (uint64 destChainId, uint64 offset, address to, bytes memory data) {
-        return (2, portal.outXStreamOffset(2), makeAddr("foo-addr-on-dest"), abi.encodeWithSignature("foo()"));
+        vm.expectRevert("OmniPortal: gasLimit too high");
+        portal.xcall(xmsg.destChainId, xmsg.to, xmsg.data, xmsg.gasLimit);
     }
 }
--- contracts/test/common/CommonTest.sol
+++ contracts/test/common/CommonTest.sol
@@ -3,23 +3,48 @@ pragma solidity 0.8.23;
 
 import { Test } from "forge-std/Test.sol";
 import { OmniPortal } from "src/OmniPortal.sol";
+import { Counter } from "test/common/Counter.sol";
 import { Events } from "test/common/Events.sol";
+import { XChain } from "src/libraries/XChain.sol";
 
 /**
  * @title CommonTest
  * @dev An extension of forge Test that includes Omni specifc setup, utils, and events.
  */
 contract CommonTest is Test, Events {
+    // some other chain id to use for source or dest of xmsgs
+    uint64 constant otherChainId = 2;
+
     address deployer;
     address xcaller;
 
     OmniPortal portal;
+    Counter counter;
 
     function setUp() public {
         deployer = makeAddr("deployer");
         xcaller = makeAddr("xcaller");
 
-        vm.prank(deployer);
+        vm.startPrank(deployer);
         portal = new OmniPortal();
+        counter = new Counter();
+        vm.stopPrank();
+    }
+
+    /**
+     * Test XMsgs
+     */
+
+    /// @dev Get XMsg fields for an outbound Counter.increment() xcall
+    function _outbound_increment() internal view returns (XChain.Msg memory) {
+        return XChain.Msg({
+            sourceChainId: portal.chainId(),
+            destChainId: otherChainId,
+            streamOffset: portal.outXStreamOffset(otherChainId),
+            sender: address(counter),
+            to: address(counter),
+            data: abi.encodeWithSignature("increment()"),
+            gasLimit: portal.XMSG_DEFAULT_GAS_LIMIT()
+        });
     }
 }
