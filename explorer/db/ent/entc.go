3cccf5fa72ce2eb06e2ad8ff2c11f9809448cf93
Flannel007 <dan.flanagan93@gmail.com>
2024-01-23 22:15:55 -0600

    entc

---
+++ explorer/db/ent/entc.go
@@ -0,0 +1,19 @@
+//go:build ignore
+
+package main
+
+import (
+	"log"
+
+	"entgo.io/ent/entc"
+	"entgo.io/ent/entc/gen"
+)
+
+func main() {
+	err := entc.Generate("./schema", &gen.Config{
+		Features: []gen.Feature{gen.FeatureVersionedMigration},
+	})
+	if err != nil {
+		log.Fatalf("running ent codegen: %v", err)
+	}
+}
