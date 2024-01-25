bf475a6577a7fa75374d7b691ad9b75a4e006395
Kevin Halliday <kevin@omni.network>
2024-01-25 18:53:45 -0500

    inherit from forge contracts, add comments explaning chains

--- contracts/test/common/Fixtures.sol
+++ contracts/test/common/Fixtures.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.23;
 
-import { Vm } from "forge-std/Vm.sol";
+import { CommonBase } from "forge-std/Base.sol";
+import { StdCheats } from "forge-std/StdCheats.sol";
 import { XTypes } from "src/libraries/XTypes.sol";
 import { TestXTypes } from "./TestXTypes.sol";
 import { TestPortal } from "./TestPortal.sol";
@@ -13,8 +14,15 @@ import { Reverter } from "./Reverter.sol";
  * @dev Defines test dependencies. Deploys portals and test contracts, and provides
  *      utilites for generating XMsgs and XSubmissions between them.
  */
-contract Fixtures {
-    Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+contract Fixtures is CommonBase, StdCheats {
+    // We introduce three "chains", this chain, chain a, and chain b. We deploy
+    // portals and contracts for each chain. Obviously, they all live in the
+    // test EVM state. But it's useful to semantically group contracts by
+    // chain, so that we may describe and test XMsgs between them.
+    //
+    // All contracts on "this" chain, do not have a prefix (portal, counter, reverter).
+    // That way tests can refer to them without introducing the idea of a "chain". Most
+    // test cases need only think about "this" chain.
 
     uint64 constant thisChainId = 1;
     uint64 constant chainAId = 2;
@@ -22,7 +30,7 @@ contract Fixtures {
 
     address deployer;
     address xcaller;
-    address xrelayer;
+    address relayer;
 
     TestPortal portal;
     TestPortal chainAPortal;
@@ -36,13 +44,14 @@ contract Fixtures {
     Reverter chainAReverter;
     Reverter chainBReverter;
 
-    mapping(uint64 => address) public reverters;
-    mapping(uint64 => address) public counters;
+    // helper mappings to generate XMsg.to and XMsg.sender for some sourceChainId & destChainId
+    mapping(uint64 => address) private _reverters;
+    mapping(uint64 => address) private _counters;
 
     function setUp() public {
-        deployer = _makeAddr("deployer");
-        xcaller = _makeAddr("xcaller");
-        xrelayer = _makeAddr("relayer");
+        deployer = makeAddr("deployer");
+        xcaller = makeAddr("xcaller");
+        relayer = makeAddr("relayer");
 
         vm.startPrank(deployer);
 
@@ -63,13 +72,13 @@ contract Fixtures {
 
         vm.stopPrank();
 
-        counters[thisChainId] = address(counter);
-        counters[chainAId] = address(chainACounter);
-        counters[chainBId] = address(chainBCounter);
+        _counters[thisChainId] = address(counter);
+        _counters[chainAId] = address(chainACounter);
+        _counters[chainBId] = address(chainBCounter);
 
-        reverters[thisChainId] = address(reverter);
-        reverters[chainAId] = address(chainAReverter);
-        reverters[chainBId] = address(chainBReverter);
+        _reverters[thisChainId] = address(reverter);
+        _reverters[chainAId] = address(chainAReverter);
+        _reverters[chainBId] = address(chainBReverter);
     }
 
     /// @dev Create an test XSubmission
@@ -109,8 +118,8 @@ contract Fixtures {
             sourceChainId: sourceChainId,
             destChainId: destChainId,
             streamOffset: streamOffset,
-            sender: counters[sourceChainId],
-            to: counters[destChainId],
+            sender: _counters[sourceChainId],
+            to: _counters[destChainId],
             data: abi.encodeWithSignature("increment()"),
             gasLimit: portal.XMSG_DEFAULT_GAS_LIMIT()
         });
@@ -126,24 +135,10 @@ contract Fixtures {
             sourceChainId: sourceChainId,
             destChainId: destChainId,
             streamOffset: offset,
-            sender: reverters[sourceChainId],
-            to: reverters[destChainId],
+            sender: _reverters[sourceChainId],
+            to: _reverters[destChainId],
             data: abi.encodeWithSignature("forceRevert()"),
             gasLimit: portal.XMSG_DEFAULT_GAS_LIMIT()
         });
-    }
-
-    // copied from forge-std/src/StdCheats.sol
-    // creates a labeled address and the corresponding private key
-    function _makeAddrAndKey(string memory name) internal virtual returns (address addr, uint256 privateKey) {
-        privateKey = uint256(keccak256(abi.encodePacked(name)));
-        addr = vm.addr(privateKey);
-        vm.label(addr, name);
-    }
-
-    // copied from forge-std/src/StdCheats.sol
-    // creates a labeled address
-    function _makeAddr(string memory name) internal virtual returns (address addr) {
-        (addr,) = _makeAddrAndKey(name);
     }
 }
