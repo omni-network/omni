5681f0b0abc479545ca0ab389a82e0d390d637fa
Kevin Halliday <halliday.kevin7@gmail.com>
2024-01-25 22:09:39 -0500

    chore(contracts): add ts xtype encoding and decoding utils (#183)
    Introduce ts xtypes encoding and decoding utils.
    
    task: none

---
+++ contracts/test/ts/abis.ts
@@ -0,0 +1,62 @@
+/**
+ * Abi parameters for xtype structs. Allowing us to encode / decode to
+ * and from bytes, matching Solidity's abi encoding. Struct fields must match
+ * the order of the corresponding struct definition in Solidity.
+ */
+
+// ABI parameters for XTypes.BlockHeader (src/libraries/XTypes.sol)
+export const xBlockHeaderParams = {
+  name: 'blockHeader',
+  type: 'tuple',
+  components: [
+    { name: 'sourceChainId', type: 'uint64' },
+    { name: 'blockHeight', type: 'uint64' },
+    { name: 'blockHash', type: 'bytes32' },
+  ],
+} as const
+
+// ABI parameters for XTypes.Msg (src/libraries/XTypes.sol)
+export const xMsgParams = {
+  name: 'msgs',
+  type: 'tuple',
+  components: [
+    { name: 'sourceChainId', type: 'uint64' },
+    { name: 'destChainId', type: 'uint64' },
+    { name: 'streamOffset', type: 'uint64' },
+    { name: 'sender', type: 'address' },
+    { name: 'to', type: 'address' },
+    { name: 'data', type: 'bytes' },
+    { name: 'gasLimit', type: 'uint64' },
+  ],
+} as const
+
+// ABI parameters for XTypes.Msg[]
+export const xMsgsParams = { ...xMsgParams, type: 'tuple[]' } as const
+
+// ABI parameters for XTypes.Submission (src/libraries/XTypes.sol)
+export const xSubParams = {
+  name: 'xsubs',
+  type: 'tuple',
+  components: [
+    { name: 'attestationRoot', type: 'bytes32' },
+    xBlockHeaderParams,
+    xMsgsParams,
+    { name: 'proof', type: 'bytes32[]' },
+    { name: 'proofFlags', type: 'bool[]' },
+    {
+      name: 'signatures',
+      type: 'tuple[]',
+      components: [
+        { name: 'validatorPubKey', type: 'bytes' },
+        { name: 'signature', type: 'bytes' },
+      ],
+    },
+  ],
+} as const
+
+// ABI parameters for TestXTypes.Block (test/common/TestXTypes.sol)
+export const xBlockParams = {
+  name: 'block',
+  type: 'tuple',
+  components: [xBlockHeaderParams, xMsgsParams],
+} as const
