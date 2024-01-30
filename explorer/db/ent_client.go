08df050c28c254ea9e8945a43c30a89bbabded1b
Dan Flanagan <dan.flanagan93@gmail.com>
2024-01-30 11:54:10 -0600

    feat(explorer/indexer): orm client funcationality and indexer config (#189)
    Creating an endpoint to mock and test inserts into db
    
    task: none

--- explorer/db/ent_client.go
+++ explorer/db/ent_client.go
@@ -17,25 +17,23 @@ import (
 type EntServerClient interface {
 	CreateNewEntClient(databaseURL string) (*ent.Client, error)
 	CreateSQLLiteEntClient() (*ent.Client, error)
+	CreateNewEntClientWithSchema(ctx context.Context, databaseURL string) (*ent.Client, error)
 }
 
-type ClientImpl struct {
+type Client struct {
 	EntServerClient
-	port int
 }
 
-func NewClient(port int) *ClientImpl {
-	client := ClientImpl{
-		port: port,
-	}
+func NewClient() *Client {
+	client := Client{}
 
 	return &client
 }
 
-func (ClientImpl) CreateNewEntClient(ctx context.Context, databaseURL string) (*ent.Client, error) {
+// Creates an ORM client without creating the tables.
+func (Client) CreateNewEntClient(databaseURL string) (*ent.Client, error) {
 	db, err := sql.Open("pgx", databaseURL)
 	if err != nil {
-		log.Error(ctx, "Failed to connect to postgres database: %v", err)
 		return nil, errors.Wrap(err, "failed to open postgres connection")
 	}
 
@@ -44,30 +42,44 @@ func (ClientImpl) CreateNewEntClient(ctx context.Context, databaseURL string) (*
 
 	client := ent.NewClient(ent.Driver(drv))
 
+	return client, nil
+}
+
+// Creates an ORM client and creates the tables on initialization.
+func (Client) CreateNewEntClientWithSchema(ctx context.Context, databaseURL string) (*ent.Client, error) {
+	db, err := sql.Open("pgx", databaseURL)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to open postgres connection")
+	}
+
+	// Create an ent.Driver from `db`.
+	drv := entsql.OpenDB(dialect.Postgres, db)
+
+	client := ent.NewClient(ent.Driver(drv))
+
+	// Run the automatic migration tool to create all schema resources.
 	if err := client.Schema.Create(ctx); err != nil {
-		log.Error(ctx, "Failed creating schema resources: %v", err)
 		return nil, errors.Wrap(err, "failed to do schema creation for postgresql db")
 	}
 
 	return client, nil
 }
 
-func (ClientImpl) CreateSQLiteEntClient(ctx context.Context) (*ent.Client, error) {
+// Creates an in-memory ORM client with tables.
+func (Client) CreateSQLiteEntClient(ctx context.Context) (*ent.Client, error) {
 	client, err := ent.Open(dialect.SQLite, "file:ent?mode=memory&cache=shared&_fk=1")
 	if err != nil {
-		log.Error(ctx, "Failed opening connection to sqlite: %v", err)
 		return nil, errors.Wrap(err, "failed to open sqlite connection")
 	}
 	defer func(client *ent.Client) {
 		err := client.Close()
 		if err != nil {
-			log.Error(ctx, "Failed closing ent client: %v", err)
+			log.Error(ctx, "Closing ent client", err)
 		}
 	}(client)
 
 	// Run the automatic migration tool to create all schema resources.
 	if err := client.Schema.Create(ctx); err != nil {
-		log.Error(ctx, "Failed creating schema resources: %v", err)
 		return nil, errors.Wrap(err, "failed to do schema creation for sqlite db")
 	}
 
