aeb4940d8d01b11e5165d55d7180f296a7791d92
corver <corver@omni.network>
2024-02-06 17:48:03 +0200

    test(e2e): fix flag typo (#245)
    Fix type causing e2e tests to fail.
    
    task: none

--- test/e2e/cmd/flags.go
+++ test/e2e/cmd/flags.go
@@ -10,7 +10,7 @@ import (
 func bindDefFlags(flags *pflag.FlagSet, cfg *app.DefinitionConfig) {
 	flags.StringVarP(&cfg.ManifestFile, "manifest-file", "f", cfg.ManifestFile, "path to manifest file")
 	flags.StringVar(&cfg.InfraProvider, "infra", cfg.InfraProvider, "infrastructure provider: docker, vmcompose")
-	flags.StringVar(&cfg.InfraProvider, "infra-file", cfg.InfraDataFile, "infrastructure data file (not required for docker provider)")
+	flags.StringVar(&cfg.InfraDataFile, "infra-file", cfg.InfraDataFile, "infrastructure data file (not required for docker provider)")
 	flags.StringVar(&cfg.DeployKeyFile, "deploy-key", cfg.DeployKeyFile, "path to deploy private key file")
 	flags.StringVar(&cfg.RelayerKeyFile, "relayer-key", cfg.RelayerKeyFile, "path to relayer private key file")
 }
