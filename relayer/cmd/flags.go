0ee978af969adbae01dd96ad541b40059d0cd182
corver <corver@omni.network>
2024-02-09 17:34:20 +0200

    chore(relayer): serve metrics (#273)
    Serve relayer metrics. 
    
    task: none

--- relayer/cmd/flags.go
+++ relayer/cmd/flags.go
@@ -6,10 +6,10 @@ import (
 	"github.com/spf13/pflag"
 )
 
+//nolint:lll,revive // Flags read easier when written as a single line.
 func bindRunFlags(flags *pflag.FlagSet, cfg *relayer.Config) {
-	flags.StringVar(&cfg.PrivateKey, "private-key", cfg.PrivateKey,
-		"The path to the private key e.g path/private.key")
+	flags.StringVar(&cfg.PrivateKey, "private-key", cfg.PrivateKey, "The path to the private key e.g path/private.key")
 	flags.StringVar(&cfg.HaloURL, "halo-url", cfg.HaloURL, "The URL of the halo node e.g localhost:26657")
-	flags.StringVar(&cfg.NetworkFile, "network-file", cfg.NetworkFile,
-		"The path to the network file e.g path/network.json")
+	flags.StringVar(&cfg.NetworkFile, "network-file", cfg.NetworkFile, "The path to the network file e.g path/network.json")
+	flags.StringVar(&cfg.MonitoringAddr, "monitoring-addr", cfg.MonitoringAddr, "The address to bind the monitoring server")
 }
