dba017f159424e747b23cfbc531fd6e3dcf9f986
lazar955 <lukic.lazar@yahoo.com>
2024-02-14 14:16:58 +0000

    logger happy

--- halo/cmd/init.go
+++ halo/cmd/init.go
@@ -196,12 +196,12 @@ func InitFiles(ctx context.Context, initCfg InitConfig) error {
 				},
 				{
 					ID:     100, // todo(Lazar): make it dynamic. this is coming from lib/xchain/provider/mock.go
-					Name:   "chainA",
+					Name:   "chain_a",
 					IsOmni: false,
 				},
 				{
 					ID:     200, // todo(Lazar): make it dynamic. this is coming from lib/xchain/provider/mock.go
-					Name:   "chainB",
+					Name:   "chain_b",
 					IsOmni: false,
 				},
 			},
