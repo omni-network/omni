e3ba53c52f89673c8445228e64f0eb4a295681dd
Kevin Halliday <kevin@omni.network>
2024-02-15 10:33:15 -0500

    reduce sol version changes

--- contracts/src/protocol/OmniPortal.sol
+++ contracts/src/protocol/OmniPortal.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-only
-pragma solidity ^0.8.12;
+pragma solidity 0.8.23;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { IFeeOracle } from "../interfaces/IFeeOracle.sol";
