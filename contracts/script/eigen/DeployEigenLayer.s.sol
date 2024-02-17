6301a25a6a3fd89de2535d2fc05de84eb362cb00
Kevin Halliday <kevin@omni.network>
2024-02-16 22:54:38 -0500

    fix var name

--- contracts/script/eigen/DeployEigenLayer.s.sol
+++ contracts/script/eigen/DeployEigenLayer.s.sol
@@ -16,22 +16,22 @@ contract DeployEigenLayer is EigenLayerDeployer, Script {
         string memory outputDir = vm.envOr("OUTPUT_DIR", defaultOutputDir);
         string memory outputFile = string.concat(outputDir, "/deployments.json");
 
-        string memory jsonOId = "id";
-        vm.serializeAddress(jsonOId, "ProxyAdmin", address(eigenLayerProxyAdmin));
-        vm.serializeAddress(jsonOId, "PauserRegistry", address(eigenLayerPauserReg));
-        vm.serializeAddress(jsonOId, "AVSDirectory", address(avsDirectory));
-        vm.serializeAddress(jsonOId, "DelegationManager", address(delegation));
-        vm.serializeAddress(jsonOId, "Slasher", address(slasher));
-        vm.serializeAddress(jsonOId, "StrategyManager", address(strategyManager));
-        vm.serializeAddress(jsonOId, "EigenPodManager", address(eigenPodManager));
-        vm.serializeAddress(jsonOId, "EigenPod", address(pod));
-        vm.serializeAddress(jsonOId, "DelayedWithdrawalRouter", address(delayedWithdrawalRouter));
-        vm.serializeAddress(jsonOId, "ETHPOSDeposit", address(ethPOSDeposit));
-        vm.serializeAddress(jsonOId, "EigenPodBeacon", address(eigenPodBeacon));
-        vm.serializeAddress(jsonOId, "EigenToken", address(eigenToken));
-        vm.serializeAddress(jsonOId, "EigenStrategy", address(eigenStrat));
-        vm.serializeAddress(jsonOId, "WETH", address(weth));
-        string memory json = vm.serializeAddress(jsonOId, "WETHStrategy", address(wethStrat));
+        string memory jsonId = "id";
+        vm.serializeAddress(jsonId, "ProxyAdmin", address(eigenLayerProxyAdmin));
+        vm.serializeAddress(jsonId, "PauserRegistry", address(eigenLayerPauserReg));
+        vm.serializeAddress(jsonId, "AVSDirectory", address(avsDirectory));
+        vm.serializeAddress(jsonId, "DelegationManager", address(delegation));
+        vm.serializeAddress(jsonId, "Slasher", address(slasher));
+        vm.serializeAddress(jsonId, "StrategyManager", address(strategyManager));
+        vm.serializeAddress(jsonId, "EigenPodManager", address(eigenPodManager));
+        vm.serializeAddress(jsonId, "EigenPod", address(pod));
+        vm.serializeAddress(jsonId, "DelayedWithdrawalRouter", address(delayedWithdrawalRouter));
+        vm.serializeAddress(jsonId, "ETHPOSDeposit", address(ethPOSDeposit));
+        vm.serializeAddress(jsonId, "EigenPodBeacon", address(eigenPodBeacon));
+        vm.serializeAddress(jsonId, "EigenToken", address(eigenToken));
+        vm.serializeAddress(jsonId, "EigenStrategy", address(eigenStrat));
+        vm.serializeAddress(jsonId, "WETH", address(weth));
+        string memory json = vm.serializeAddress(jsonId, "WETHStrategy", address(wethStrat));
         vm.writeJson(json, outputFile);
     }
 }
