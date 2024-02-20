f2c55bd9a7bdfea2eb1cd1e6bfa368be49ab1650
ttarsi <tyler@omni.network>
2024-02-20 07:41:50 -0500

    update anvil dump script

--- contracts/script/eigen/deploy-eigenlayer-save-anvil-state.sh
+++ contracts/script/eigen/deploy-eigenlayer-save-anvil-state.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-
-export OUTPUT_DIR="${OUTPUT_DIR:-"script/eigen/output"}"
-ANVIL_STATE_FILE="${OUTPUT_DIR}/anvil-state.json"
-
+project_root="$(git rev-parse --show-toplevel)"
+anvil_state_file="${project_root}/test/e2e/app/static/el-anvil-state.json"
+tmp_file=$(mktemp)
 
 # anvil private key 9
 DEPLOYER_KEY="${DEPLOYER_KEY:-0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6}"
 
-anvil --dump-state $ANVIL_STATE_FILE &
+anvil --dump-state $tmp_file &
 
 forge script script/eigen/DeployEigenLayer.s.sol \
   --rpc-url http://localhost:8545 \
@@ -18,6 +17,4 @@ forge script script/eigen/DeployEigenLayer.s.sol \
 # # kill anvil to save its state
 pkill anvil
 
-mv $ANVIL_STATE_FILE $OUTPUT_DIR/anvil-state.json.tmp
-jq '.block.number = "0x0"' $OUTPUT_DIR/anvil-state.json.tmp > $ANVIL_STATE_FILE
-rm $OUTPUT_DIR/anvil-state.json.tmp
+jq '.block.number = "0x0"' $tmp_file > $anvil_state_file
