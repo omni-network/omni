0c4628de31ff64c865006d4350ea394f0af6836e
corver <corver@omni.network>
2024-02-23 17:49:28 +0200

    fix(lib/txmgr): handle wait mine edge case (#382)
    Fixes root cause of "nonce to low" in e2e tests. When quering
    txreceipts, geth sometimes returns "transaction indexing in progress".
    Old txmgr didn't handle this and tried resubmitting while the tx was
    then confirmed in most cases. Instead it should re-attempt fetching the
    receipt.
    
    task: none

--- lib/txmgr/txmgr.go
+++ lib/txmgr/txmgr.go
@@ -135,12 +135,17 @@ func (m *SimpleTxManager) Close() {
 // txFields returns a logger with the transaction hash and nonce fields set.
 //
 //nolint:revive // Might fix
-func txFields(tx *types.Transaction,
-	logGas bool) []slog.Attr {
-	fields := []slog.Attr{slog.String("tx", tx.Hash().String()), slog.Int64("nonce", int64(tx.Nonce()))}
+func txFields(tx *types.Transaction, logGas bool) []any {
+	fields := []any{
+		slog.Int64("nonce", int64(tx.Nonce())),
+		slog.String("tx", tx.Hash().String()),
+	}
 	if logGas {
-		fields = append(fields, slog.String("gas_tip_cap", tx.GasTipCap().String()),
-			slog.String("gas_fee_cap", tx.GasFeeCap().String()), slog.Int64("gas_limit", int64(tx.Gas())))
+		fields = append(fields,
+			slog.String("gas_tip_cap", tx.GasTipCap().String()),
+			slog.String("gas_fee_cap", tx.GasFeeCap().String()),
+			slog.Int64("gas_limit", int64(tx.Gas())),
+		)
 	}
 
 	return fields
@@ -353,8 +358,7 @@ func (m *SimpleTxManager) sendTx(ctx context.Context, tx *types.Transaction) (*t
 			}
 			// If we see lots of unrecoverable errors (and no pending transactions) abort sending the transaction.
 			if sendState.ShouldAbortImmediately() {
-				attrs := txFields(tx, false)
-				return nil, errors.New("aborted transaction sending", attrs)
+				return nil, errors.New("aborted transaction sending", txFields(tx, false)...)
 			}
 			// if the tx manager closed while we were waiting for the tx, give up
 			if m.closed.Load() {
@@ -493,7 +497,9 @@ func (m *SimpleTxManager) queryReceipt(ctx context.Context, txHash common.Hash,
 	defer cancel()
 	receipt, err := m.backend.TransactionReceipt(ctx, txHash)
 	if err != nil {
-		if errors.Is(err, ethereum.NotFound) || strings.Contains(err.Error(), ethereum.NotFound.Error()) {
+		if strings.Contains(err.Error(), "transaction indexing is in progress") {
+			return nil, false, nil // Just back off here
+		} else if errors.Is(err, ethereum.NotFound) || strings.Contains(err.Error(), ethereum.NotFound.Error()) {
 			sendState.TxNotMined(txHash)
 			return nil, false, nil
 		}
@@ -533,7 +539,7 @@ func (m *SimpleTxManager) increaseGasPrice(ctx context.Context, tx *types.Transa
 	log.Debug(ctx, "Bumping gas price")
 	tip, baseFee, err := m.suggestGasPriceCaps(ctx)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to get gas price info", txFields(tx, true))
+		return nil, errors.Wrap(err, "failed to get gas price info", txFields(tx, true)...)
 	}
 	bumpedTip, bumpedFee := updateFees(ctx, tx.GasTipCap(), tx.GasFeeCap(), tip, baseFee)
 
