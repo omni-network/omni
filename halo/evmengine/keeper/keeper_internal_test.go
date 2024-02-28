206948dccac1d0421c6c8d8d153aa24e9003e5f6
lazar955 <lukic.lazar@yahoo.com>
2024-02-28 12:08:26 +0000

    custom fuzz

--- halo/evmengine/keeper/keeper_internal_test.go
+++ halo/evmengine/keeper/keeper_internal_test.go
@@ -3,12 +3,15 @@ package keeper
 import (
 	"context"
 	"testing"
+	"time"
 
+	k1 "github.com/cometbft/cometbft/crypto/secp256k1"
 	cmttypes "github.com/cometbft/cometbft/types"
-	"github.com/ethereum/go-ethereum/common"
-
 	authtx "github.com/cosmos/cosmos-sdk/x/auth/tx"
+	"github.com/ethereum/go-ethereum/common"
+	fuzz "github.com/google/gofuzz"
 	"github.com/omni-network/omni/halo/comet"
+	"github.com/omni-network/omni/lib/errors"
 	"github.com/stretchr/testify/require"
 )
 
@@ -24,15 +27,53 @@ func TestKeeper_isNextProposer(t *testing.T) {
 		address: common.BytesToAddress([]byte("test")), // todo valid address
 	}
 	keeper := NewKeeper(cdc, storeService, &mockEngine, txConfig, ap)
-	keeper.cmtAPI = mockCometAPI{}
+	keeper.cmtAPI = newMockCometAPI()
 
 	keeper.isNextProposer(ctx)
 }
 
 type mockCometAPI struct {
 	comet.API
+	fuzzer *fuzz.Fuzzer
 }
 
-func (m mockCometAPI) Validators(ctx context.Context, height int64) (*cmttypes.ValidatorSet, bool, error) {
-	return nil, false, nil
+func newMockCometAPI() mockCometAPI {
+	return mockCometAPI{
+		fuzzer: NewFuzzer(0),
+	}
+}
+
+func (m mockCometAPI) Validators(context.Context, int64) (*cmttypes.ValidatorSet, bool, error) {
+	var validators []*cmttypes.Validator
+
+	m.fuzzer.NilChance(0).NumElements(3, 7).Fuzz(&validators)
+
+	valSet := new(cmttypes.ValidatorSet)
+	if err := valSet.UpdateWithChangeSet(validators); err != nil {
+		return nil, false, errors.Wrap(err, "update with change set")
+	}
+
+	return valSet, true, nil
+}
+
+// NewFuzzer returns a new fuzzer for valid ethereum types.
+// If seed is zero, it uses current nano time as the seed.
+func NewFuzzer(seed int64) *fuzz.Fuzzer {
+	if seed == 0 {
+		seed = time.Now().UnixNano()
+	}
+
+	f := fuzz.NewWithSeed(seed).NilChance(0)
+	f.Funcs(
+		func(v *cmttypes.Validator, c fuzz.Continue) {
+			privKey := k1.GenPrivKey()
+			v.PubKey = privKey.PubKey()
+			v.VotingPower = int64(c.Intn(200))
+			val := cmttypes.NewValidator(v.PubKey, v.VotingPower)
+
+			*v = *val
+		},
+	)
+
+	return f
 }
