be06e0a07d9e0463f02978a28542eeeb05bd3d4c
Kevin Halliday <kevin@omni.network>
2024-02-29 12:46:34 -0500

    update bindings and renames

--- contracts/bindings/renames.go
+++ contracts/bindings/renames.go
@@ -1,8 +1,7 @@
 package bindings
 
 // abigen generates types for library structs with the format `{LibraryName}{StructName}`.
-// leading to some awkward go code like `bindings.ValidatorsValidator`
-// we rename those types here to make them more idiomatic
+// leading to some awkward type name. we rename some types here
 
-type Validator = ValidatorsValidator
-type ValidatorSigTuple = ValidatorsSigTuple
+type Validator = XTypesValidator
+type ValidatorSigTuple = XTypesSigTuple
