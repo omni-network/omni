aff7c3b138e5b50f795c169b3dc835dd2c87d6ad
Lazar <12626340+Lazar955@users.noreply.github.com>
2024-02-29 15:14:07 +0100

    test(halo/evmengine): next proposer tests (#448)
    adds tests for next proposer
    
    task: none

---
+++ halo/evmengine/keeper/keeper_internal_test.go
@@ -0,0 +1,188 @@
+package keeper
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/omni-network/omni/halo/comet"
+	"github.com/omni-network/omni/lib/errors"
+	"github.com/omni-network/omni/lib/k1util"
+
+	k1 "github.com/cometbft/cometbft/crypto/secp256k1"
+	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	cmttypes "github.com/cometbft/cometbft/types"
+
+	authtx "github.com/cosmos/cosmos-sdk/x/auth/tx"
+	fuzz "github.com/google/gofuzz"
+	"github.com/stretchr/testify/require"
+)
+
+func TestKeeper_isNextProposer(t *testing.T) {
+	t.Parallel()
+	type args struct {
+		height         int64
+		validatorsFunc func(context.Context, int64) (*cmttypes.ValidatorSet, bool, error)
+		current        int
+		next           int
+	}
+	height := int64(1)
+	tests := []struct {
+		name       string
+		args       args
+		want       bool
+		wantHeight uint64
+		wantErr    bool
+	}{
+		{
+			name: "is next proposer",
+			args: args{
+				height:  height,
+				current: 0,
+				next:    1,
+			},
+			want:       true,
+			wantHeight: 2,
+			wantErr:    false,
+		},
+		{
+			name: "proposer false",
+			args: args{
+				height:  height,
+				current: 0,
+				next:    2,
+			},
+			want:       false,
+			wantHeight: 2,
+			wantErr:    false,
+		},
+		{
+			name: "validatorsFunc error",
+			args: args{
+				height:  height,
+				current: 0,
+				next:    1,
+				validatorsFunc: func(ctx context.Context, i int64) (*cmttypes.ValidatorSet, bool, error) {
+					return nil, false, errors.New("error")
+				},
+			},
+			want:    false,
+			wantErr: true,
+		},
+		{
+			name: "validatorsFunc not ok",
+			args: args{
+				height:  height,
+				current: 0,
+				next:    1,
+				validatorsFunc: func(ctx context.Context, i int64) (*cmttypes.ValidatorSet, bool, error) {
+					return nil, false, nil
+				},
+			},
+			want:    false,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			cdc := getCodec(t)
+			txConfig := authtx.NewTxConfig(cdc, nil)
+			mockEngine, err := newMockEngineAPI()
+			require.NoError(t, err)
+
+			cmtAPI := newMockCometAPI(t, tt.args.validatorsFunc)
+			header := cmtproto.Header{Height: tt.args.height}
+			header.ProposerAddress = cmtAPI.validatorSet.Validators[tt.args.current].Address
+
+			nxtAddr, err := k1util.PubKeyToAddress(cmtAPI.validatorSet.Validators[tt.args.next].PubKey)
+			require.NoError(t, err)
+
+			ctx, storeService := setupCtxStore(t, &header)
+
+			keeper := NewKeeper(cdc, storeService, &mockEngine, txConfig)
+			keeper.SetCometAPI(&cmtAPI)
+			keeper.SetAddressProvider(mockAddressProvider{
+				address: nxtAddr,
+			})
+
+			got, gotHeight, err := keeper.isNextProposer(ctx)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("isNextProposer() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("isNextProposer() got = %v, want %v", got, tt.want)
+			}
+			if gotHeight != tt.wantHeight {
+				t.Errorf("isNextProposer() gotHeight = %v, want %v", gotHeight, tt.wantHeight)
+			}
+			// make sure that height passed into Validators is correct
+			require.Equal(t, tt.args.height, cmtAPI.height)
+		})
+	}
+}
+
+type mockCometAPI struct {
+	comet.API
+	fuzzer         *fuzz.Fuzzer
+	validatorSet   *cmttypes.ValidatorSet
+	validatorsFunc func(context.Context, int64) (*cmttypes.ValidatorSet, bool, error)
+	height         int64
+}
+
+func newMockCometAPI(t *testing.T, valFun func(context.Context, int64) (*cmttypes.ValidatorSet, bool, error)) mockCometAPI {
+	t.Helper()
+	fuzzer := newFuzzer(0)
+	valSet := fuzzValidators(t, fuzzer)
+
+	return mockCometAPI{
+		fuzzer:         fuzzer,
+		validatorSet:   valSet,
+		validatorsFunc: valFun,
+	}
+}
+
+func fuzzValidators(t *testing.T, fuzzer *fuzz.Fuzzer) *cmttypes.ValidatorSet {
+	t.Helper()
+	var validators []*cmttypes.Validator
+
+	fuzzer.NilChance(0).NumElements(3, 7).Fuzz(&validators)
+
+	valSet := new(cmttypes.ValidatorSet)
+	err := valSet.UpdateWithChangeSet(validators)
+	require.NoError(t, err)
+
+	return valSet
+}
+
+func (m *mockCometAPI) Validators(ctx context.Context, height int64) (*cmttypes.ValidatorSet, bool, error) {
+	m.height = height
+	if m.validatorsFunc != nil {
+		return m.validatorsFunc(ctx, height)
+	}
+
+	return m.validatorSet, true, nil
+}
+
+// newFuzzer - create a new custom cmttypes.Validator fuzzer.
+func newFuzzer(seed int64) *fuzz.Fuzzer {
+	if seed == 0 {
+		seed = time.Now().UnixNano()
+	}
+
+	f := fuzz.NewWithSeed(seed).NilChance(0)
+	f.Funcs(
+		func(v *cmttypes.Validator, c fuzz.Continue) {
+			privKey := k1.GenPrivKey()
+			v.PubKey = privKey.PubKey()
+			v.VotingPower = 200
+			val := cmttypes.NewValidator(v.PubKey, v.VotingPower)
+
+			*v = *val
+		},
+	)
+
+	return f
+}
