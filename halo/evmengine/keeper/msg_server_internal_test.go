6df5cfadf8800f694bc1e1928a7dd82d7982aa50
lazar955 <lukic.lazar@yahoo.com>
2024-03-01 12:53:00 +0000

    cleanup

--- halo/evmengine/keeper/msg_server_internal_test.go
+++ halo/evmengine/keeper/msg_server_internal_test.go
@@ -6,12 +6,14 @@ import (
 	"testing"
 	"time"
 
+	"github.com/omni-network/omni/halo/evmengine/types"
+	"github.com/omni-network/omni/lib/k1util"
+
 	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtx "github.com/cosmos/cosmos-sdk/x/auth/tx"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-	"github.com/omni-network/omni/halo/evmengine/types"
-	"github.com/omni-network/omni/lib/k1util"
 	"github.com/stretchr/testify/require"
 )
 
@@ -23,9 +25,9 @@ func Test_msgServer_ExecutionPayload(t *testing.T) {
 	mockEngine, err := newMockEngineAPI()
 	require.NoError(t, err)
 	cmtAPI := newMockCometAPI(t, nil)
+	// set the header and proposer so we have the correct next proposer
 	header := cmtproto.Header{Height: 1}
 	header.ProposerAddress = cmtAPI.validatorSet.Validators[0].Address
-
 	nxtAddr, err := k1util.PubKeyToAddress(cmtAPI.validatorSet.Validators[1].PubKey)
 	require.NoError(t, err)
 
@@ -40,26 +42,37 @@ func Test_msgServer_ExecutionPayload(t *testing.T) {
 	keeper.SetCometAPI(&cmtAPI)
 	msgSrv := NewMsgServerImpl(keeper)
 
-	ts := time.Now()
-	latestHeight, err := mockEngine.BlockNumber(ctx)
-	require.NoError(t, err)
-	latestBlock, err := mockEngine.BlockByNumber(ctx, big.NewInt(int64(latestHeight)))
-	require.NoError(t, err)
+	var payloadData []byte
+	newPayload := func() {
+		// get latest block to build on top of
+		latestHeight, err := mockEngine.BlockNumber(ctx)
+		require.NoError(t, err)
+		latestBlock, err := mockEngine.BlockByNumber(ctx, big.NewInt(int64(latestHeight)))
+		require.NoError(t, err)
 
-	payloadID := mockEngine.pushPayload(t, ctx, ap.LocalAddress(), latestBlock.Hash(), ts)
-	payloadResp, err := mockEngine.GetPayloadV2(ctx, *payloadID)
-	require.NoError(t, err)
+		payloadID := mockEngine.pushPayload(t, ctx, ap.LocalAddress(), latestBlock.Hash(), time.Now())
+		payloadResp, err := mockEngine.GetPayloadV2(ctx, *payloadID)
+		require.NoError(t, err)
 
-	// Create execution payload message
-	payloadData, err := json.Marshal(payloadResp.ExecutionPayload)
-	require.NoError(t, err)
-
-	msg := &types.MsgExecutionPayload{
-		Authority: authtypes.NewModuleAddress(types.ModuleName).String(),
-		Data:      payloadData,
+		// Create execution payload message
+		payloadData, err = json.Marshal(payloadResp.ExecutionPayload)
+		require.NoError(t, err)
 	}
 
-	resp, err := msgSrv.ExecutionPayload(ctx, msg)
-	require.NoError(t, err)
-	require.NotNil(t, resp)
+	assertExecutionPayload := func() {
+		resp, err := msgSrv.ExecutionPayload(ctx, &types.MsgExecutionPayload{
+			Authority: authtypes.NewModuleAddress(types.ModuleName).String(),
+			Data:      payloadData,
+		})
+		require.NoError(t, err)
+		require.NotNil(t, resp)
+	}
+
+	newPayload()
+	assertExecutionPayload()
+
+	// not lets run optimistic flow
+	newPayload()
+	keeper.SetBuildOptimistic(true)
+	assertExecutionPayload()
 }
