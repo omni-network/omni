89e783f5d354423f08761b5961a2545c80bd4668
Kevin Halliday <kevin@omni.network>
2024-03-01 14:03:26 -0500

    have mock portal reflect real portal

--- contracts/test/utils/MockPortal.sol
+++ contracts/test/utils/MockPortal.sol
@@ -3,47 +3,81 @@ pragma solidity ^0.8.12;
 
 import { XTypes } from "../../src/libraries/XTypes.sol";
 import { OmniPortalConstants } from "../../src/protocol/OmniPortalConstants.sol";
+import { IFeeOracle } from "../../src/interfaces/IFeeOracle.sol";
+import { MockFeeOracle } from "./MockFeeOracle.sol";
 
 /**
  * @title MockPortal
- * @dev A mock OmniPortal contract, for testing purposes.
+ * @notice A mock portal, used for testing.
+ *         - Matches real portal functionality for user facing functions (xcall, feeFor, and xmsg),
+ *           so that user unit tests consume gas as expected.
+ *         - All non-user facing functions & state are not included.
+ *         - Provides a mockXCall function for testing xcall execution.
  */
 contract MockPortal is OmniPortalConstants {
-    /// @notice The current XMsg being executed, exposed via xmsg() getter
-    XTypes.Msg private _currentXMsg;
+    event XMsg(
+        uint64 indexed destChainId, uint64 indexed streamOffset, address sender, address to, bytes data, uint64 gasLimit
+    );
 
-    /// @notice Returns current xmsg, same as OmniPortal.xmsg()
+    event XReceipt(
+        uint64 indexed sourceChainId, uint64 indexed streamOffset, uint256 gasUsed, address relayer, bool success
+    );
+
+    uint64 public immutable chainId;
+
+    address public feeOracle;
+
+    mapping(uint64 => uint64) public outXStreamOffset;
+
+    XTypes.Msg private _currentXmsg;
+
+    constructor() {
+        chainId = uint64(block.chainid);
+        feeOracle = address(new MockFeeOracle(1 gwei));
+    }
+
+    //////////////////////////////////////////////////////////////////////////////
+    //                      Standard Portal Functions                           //
+    //////////////////////////////////////////////////////////////////////////////
+
+    function xcall(uint64 destChainId, address to, bytes calldata data) external payable {
+        _xcall(destChainId, msg.sender, to, data, XMSG_DEFAULT_GAS_LIMIT);
+    }
+
+    function xcall(uint64 destChainId, address to, bytes calldata data, uint64 gasLimit) external payable {
+        _xcall(destChainId, msg.sender, to, data, gasLimit);
+    }
+
+    function feeFor(uint64 destChainId, bytes calldata data) public view returns (uint256) {
+        return IFeeOracle(feeOracle).feeFor(destChainId, data, XMSG_DEFAULT_GAS_LIMIT);
+    }
+
+    function feeFor(uint64 destChainId, bytes calldata data, uint64 gasLimit) public view returns (uint256) {
+        return IFeeOracle(feeOracle).feeFor(destChainId, data, gasLimit);
+    }
+
+    function _xcall(uint64 destChainId, address sender, address to, bytes calldata data, uint64 gasLimit) private {
+        require(msg.value >= feeFor(destChainId, data, gasLimit), "OmniPortal: insufficient fee");
+        require(gasLimit <= XMSG_MAX_GAS_LIMIT, "OmniPortal: gasLimit too high");
+        require(gasLimit >= XMSG_MIN_GAS_LIMIT, "OmniPortal: gasLimit too low");
+        require(destChainId != chainId, "OmniPortal: no same-chain xcall");
+
+        outXStreamOffset[destChainId] += 1;
+
+        emit XMsg(destChainId, outXStreamOffset[destChainId], sender, to, data, gasLimit);
+    }
+
     function xmsg() external view returns (XTypes.Msg memory) {
-        return _currentXMsg;
+        return _currentXmsg;
     }
 
-    /// @notice Stubbed feeFor, default gas limit
-    function feeFor(uint64, /* destChainId */ bytes calldata /* data */ ) external pure returns (uint256) {
-        return 1 gwei;
-    }
-
-    /// @notice Stubbed feeFor, custom gas limit
-    function feeFor(uint64, /* destChainId */ bytes calldata, /* data */ uint64 /* gasLimit */ )
-        external
-        pure
-        returns (uint256)
-    {
-        return 1 gwei;
-    }
-
-    /// @notice Stubbed xcall, default gas limit
-    function xcall(uint64, /* destChainId */ address, /* to */ bytes calldata /* data */ ) external payable { }
-
-    /// @notice Stubbed xcall, custom gas limit
-    function xcall(uint64, /* destChainId */ address, /* to */ bytes calldata, /* data */ uint64 /* gasLimit */ )
-        external
-        payable
-    { }
-
-    /// @notice Returns true if the current call is an xcall, same as OmniPortal.isXCall()
     function isXCall() external view returns (bool) {
-        return _currentXMsg.sourceChainId != 0;
+        return _currentXmsg.sourceChainId != 0;
     }
+
+    //////////////////////////////////////////////////////////////////////////////
+    //                              Portal Mocks                                //
+    //////////////////////////////////////////////////////////////////////////////
 
     /// @notice Execute a mock xcall, default gas limit. Reverts if the call fails, or if the gas limit is too low
     function mockXCall(uint64 sourceChainId, address sender, address to, bytes calldata data) external {
@@ -52,10 +86,10 @@ contract MockPortal is OmniPortalConstants {
 
     /// @dev Execute a mock xcall, custom gas limit. Reverts if the call fails, or if the gas limit is too low
     function mockXCall(uint64 sourceChainId, address sender, address to, bytes calldata data, uint64 gasLimit) public {
-        require(gasLimit <= XMSG_MAX_GAS_LIMIT, "MockPortal: gasLimit too high");
-        require(gasLimit >= XMSG_MIN_GAS_LIMIT, "MockPortal: gasLimit too low");
+        require(gasLimit <= XMSG_MAX_GAS_LIMIT, "OmniPortal: gasLimit too high");
+        require(gasLimit >= XMSG_MIN_GAS_LIMIT, "OmniPortal: gasLimit too low");
 
-        _currentXMsg = XTypes.Msg({ sourceChainId: sourceChainId, sender: sender });
+        _currentXmsg = XTypes.Msg({ sourceChainId: sourceChainId, sender: sender });
 
         uint256 gasUsed = gasleft();
         (bool success, bytes memory returnData) = to.call{ gas: gasLimit }(data);
