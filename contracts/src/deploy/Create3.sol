199b84f31d47c89084f1addecc92e14e180a90c0
Kevin Halliday <kevin@omni.network>
2024-03-05 15:38:37 -0500

    feat(contracts): use create3 for avs deployments (#500)
    Use create3 for avs deployments
    
    task: https://app.asana.com/0/1206208509925075/1206648863973026

--- contracts/src/deploy/Create3.sol
+++ contracts/src/deploy/Create3.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.12;
 
 import { CREATE3 } from "solmate/src/utils/CREATE3.sol";
 
--- contracts/test/avs/common/Fixtures.sol
+++ contracts/test/avs/common/Fixtures.sol
@@ -5,6 +5,8 @@ import { ProxyAdmin } from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin
 import { TransparentUpgradeableProxy } from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import { ITransparentUpgradeableProxy } from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
+import { Bytes32AddressLib } from "solmate/src/utils/Bytes32AddressLib.sol";
+
 import { IAVSDirectory } from "eigenlayer-contracts/src/contracts/interfaces/IAVSDirectory.sol";
 import { IStrategy } from "eigenlayer-contracts/src/contracts/interfaces/IStrategyManager.sol";
 
@@ -13,6 +15,7 @@ import { IOmniPortal } from "src/interfaces/IOmniPortal.sol";
 import { IOmniAVS } from "src/interfaces/IOmniAVS.sol";
 import { OmniAVS } from "src/protocol/OmniAVS.sol";
 
+import { Create3 } from "src/deploy/Create3.sol";
 import { DeployGoerliAVS } from "script/avs/DeployGoerliAVS.s.sol";
 import { StrategyParams } from "script/avs/StrategyParams.sol";
 import { MockOmniPredeploys } from "test/utils/MockOmniPredeploys.sol";
@@ -20,11 +23,15 @@ import { MockPortal } from "test/utils/MockPortal.sol";
 import { EigenLayerFixtures } from "./eigen/EigenLayerFixtures.sol";
 import { Empty } from "./Empty.sol";
 
+import { console } from "forge-std/console.sol";
+
 /**
  * @title Fixtures
  * @dev Common fixtures contract for all AVS tests.
  */
 contract Fixtures is EigenLayerFixtures {
+    using Bytes32AddressLib for bytes32;
+
     address multisig = makeAddr("multisig");
     address proxyAdminOwner = multisig;
     address omniAVSOwner = multisig;
@@ -52,26 +59,48 @@ contract Fixtures is EigenLayerFixtures {
 
     function _deployGoerliAVS() internal returns (OmniAVS) {
         DeployGoerliAVS deployer = new DeployGoerliAVS();
+
+        Create3 create3 = new Create3();
+        bytes32 create3Salt = keccak256("avs-goerli-fork-test");
+
         return OmniAVS(
-            deployer.prankDeploy(proxyAdminOwner, omniAVSOwner, address(proxyAdmin), address(portal), omniChainId)
+            deployer.prankDeploy(
+                proxyAdminOwner,
+                address(create3),
+                create3Salt,
+                omniAVSOwner,
+                address(proxyAdmin),
+                address(portal),
+                omniChainId,
+                ethStakeInbox
+            )
         );
     }
 
     function _deployLocalAVS() internal returns (OmniAVS) {
-        vm.startPrank(proxyAdminOwner);
+        Create3 create3 = new Create3();
+        bytes32 create3Salt = keccak256("avs-local-test");
 
-        address proxy = address(new TransparentUpgradeableProxy(address(new Empty()), address(proxyAdmin), ""));
         address impl =
             address(new OmniAVS(IDelegationManager(address(delegation)), IAVSDirectory(address(avsDirectory))));
 
-        ProxyAdmin(proxyAdmin).upgradeAndCall(
-            ITransparentUpgradeableProxy(proxy),
-            impl,
-            abi.encodeWithSelector(
-                OmniAVS.initialize.selector, omniAVSOwner, portal, omniChainId, ethStakeInbox, _localStrategyParams()
+        bytes memory bytecode = abi.encodePacked(
+            vm.getCode("TransparentUpgradeableProxy.sol:TransparentUpgradeableProxy.0.8.12"),
+            abi.encode(
+                address(impl),
+                proxyAdmin,
+                abi.encodeWithSelector(
+                    OmniAVS.initialize.selector,
+                    omniAVSOwner,
+                    portal,
+                    omniChainId,
+                    ethStakeInbox,
+                    _localStrategyParams()
+                )
             )
         );
-        vm.stopPrank();
+
+        address proxy = create3.deploy(create3Salt, bytecode);
 
         return OmniAVS(proxy);
     }
