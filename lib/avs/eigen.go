e1d1acf3b0c7c2ed80fdf43714dd7eb35d361b6b
Kevin Halliday <kevin@omni.network>
2024-03-05 16:02:18 -0500

    chore(lib/avs): denominate amounts in wei

--- lib/avs/eigen.go
+++ lib/avs/eigen.go
@@ -2,7 +2,6 @@ package avs
 
 import (
 	"context"
-	"math/big"
 
 	"github.com/omni-network/omni/contracts/bindings"
 	"github.com/omni-network/omni/lib/errors"
@@ -42,36 +41,6 @@ func RegisterOperatorWithEigen(ctx context.Context, avs Contracts, backend *ethb
 	}
 
 	return receipt, nil
-}
-
-// DelegateWETH delegates WETH to the Eigen Layer strategy manager's WETH strategy.
-func DelegateWETH(ctx context.Context, contracts Contracts, backend *ethbackend.Backend, delegator common.Address, amount int64) error {
-	txOpts, err := backend.BindOpts(ctx, delegator)
-	if err != nil {
-		return err
-	}
-
-	// First approve the strategy manager to "assign" the WETH to itself.
-	tx, err := contracts.WETHToken.Approve(txOpts, contracts.StrategyManagerAddr, big.NewInt(amount))
-	if err != nil {
-		return errors.Wrap(err, "approve")
-	}
-
-	if _, err = backend.WaitMined(ctx, tx); err != nil {
-		return errors.Wrap(err, "wait mined")
-	}
-
-	// Then deposit the WETH into the strategy (it will assign it to itself).
-	tx, err = contracts.StrategyManager.DepositIntoStrategy(txOpts, contracts.WETHStrategyAddr, contracts.WETHTokenAddr, big.NewInt(amount))
-	if err != nil {
-		return errors.Wrap(err, "deposit into strategy")
-	}
-
-	if _, err = backend.WaitMined(ctx, tx); err != nil {
-		return errors.Wrap(err, "wait mined")
-	}
-
-	return nil
 }
 
 // Undelegate undelegates WETH from the Eigen Layer strategy manager's WETH strategy.
