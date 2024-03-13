05851df2626a69edd7e1f0befe08bcdac14ed6f4
Kevin Halliday <kevin@omni.network>
2024-03-13 14:07:40 -0400

    feat(contracts): require validator pubkey in deposit (#582)
    Require secp256k1 65 uncompressed public key in validator deposits
    
    task: https://app.asana.com/0/1206208509925075/1206227813760101

--- contracts/src/protocol/OmniStake.sol
+++ contracts/src/protocol/OmniStake.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.23;
 
+import { Secp256k1 } from "../libraries/Secp256k1.sol";
+
 /**
  * @title OmniStake
  * @notice The deposit contract for OMNI-staked validators.
@@ -8,18 +10,20 @@ pragma solidity 0.8.23;
 contract OmniStake {
     /**
      * @notice Emitted when a user deposits funds into the contract.
-     * @param depositor Address of the depositor
+     * @param pubkey    64 byte uncompressed secp256k1 public key (no 0x04 prefix)
      * @param amount    Funds deposited
      */
-    event Deposit(address indexed depositor, uint256 amount);
+    event Deposit(bytes pubkey, uint256 amount);
 
     /**
      * @notice Deposit OMNI. This is the entry point for validator staking. The consensus chain is
      *         notified of the deposit, and manages stake accounting / validator onboarding.
+     * @param pubkey 64 byte uncompressed secp256k1 public key (no 0x04 prefix)
      */
-    function deposit() external payable {
+    function deposit(bytes memory pubkey) external payable {
         require(msg.value > 1 ether, "OmniStake: deposit amt too low");
         require(msg.value < type(uint64).max, "OmniStake: deposit amt too high");
-        emit Deposit(msg.sender, msg.value);
+        require(msg.sender == Secp256k1.pubkeyToAddress(pubkey), "OmniStake: pubkey not sender");
+        emit Deposit(pubkey, msg.value);
     }
 }
