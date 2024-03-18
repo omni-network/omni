b2008c39e5e89de26f95349dbaac68276fddada9
lazar955 <lukic.lazar@yahoo.com>
2024-03-18 10:11:57 +0000

    halo cfg depending on node type

--- test/e2e/app/setup.go
+++ test/e2e/app/setup.go
@@ -108,7 +108,21 @@ func Setup(ctx context.Context, def Definition, agentSecrets agent.Secrets, test
 		}
 		config.WriteConfigFile(filepath.Join(nodeDir, "config", "config.toml"), cfg) // panics
 
-		if err := writeHaloConfig(nodeDir, logCfg, testCfg); err != nil {
+		var minRetainBlocks uint64
+		var pruningOption string
+		switch node.Mode {
+		case e2e.ModeValidator, e2e.ModeFull:
+			pruningOption = "nothing"
+			minRetainBlocks = 0
+		case e2e.ModeSeed, e2e.ModeLight:
+			pruningOption = "everything"
+			minRetainBlocks = 1
+		default:
+			pruningOption = "nothing"
+			minRetainBlocks = 0
+		}
+
+		if err := writeHaloConfig(nodeDir, logCfg, testCfg, minRetainBlocks, pruningOption); err != nil {
 			return err
 		}
 
@@ -306,10 +320,12 @@ func MakeConfig(node *e2e.Node, nodeDir string) (*config.Config, error) {
 }
 
 // writeHaloConfig generates an halo application config for a node and writes it to disk.
-func writeHaloConfig(nodeDir string, logCfg log.Config, testCfg bool) error {
+func writeHaloConfig(nodeDir string, logCfg log.Config, testCfg bool, minRetainBlocks uint64, pruningOption string) error {
 	cfg := halocfg.DefaultConfig()
 	cfg.HomeDir = nodeDir
 	cfg.EngineJWTFile = "/geth/jwtsecret" // As per docker-compose mount
+	cfg.PruningOption = pruningOption
+	cfg.MinRetainBlocks = minRetainBlocks
 	if testCfg {
 		cfg.SnapshotInterval = 1   // Write snapshots each block in e2e tests
 		cfg.SnapshotKeepRecent = 0 // Keep all snapshots in e2e tests
