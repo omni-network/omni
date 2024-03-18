e2cc514c6ab615a3310fcbd04e67895a7f8b565c
corver <corver@omni.network>
2024-03-18 09:02:22 +0200

    fix(halo/attest): only approve sequentially

--- halo/attest/types/keys.go
+++ halo/attest/types/keys.go
@@ -3,10 +3,4 @@ package types
 const (
 	// ModuleName defines the module name.
 	ModuleName = "attest"
-
-	// StoreKey defines the primary module store key.
-	StoreKey = ModuleName
-
-	// MemStoreKey defines the in-memory store key.
-	MemStoreKey = "mem_attest"
 )
