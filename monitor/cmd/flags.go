fe65b58e5e45e6dd2fa3ef5ab2da8111c1a0895f
corver <corver@omni.network>
2024-03-25 15:47:02 +0200

    ci(monitor/loadgen): add auto validator self-delegation (#649)
    Add a `loadgen` package to `monitor` service that periodically
    self-delegates validator stake to test validator updates in
    staging/devnet.
    
    Update monitor config to load those keys and e2e app to wire it.
    
    task: none

--- monitor/cmd/flags.go
+++ monitor/cmd/flags.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	monitor "github.com/omni-network/omni/monitor/app"
+	"github.com/omni-network/omni/monitor/loadgen"
 
 	"github.com/spf13/pflag"
 )
@@ -9,4 +10,8 @@ import (
 func bindRunFlags(flags *pflag.FlagSet, cfg *monitor.Config) {
 	flags.StringVar(&cfg.NetworkFile, "network-file", cfg.NetworkFile, "The path to the network file e.g path/network.json")
 	flags.StringVar(&cfg.MonitoringAddr, "monitoring-addr", cfg.MonitoringAddr, "The address to bind the monitoring server")
+}
+
+func bindLoadGenFlags(flags *pflag.FlagSet, cfg *loadgen.Config) {
+	flags.StringVar(&cfg.ValidatorKeysGlob, "loadgen-validator-keys-glob", cfg.ValidatorKeysGlob, "Glob path to the validator keys used for self-delegation load generation. Only applicable to devnet and staging")
 }
