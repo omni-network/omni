e6bd2f4865a31a79d9c68d8bd78653701d4066d3
corver <corver@omni.network>
2024-04-04 15:06:24 +0200

    fix(halo/valsync): fix latest valset query

---
+++ halo/valsync/types/interfaces.go
@@ -0,0 +1,20 @@
+package types
+
+import (
+	"context"
+
+	atypes "github.com/omni-network/omni/halo/attest/types"
+
+	abci "github.com/cometbft/cometbft/abci/types"
+
+	stypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+)
+
+type AttestKeeper interface {
+	ListAttestationsFrom(ctx context.Context, chainID uint64, height uint64, max uint64) ([]*atypes.Attestation, error)
+}
+
+type StakingKeeper interface {
+	EndBlocker(ctx context.Context) ([]abci.ValidatorUpdate, error)
+	GetLastValidators(ctx context.Context) ([]stypes.Validator, error)
+}
