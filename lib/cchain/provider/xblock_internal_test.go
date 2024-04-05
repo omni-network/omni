2f47b5a92d1d5fea68d506ded8574c46d9b7eaf2
corver <corver@omni.network>
2024-04-05 11:36:07 +0200

    ci(e2e): fix instance equal (#774)
    Fix comparing e2e infra instances by only using IPs. Since ports are
    only defined for the one set, not the other.
    
    task: none

--- lib/cchain/provider/xblock_internal_test.go
+++ lib/cchain/provider/xblock_internal_test.go
@@ -3,9 +3,13 @@ package provider
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/omni-network/omni/lib/cchain"
 	"github.com/omni-network/omni/lib/tutil"
+
+	ctypes "github.com/cometbft/cometbft/rpc/core/types"
+	"github.com/cometbft/cometbft/types"
 
 	fuzz "github.com/google/gofuzz"
 	"github.com/stretchr/testify/require"
@@ -29,18 +33,30 @@ func TestXBlock(t *testing.T) {
 	var height uint64
 	f.Fuzz(&height)
 
-	valFunc := func(ctx context.Context, h uint64, _ bool) ([]cchain.Validator, uint64, bool, error) {
+	timestamp := time.Unix(1712312027, 0).UTC()
+
+	valFunc := func(ctx context.Context, h uint64, _ bool) (valSetResponse, bool, error) {
 		require.EqualValues(t, height, h)
 		var resp []cchain.Validator
 		f.Fuzz(&resp)
 
-		return resp, height, true, nil
+		return valSetResponse{
+			ValSetID:   height,
+			Validators: resp,
+		}, true, nil
 	}
 	chainFunc := func(ctx context.Context) (uint64, error) {
 		return 77, nil
 	}
+	headerFunc := func(ctx context.Context, h *int64) (*ctypes.ResultHeader, error) {
+		return &ctypes.ResultHeader{
+			Header: &types.Header{
+				Time: timestamp,
+			},
+		}, nil
+	}
 
-	prov := Provider{valset: valFunc, chainID: chainFunc}
+	prov := Provider{valset: valFunc, chainID: chainFunc, header: headerFunc}
 
 	block, ok, err := prov.XBlock(context.Background(), height, false)
 	require.NoError(t, err)
