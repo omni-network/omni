f97f9b8cac7c5dc2f1813685d479357ba1bdca62
Flannel007 <dan.flanagan93@gmail.com>
2024-04-11 10:10:29 -0500

    init

--- explorer/graphql/resolvers/messages_test.go
+++ explorer/graphql/resolvers/messages_test.go
@@ -85,3 +85,42 @@ func TestXMsgRange(t *testing.T) {
 		},
 	})
 }
+
+func TestXMsg(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	test := createGqlTest(t)
+	t.Cleanup(func() {
+		if err := test.Client.Close(); err != nil {
+			t.Error(err)
+		}
+	})
+	db.CreateTestBlocks(t, ctx, test.Client, 2)
+
+	gqltesting.RunTests(t, []*gqltesting.Test{
+		{
+			Context: ctx,
+			Schema:  graphql.MustParseSchema(app.Schema, &resolvers.Query{BlocksResolver: test.Resolver}, test.Opts...),
+			Query: `
+				{
+					xmsg(sourceChainID: 1, destChainID: 2, streamOffset: 0){
+						SourceMessageSender
+						TxHash
+						BlockHeight
+						BlockHash
+					}
+				}
+			`,
+			ExpectedResult: `
+			{
+				"xmsg":{
+					"BlockHash":"0x0000000000000000000000000103176f1b2d62675e370103176f1b2d62675e37",
+					"BlockHeight":"0x0",
+					"SourceMessageSender":"0x0102030405060708090a0b0c0d0e0f1011121314",
+					"TxHash":"0x0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20"
+				}
+			}
+			`,
+		},
+	})
+}
