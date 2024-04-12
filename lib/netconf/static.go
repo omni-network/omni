5cb5ae9ad79541e4ad61a3ef20345528d38e1c23
Kevin Halliday <kevin@omni.network>
2024-04-12 15:14:37 -0400

    chore(cli): add mainnet avs and proxy admin addrs (#843)
    Add mainnet avs address, and its proxy admin address.
    
    task: none

--- lib/netconf/static.go
+++ lib/netconf/static.go
@@ -59,13 +59,16 @@ func (s Static) PortalDeployment(chainID uint64) (Deployment, bool) {
 //nolint:gochecknoglobals // Static ID
 var runid = uuid.New().String()
 
-// Address matches lib/contracts.TestnetPortal() and lib/contracts.TestnetAVS().
-// We do not import to avoid cylic dependencies.
-//
 //nolint:gochecknoglobals // Static addresses
 var (
+	// Address matches lib/contracts.TestnetPortal() and lib/contracts.TestnetAVS().
+	// We do not import to avoid cylic dependencies.
 	testnetPortal = common.HexToAddress("0x71d510f4dc4e7E7716D03209c603C76F4398cF53")
 	testnetAVS    = common.HexToAddress("0xa7b2e7830C51728832D33421670DbBE30299fD92")
+
+	// This address DOES NOT match lib/contracts.MainnetAVS().
+	// This mainnet AVS was deployed outside of the e2e deployment flow, without Create3.
+	mainnetAVS = common.HexToAddress("0xed2f4d90b073128ae6769a9A8D51547B1Df766C8")
 )
 
 //nolint:gochecknoglobals // Static mappings.
@@ -103,6 +106,9 @@ var statics = map[ID]Static{
 				DeployHeight: 31688713,
 			},
 		},
+	},
+	Mainnet: {
+		AVSContractAddress: mainnetAVS,
 	},
 }
 
