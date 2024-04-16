3818ee245b6703a10d9bb3a3d5b6a32ba2eac627
Flannel007 <dan.flanagan93@gmail.com>
2024-04-16 09:05:13 -0500

    removing non SSR helpers

--- explorer/ui/app/components/queries/chains.tsx
+++ explorer/ui/app/components/queries/chains.tsx
@@ -1,26 +1,4 @@
-import { useQuery } from 'urql'
 import { graphql } from '~/graphql'
-import { Chain } from '~/graphql/graphql'
-
-export function GetSupportedChains(from: number, to: number): Chain[] {
-  const [result] = useQuery({
-    query: supportedchains,
-  })
-  const { data, fetching, error } = result
-
-  var rows: Chain[] = []
-
-  data?.supportedchains.map((chain: any) => {
-    let c = {
-      ChainID: chain.ChainID,
-      Name: chain.Name,
-    }
-
-    rows.push(c)
-  })
-
-  return rows
-}
 
 export const supportedchains = graphql(`
   query SupportedChains {
