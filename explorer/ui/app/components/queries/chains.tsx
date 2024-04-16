9318f527eecf6ae747bced6b3d616842e0ae4899
Flannel007 <dan.flanagan93@gmail.com>
2024-04-16 08:33:36 -0500

    Update chains.tsx

--- explorer/ui/app/components/queries/chains.tsx
+++ explorer/ui/app/components/queries/chains.tsx
@@ -2,7 +2,7 @@ import { useQuery } from 'urql'
 import { graphql } from '~/graphql'
 import { Chain } from '~/graphql/graphql'
 
-export function GetBlocksInRange(from: number, to: number): Chain[] {
+export function GetSupportedChains(from: number, to: number): Chain[] {
   const [result] = useQuery({
     query: supportedchains,
   })
