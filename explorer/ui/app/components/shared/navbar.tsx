6aec8aeede010b66d6aa877eead02392c6299998
Morne <morne@luckybeard.com>
2024-04-23 16:29:09 +0200

    Updated testnet/mainnet dropdown, fixed color issues, added redirects when net is chosen

--- explorer/ui/app/components/shared/navbar.tsx
+++ explorer/ui/app/components/shared/navbar.tsx
@@ -3,6 +3,9 @@ import logo from '../../../public/Horizontal_Word Black_Symbol Black.png'
 import Logo from './logo'
 import { Link } from '@remix-run/react'
 import ExplorerDropdown from './explorerDropdown'
+import Dropdown from './dropdown'
+import { useLocation } from '@remix-run/react'
+import { useEffect, useState } from 'react'
 
 export default function Navbar() {
   const evmExplorerLinks = [
@@ -13,12 +16,42 @@ export default function Navbar() {
     },
   ]
 
+  const location = useLocation()
+
+  const [currentNet, setCurrentNet] = useState<'testnet' | 'mainnet'>('testnet')
+
+  useEffect(() => {
+    console.log(window.location)
+
+    if (window.location.host.includes('test')) {
+      setCurrentNet('testnet')
+    } else if (window.location.host.includes('main')) {
+      setCurrentNet('mainnet')
+    }
+  }, [])
+
   return (
     <header className="static border-none py-3 px-8">
       <div className="w-full flex items-center gap-8">
         <Link to="/" className="m-auto">
           <Logo />
         </Link>
+
+        <Dropdown
+          onChange={value => {
+            if (value === 'testnet') {
+              window.location.replace('https://explorer.testnet.omni.network')
+            }
+            if (value === 'mainnet') {
+              window.location.replace('https://explorer.mainnet.omni.network')
+            }
+          }}
+          defaultValue={currentNet}
+          options={[
+            { display: 'Testnet', value: 'testnet' },
+            { display: 'Mainnet', value: 'mainnet' },
+          ]}
+        />
 
         <div className="grow"></div>
 
@@ -31,13 +64,6 @@ export default function Navbar() {
         <ExplorerDropdown />
         <div className="flex">
           <ThemeButton />
-        </div>
-
-        <div
-          className={`flex gap-2 items-center justify-center border-[1px] border-border-default px-4 py-[14px] rounded-full`}
-        >
-          <div className="w-3 h-3 rounded-full bg-positive"></div>
-          <span className={`text-default `}>Testnet</span>
         </div>
       </div>
     </header>
