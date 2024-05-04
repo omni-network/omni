027de9077bc58fe887c00305e5347defa53539f6
Dennis <dennis@omni.network>
2024-05-05 00:42:52 +0200

    feat(docs): refactor

--- docs/site/src/components/GitHubCodeBlock/GitHubCodeBlock.tsx
+++ docs/site/src/components/GitHubCodeBlock/GitHubCodeBlock.tsx
@@ -9,65 +9,76 @@ const GitHubCodeBlock = ({ repoUrl }) => {
   const [code, setCode] = useState('');
   const [language, setLanguage] = useState('plaintext');
   const [sourceUrl, setSourceUrl] = useState('');
-  const [mainUrl , setMainUrl] = useState('');
+  const [mainUrl, setMainUrl] = useState('');
   const [isCodeMatch, setCodeMatch] = useState(true);
 
   useEffect(() => {
-    async function fetchCode() {
-
-      const regex = /(https:\/\/github\.com\/)([^\/]+)\/([^\/]+)\/blob\/([^\/]+)\/([^#]+)#L(\d+)-L(\d+)/;
-      var match = repoUrl.match(regex);
-      if (match) {
-        // check if the URL is a permalink including a hex, if not then throw an error
-        if (match[4].length !== 40) {
-          console.error("Invalid URL: Permalink with lines URL should include a commit hash.");
-          setCode("Error: Invalid GitHub Permalink Lines URL format.");
-          return;
-        }
-      }
-      if (!match) {
-        const noLinesRegex = /(https:\/\/github\.com\/)([^\/]+)\/([^\/]+)\/blob\/([^\/]+)\/([^#]+)/;
-        match = repoUrl.match(noLinesRegex);
-      }
-
-      if (match) {
-        const [wholeUrl, site, owner, repo, branch, filePath, startLine, endLine] = match;
-        // Build the source URL to view on GitHub, including line numbers if available
-        setSourceUrl(`https://github.com/${owner}/${repo}/blob/${branch}/${filePath}${startLine ? `#L${startLine}-L${endLine}` : ''}`);
-        // Fetch the raw content from GitHub
-        const rawUrl = `https://raw.githubusercontent.com/${owner}/${repo}/${branch}/${filePath}` + (startLine && endLine ? `#L${startLine}-L${endLine}` : '');
-
-        try {
-          const response = await axios.get(rawUrl);
-          const allLines = response.data.split('\n');
-          const lines = (startLine && endLine) ? allLines.slice(parseInt(startLine) - 1, parseInt(endLine)).join('\n') : allLines.join('\n');
-
-          // Fetch the raw content from the main branch
-          const mainRawUrl = `https://raw.githubusercontent.com/${owner}/${repo}/main/${filePath}`;
-          setMainUrl(`https://github.com/${owner}/${repo}/blob/main/${filePath}`);
-          const mainResponse = await axios.get(mainRawUrl);
-          const mainAllLines = mainResponse.data.split('\n');
-          const mainLines = (startLine && endLine) ? mainAllLines.slice(parseInt(startLine) - 1, parseInt(endLine)).join('\n') : mainAllLines.join('\n');
-
-          // Compare the lines from the specified branch and the main branch
-          if (lines !== mainLines) {
-            setCodeMatch(false);
-          }
-
-          setCode(lines);
-          setLanguage(determineLanguage(filePath));
-        } catch (error) {
-          console.error('Error fetching code:', error.response ? error.response.data : error.message);
-          setCode(`Error: ${error.response ? error.response.data : "Could not fetch file"}`);
-        }
-      } else {
-        console.error("Regex match failed. Check the URL format.");
-        setCode("Error: Invalid GitHub URL format.");
-      }
-    }
-
     fetchCode();
   }, [repoUrl]);
+
+  async function fetchCode() {
+    if (!repoUrl) {
+      console.error("No URL provided.");
+      setCode("Error: No URL provided.");
+      return;
+    }
+
+    var match = matchUrl(repoUrl);
+    if (!match) {
+      return;
+    }
+
+    const [wholeUrl, site, owner, repo, branch, filePath, startLine, endLine] = match;
+
+    const rawUrl = `https://raw.githubusercontent.com/${owner}/${repo}/${branch}/${filePath}` + (startLine && endLine ? `#L${startLine}-L${endLine}` : ''); // Fetch the raw content from GitHub
+    const mainRawUrl = `https://raw.githubusercontent.com/${owner}/${repo}/main/${filePath}`; // main branch raw content URL
+    setSourceUrl(`https://github.com/${owner}/${repo}/blob/${branch}/${filePath}` + (startLine && endLine ? `#L${startLine}-L${endLine}` : '')); // Build the source URL to view on GitHub, including line numbers if available
+    setMainUrl(`https://github.com/${owner}/${repo}/blob/main/${filePath}`); // main branch source URL
+
+    try {
+      const response = await axios.get(rawUrl); // Fetch the raw content from the specified commit hash
+      const lines = extractLines(response, startLine, endLine);
+
+      const mainResponse = await axios.get(mainRawUrl); // Fetch the raw content from the main branch
+      const mainLines = extractLines(mainResponse, startLine, endLine);
+
+      if (lines !== mainLines) { // Check if the code matches the main branch
+        setCodeMatch(false);
+      }
+
+      setCode(lines);
+      setLanguage(determineLanguage(filePath));
+    } catch (error) {
+      console.error('Error fetching code:', error.response ? error.response.data : error.message);
+      setCode(`Error: ${error.response ? error.response.data : "Could not fetch file"}`);
+    }
+  }
+
+  function extractLines(mainResponse, startLine: string, endLine: string) {
+    const allLines = mainResponse.data.split('\n');
+    const lines = (startLine && endLine) ? allLines.slice(parseInt(startLine) - 1, parseInt(endLine)).join('\n') : allLines.join('\n');
+    return lines;
+  }
+
+  function matchUrl(url: string) {
+    const permalinkLinesRegex = /(https:\/\/github\.com\/)([^\/]+)\/([^\/]+)\/blob\/([^\/]+)\/([^#]+)#L(\d+)-L(\d+)/;
+    const repoFileRegex = /(https:\/\/github\.com\/)([^\/]+)\/([^\/]+)\/blob\/([^\/]+)\/([^#]+)/;
+    let match = url.match(permalinkLinesRegex);
+    if (match) {
+      if (match[4].length !== 40) {
+        console.error("Invalid URL: Permalink with lines URL should include a commit hash.");
+        setCode("Error: Invalid GitHub Permalink Lines URL format.");
+        return null;
+      }
+      return match;
+    }
+    match = url.match(repoFileRegex);
+    if (!match) {
+      console.error("Invalid URL: Check the URL format.");
+      setCode("Error: Invalid GitHub URL format.");
+    }
+    return match;
+  }
 
   function determineLanguage(filePath) {
     const extension = filePath.slice(filePath.lastIndexOf('.'));
@@ -111,7 +122,7 @@ const GitHubCodeBlock = ({ repoUrl }) => {
         </div>
       }
       <div className="code-snippet-footer">
-        <a href={sourceUrl} target="_blank" rel="noopener noreferrer">
+        <a href={sourceUrl}>
           See source on GitHub <img src={getGitHubIcon()} alt="GitHub" />
         </a>
       </div>
