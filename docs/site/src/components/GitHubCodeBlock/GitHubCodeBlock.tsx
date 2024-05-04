ab2efb5065d78bb0cd68b8e5e053ffa5c6a7b704
Dennis <dennis@omni.network>
2024-05-05 00:00:32 +0200

    feat(docs): add warnings for codeblocks with lines

--- docs/site/src/components/GitHubCodeBlock/GitHubCodeBlock.tsx
+++ docs/site/src/components/GitHubCodeBlock/GitHubCodeBlock.tsx
@@ -9,12 +9,22 @@ const GitHubCodeBlock = ({ repoUrl }) => {
   const [code, setCode] = useState('');
   const [language, setLanguage] = useState('plaintext');
   const [sourceUrl, setSourceUrl] = useState('');
+  const [mainUrl , setMainUrl] = useState('');
+  const [isCodeMatch, setCodeMatch] = useState(true);
 
   useEffect(() => {
     async function fetchCode() {
-      // Use a single regex that can optionally capture line numbers
+
       const regex = /(https:\/\/github\.com\/)([^\/]+)\/([^\/]+)\/blob\/([^\/]+)\/([^#]+)#L(\d+)-L(\d+)/;
       var match = repoUrl.match(regex);
+      if (match) {
+        // check if the URL is a permalink including a hex, if not then throw an error
+        if (match[4].length !== 40) {
+          console.error("Invalid URL: Permalink with lines URL should include a commit hash.");
+          setCode("Error: Invalid GitHub Permalink Lines URL format.");
+          return;
+        }
+      }
       if (!match) {
         const noLinesRegex = /(https:\/\/github\.com\/)([^\/]+)\/([^\/]+)\/blob\/([^\/]+)\/([^#]+)/;
         match = repoUrl.match(noLinesRegex);
@@ -30,8 +40,20 @@ const GitHubCodeBlock = ({ repoUrl }) => {
         try {
           const response = await axios.get(rawUrl);
           const allLines = response.data.split('\n');
-          // Apply line slicing only if line numbers are provided and valid
           const lines = (startLine && endLine) ? allLines.slice(parseInt(startLine) - 1, parseInt(endLine)).join('\n') : allLines.join('\n');
+
+          // Fetch the raw content from the main branch
+          const mainRawUrl = `https://raw.githubusercontent.com/${owner}/${repo}/main/${filePath}`;
+          setMainUrl(`https://github.com/${owner}/${repo}/blob/main/${filePath}`);
+          const mainResponse = await axios.get(mainRawUrl);
+          const mainAllLines = mainResponse.data.split('\n');
+          const mainLines = (startLine && endLine) ? mainAllLines.slice(parseInt(startLine) - 1, parseInt(endLine)).join('\n') : mainAllLines.join('\n');
+
+          // Compare the lines from the specified branch and the main branch
+          if (lines !== mainLines) {
+            setCodeMatch(false);
+          }
+
           setCode(lines);
           setLanguage(determineLanguage(filePath));
         } catch (error) {
@@ -81,6 +103,13 @@ const GitHubCodeBlock = ({ repoUrl }) => {
   return (
     <div className="code-snippet-container">
       <CodeBlock language={language} className="code-snippet-block">{code}</CodeBlock>
+      {!isCodeMatch &&
+        <div className="code-snippet-warning">
+          <a href={mainUrl}>
+            <strong>Warning: </strong>Code shown does not match the main branch. Please visit the repository URL for actual code.
+          </a>
+        </div>
+      }
       <div className="code-snippet-footer">
         <a href={sourceUrl} target="_blank" rel="noopener noreferrer">
           See source on GitHub <img src={getGitHubIcon()} alt="GitHub" />
@@ -88,6 +117,6 @@ const GitHubCodeBlock = ({ repoUrl }) => {
       </div>
     </div>
   );
-};
+}
 
 export default GitHubCodeBlock;
