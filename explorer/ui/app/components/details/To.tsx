ca68e5ec8bd4b39d67d53913c37ddd68e21ebce8
CaidynLuckyBeard <caidyn@luckybeard.com>
2024-05-09 09:14:11 +0200

    feat(explorer/ui) xmsg view with hookup to BE

--- explorer/ui/app/components/details/To.tsx
+++ explorer/ui/app/components/details/To.tsx
@@ -1,50 +1,51 @@
 import { Link } from '@remix-run/react'
 import React from 'react'
-import { hashShortener } from '~/lib/formatting'
+import { dateFormatter, hashShortener } from '~/lib/formatting'
+import { dateFormatterXMsgPage } from './date.service';
 
-export const To = () => {
+export const To = ({ xMsgDetails }) => {
   return (
     <>
     <h5 className="text-default my-5">To</h5>
-    {/* Offset */}
+    {/* Destination Chain */}
     <div className="flex mt-5 pb-2 border-b-2 border-gray-500 border-solid">
-        <p className="w-48">Destination Chain</p>
+        <p className="w-[150px] sm:w-48">Destination Chain</p>
         <p>
-          <span className="text-default">Optimism </span> (356)
+          <span className="text-default">Optimism</span>(356)
         </p>
       </div>
-      {/* Offset */}
+      {/* Block Timestamp */}
       <div className="flex mt-5 pb-2 border-b-2 border-gray-500 border-solid">
-        <p className="w-48">Block Timestamp</p>
-        <p>
-          <span className="text-default">16 mins ago </span> (Apr 25 2024 00:05:23 AM +UTC)
+        <p className="w-[150px] sm:w-48">Block Timestamp</p>
+        <p className='flex-1'>
+          <span className="text-default"></span> {dateFormatterXMsgPage(new Date(xMsgDetails.block.timestamp))}
         </p>
       </div>
-       {/* Offset */}
+       {/* Destination Address */}
        <div className="flex mt-5 pb-2 border-b-2 border-gray-500 border-solid">
-        <p className="w-48">Source Address</p>
-        <Link to="/" className="underline text-indigo-400 hidden lg:block">
-          0x109f40f806567158aaad05e43afe240cf394608cacd0016466dfb24dce2927d4
+        <p className="w-[150px] sm:w-48">Destination Address</p>
+        <Link target='_blank' to={xMsgDetails.toUrl} className="underline text-indigo-400 hidden lg:block">
+          {xMsgDetails.to}
         </Link>
-        <Link to="/" className="underline text-indigo-400 block lg:hidden">
-          {hashShortener('0x109f40f806567158aaad05e43afe240cf394608cacd0016466dfb24dce2927d4')}
-        </Link>
-      </div>
-      {/* Offset */}
-      <div className="flex mt-5 pb-2 border-b-2 border-gray-500 border-solid">
-        <p className="w-48">Tx Hash</p>
-        <Link to="/" className="underline text-indigo-400 hidden lg:block">
-          0x109f40f806567158aaad05e43afe240cf394608cacd0016466dfb24dce2927d4
-        </Link>
-        <Link to="/" className="underline text-indigo-400 block lg:hidden">
-          {hashShortener('0x109f40f806567158aaad05e43afe240cf394608cacd0016466dfb24dce2927d4')}
+        <Link target='_blank' to={xMsgDetails.toUrl} className="underline text-indigo-400 block lg:hidden">
+          {hashShortener(xMsgDetails.to)}
         </Link>
       </div>
       {/* Offset */}
       <div className="flex mt-5 pb-2 border-b-2 border-gray-500 border-solid">
-        <p className="w-48">Gas Limit</p>
+        <p className="w-[150px] sm:w-48">Tx Hash</p>
+        <Link target='_blank' to={xMsgDetails.txHashUrl} className="underline text-indigo-400 hidden lg:block">
+          {xMsgDetails.receipt.txHash}
+        </Link>
+        <Link target='_blank' to={xMsgDetails.txHashUrl} className="underline text-indigo-400 block lg:hidden">
+          {hashShortener(xMsgDetails.receipt.txHash)}
+        </Link>
+      </div>
+      {/* Gas Limit */}
+      <div className="flex mt-5 pb-2 border-b-2 border-gray-500 border-solid">
+        <p className="w-[150px] sm:w-48">Gas Limit</p>
         <p className="text-default">
-          30,009
+          {xMsgDetails.gasLimit}
         </p>
       </div>
     </>
