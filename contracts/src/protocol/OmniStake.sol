3fde6039cb2d5232f728b1532783a99d2b760bf3
Kevin Halliday <kevin@omni.network>
2024-05-15 23:49:59 +0200

    feat(contracts): allow omni stakers to register an indepdent val pubkey

--- contracts/src/protocol/OmniStake.sol
+++ contracts/src/protocol/OmniStake.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity =0.8.24;
 
+import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import { Secp256k1 } from "../libraries/Secp256k1.sol";
 
 /**
@@ -9,21 +10,110 @@ import { Secp256k1 } from "../libraries/Secp256k1.sol";
  */
 contract OmniStake {
     /**
-     * @notice Emitted when a user deposits funds into the contract.
-     * @param pubkey    64 byte uncompressed secp256k1 public key (no 0x04 prefix)
-     * @param amount    Funds deposited
+     * @notice Emitted when a registers a validator key with some stake.
+     * @param delegator         Address of the delegator, the account funding the deposit on behalf of
+     *                          their validator key.
+     * @param validatorPubKey   64 byte uncompressed secp256k1 validator public key (no 0x04 prefix)
+     * @param deposit           Funds deposited
      */
-    event Deposit(bytes pubkey, uint256 amount);
+    event ValidatorRegistered(address delegator, bytes validatorPubKey, uint256 deposit);
 
     /**
-     * @notice Deposit OMNI. This is the entry point for validator staking. The consensus chain is
-     *         notified of the deposit, and manages stake accounting / validator onboarding.
-     * @param pubkey 64 byte uncompressed secp256k1 public key (no 0x04 prefix)
+     * @notice Emitted when a delegator increases their stake.
+     * @param delegator         The delegator's address
+     * @param amount            The amount of deposited
      */
-    function deposit(bytes memory pubkey) external payable {
-        require(msg.value >= 1 ether, "OmniStake: deposit amt too low");
-        require(msg.value < type(uint64).max, "OmniStake: deposit amt too high");
-        require(msg.sender == Secp256k1.pubkeyToAddress(pubkey), "OmniStake: pubkey not sender");
-        emit Deposit(pubkey, msg.value);
+    event Deposit(address delegator, uint256 amount);
+
+    /**
+     * @notice The minimum deposit required to register a validator.
+     */
+    uint256 public constant MIN_REGISTER_DEPOSIT = 100 ether;
+
+    /**
+     * @notice The minimum deposit required to delegate to a validator..
+     */
+    uint256 public constant MIN_DEPOSIT = 1 ether;
+
+    /**
+     * @notice The EIP-712 typehash for the contract's domain
+     */
+    bytes32 public constant DOMAIN_TYPEHASH =
+        keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
+
+    /**
+     * @notice The EIP-712 typehash for the validator registration struct
+     */
+    bytes32 public constant VALIDATOR_REGISTRATION_TYPEHASH =
+        keccak256("ValidatorRegistration(address delegator,bytes validatorPubKey,bytes32 salt,uint256 expiry)");
+
+    struct SignatureWithSaltAndExpiry {
+        bytes signature;
+        bytes32 salt;
+        uint256 expiry;
+    }
+
+    /**
+     * @notice Tracks spent salts per each validator key.
+     */
+    mapping(bytes => mapping(bytes32 => bool)) internal _isValSaltSpent;
+
+    /**
+     * @notice Deposit OMNI and register a validator key.
+     * @param validatorPubKey       64 byte uncompressed secp256k1 validator public key (no 0x04 prefix)
+     * @param validatorSignature    Signature validatorRegistrationDigestHash() with salt and expiry
+     */
+    function register(bytes calldata validatorPubKey, SignatureWithSaltAndExpiry calldata validatorSignature)
+        external
+        payable
+    {
+        require(msg.value >= MIN_REGISTER_DEPOSIT, "OmniStake: deposit below min");
+        require(validatorSignature.expiry >= block.timestamp, "OmniStake: expired signature");
+        require(!_isValSaltSpent[validatorPubKey][validatorSignature.salt], "OmniStake: spent salt");
+        require(
+            ECDSA.recover(
+                validatorRegistrationDigestHash(
+                    msg.sender, validatorPubKey, validatorSignature.salt, validatorSignature.expiry
+                ),
+                validatorSignature.signature
+            ) == Secp256k1.pubkeyToAddress(validatorPubKey),
+            "OmniStake: invalid val signature"
+        );
+
+        _isValSaltSpent[validatorPubKey][validatorSignature.salt] = true;
+
+        emit ValidatorRegistered(msg.sender, validatorPubKey, msg.value);
+    }
+
+    /**
+     * @notice Delegate additional OMNI stake to an existing validator key.
+     *         If the delegator (msg.sender) has not registered a validator key, the deposit is lost.
+     */
+    function deposit() external payable {
+        require(msg.value >= MIN_DEPOSIT, "OmniStake: deposit below min");
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    /**
+     * @notice Returns the digest hash to be signed by an opeator's validator key on registration.
+     * @param delegator     The delegator's ethereum address
+     * @param valPubKey     The delegator's 64 byte uncompressed secp256k1 validator public key
+     * @param salt          A salt unique to this registration
+     * @param expiry        The timestamp at which this registration expires
+     */
+    function validatorRegistrationDigestHash(address delegator, bytes calldata valPubKey, bytes32 salt, uint256 expiry)
+        public
+        view
+        returns (bytes32)
+    {
+        bytes32 structHash = keccak256(abi.encode(VALIDATOR_REGISTRATION_TYPEHASH, delegator, valPubKey, salt, expiry));
+        return keccak256(abi.encodePacked("\x19\x01", domainSeparator(), structHash));
+    }
+
+    /**
+     * @notice Domain separator for EIP-712 signatures.
+     */
+    function domainSeparator() public view returns (bytes32) {
+        return keccak256(abi.encode(DOMAIN_TYPEHASH, keccak256(bytes("OmniStake")), block.chainid, address(this)));
     }
 }
--- contracts/test/stake/OmniStake.t.sol
+++ contracts/test/stake/OmniStake.t.sol
@@ -9,8 +9,17 @@ import { Test, Vm } from "forge-std/Test.sol";
  * @notice Test suite for the OmniStake contract
  */
 contract OmniStake_Test is Test {
-    /// @notice Emitted when a user deposits into OmniStake
-    event Deposit(bytes pubkey, uint256 amount);
+    /**
+     * @notice Emitted when a registers a validator key with some stake.
+     * @dev Matches OmniStake.ValidatorRegistered event
+     */
+    event ValidatorRegistered(address delegator, bytes validatorPubKey, uint256 deposit);
+
+    /**
+     * @notice Emitted when a delegator increases their stake.
+     * @dev Matches OmniStake.Deposit event
+     */
+    event Deposit(address delegator, uint256 amount);
 
     OmniStake stake;
 
@@ -18,77 +27,192 @@ contract OmniStake_Test is Test {
         stake = new OmniStake();
     }
 
-    /// @dev Test that a valid deposit succeeds
-    function test_deposit_succeeds() public {
-        Vm.Wallet memory wallet = vm.createWallet("test val");
-        bytes memory pubkey = _pubkey(wallet);
+    /// @dev Test that a delegate can register a validator
+    function test_register_succeds() public {
+        Vm.Wallet memory val = vm.createWallet("val");
+        address delegator = makeAddr("delegator");
 
-        uint256 amt = 16 ether;
-        vm.deal(wallet.addr, amt);
+        uint256 deposit = stake.MIN_REGISTER_DEPOSIT() + 1 ether;
+        vm.deal(delegator, deposit);
+
+        bytes32 salt = keccak256("salt");
+        uint256 expiry = block.timestamp + 1 days;
+
+        bytes32 digestHash = stake.validatorRegistrationDigestHash(delegator, _pubkey(val), salt, expiry);
+
+        OmniStake.SignatureWithSaltAndExpiry memory sig =
+            OmniStake.SignatureWithSaltAndExpiry({ signature: _sign(val, digestHash), salt: salt, expiry: expiry });
 
         vm.expectEmit();
-        emit Deposit(pubkey, amt);
+        emit ValidatorRegistered(delegator, _pubkey(val), deposit);
 
-        vm.prank(wallet.addr);
-        stake.deposit{ value: amt }(pubkey);
+        vm.prank(delegator);
+        stake.register{ value: deposit }(_pubkey(val), sig);
     }
 
     /// @dev Test that the pubkey of a deposit must match the sender
-    function test_deposit_wrongPubkey_reverts() public {
-        Vm.Wallet memory wallet = vm.createWallet("test val");
+    function test_register_invalidSignature_reverts() public {
+        Vm.Wallet memory val = vm.createWallet("val");
+        address delegator = makeAddr("delegator");
 
-        Vm.Wallet memory wallet2 = vm.createWallet("test val2");
-        bytes memory pubkey2 = _pubkey(wallet2);
+        uint256 deposit = stake.MIN_REGISTER_DEPOSIT() + 1 ether;
+        vm.deal(delegator, deposit);
 
-        uint256 amt = 16 ether;
-        vm.deal(wallet.addr, amt);
+        bytes32 salt = keccak256("salt");
+        uint256 expiry = block.timestamp + 1 days;
 
-        vm.expectRevert("OmniStake: pubkey not sender");
-        vm.prank(wallet.addr);
-        stake.deposit{ value: amt }(pubkey2);
+        // use invalid digest
+        bytes32 digestHash = valRegDigestHash_wrongDomain(delegator, _pubkey(val), salt, expiry);
+
+        OmniStake.SignatureWithSaltAndExpiry memory sig =
+            OmniStake.SignatureWithSaltAndExpiry({ signature: _sign(val, digestHash), salt: salt, expiry: expiry });
+
+        vm.expectRevert("OmniStake: invalid val signature");
+        vm.prank(delegator);
+        stake.register{ value: deposit }(_pubkey(val), sig);
     }
 
-    /// @dev Test that a deposit with a pubkey of incorrect length reverts
-    function test_deposit_invalidLength_reverts() public {
-        Vm.Wallet memory wallet = vm.createWallet("test val");
-        bytes memory pubkey = bytes.concat(hex"04", _pubkey(wallet));
+    // @dev Test that a deposit with an invalid pubkey reverts
+    function test_register_invalidPubKey_reverts() public {
+        Vm.Wallet memory val = vm.createWallet("test val");
+        address delegator = makeAddr("delegator");
 
-        uint256 amt = 16 ether;
-        vm.deal(wallet.addr, amt);
+        uint256 deposit = stake.MIN_REGISTER_DEPOSIT() + 1 ether;
+        vm.deal(delegator, deposit);
+
+        bytes32 salt = keccak256("salt");
+        uint256 expiry = block.timestamp + 1 days;
+
+        // use invalid digest
+        bytes32 digestHash = valRegDigestHash_wrongDomain(delegator, _pubkey(val), salt, expiry);
+
+        OmniStake.SignatureWithSaltAndExpiry memory sig =
+            OmniStake.SignatureWithSaltAndExpiry({ signature: _sign(val, digestHash), salt: salt, expiry: expiry });
+
+        bytes memory invalidPubKey = bytes.concat(hex"04", _pubkey(val));
 
         vm.expectRevert("Secp256k1: invalid pubkey length");
-        vm.prank(wallet.addr);
-        stake.deposit{ value: amt }(pubkey);
+        vm.prank(delegator);
+        stake.register{ value: deposit }(invalidPubKey, sig);
     }
 
-    /// @dev Test that a deposit below 1 ether reverts
-    function test_deposit_below1Ether_reverts() public {
-        Vm.Wallet memory wallet = vm.createWallet("test val");
-        bytes memory pubkey = _pubkey(wallet);
+    /// @dev Test that using a validator signature with a spent salt reverts
+    function test_register_spentSalt_reverts() public {
+        // register once
+        Vm.Wallet memory val = vm.createWallet("val");
+        address delegator = makeAddr("delegator");
 
-        uint256 amt = 1 ether - 1;
-        vm.deal(wallet.addr, amt);
+        uint256 deposit = stake.MIN_REGISTER_DEPOSIT() + 1 ether;
+        vm.deal(delegator, deposit);
 
-        vm.expectRevert("OmniStake: deposit amt too low");
-        vm.prank(wallet.addr);
-        stake.deposit{ value: amt }(pubkey);
+        bytes32 salt = keccak256("salt");
+        uint256 expiry = block.timestamp + 1 days;
+
+        bytes32 digestHash = stake.validatorRegistrationDigestHash(delegator, _pubkey(val), salt, expiry);
+
+        OmniStake.SignatureWithSaltAndExpiry memory sig =
+            OmniStake.SignatureWithSaltAndExpiry({ signature: _sign(val, digestHash), salt: salt, expiry: expiry });
+
+        vm.expectEmit();
+        emit ValidatorRegistered(delegator, _pubkey(val), deposit);
+
+        vm.prank(delegator);
+        stake.register{ value: deposit }(_pubkey(val), sig);
+
+        // register again, with a new delegator
+        delegator = makeAddr("delegator2");
+        vm.deal(delegator, deposit);
+
+        vm.expectRevert("OmniStake: spent salt");
+        stake.register{ value: deposit }(_pubkey(val), sig);
     }
 
-    /// @dev Test that a deposit above max uint64 reverts
-    function test_deposit_aboveMaxUint64_reverts() public {
-        Vm.Wallet memory wallet = vm.createWallet("test val");
-        bytes memory pubkey = _pubkey(wallet);
+    /// @dev Test that registering with an expired signature reverts
+    function test_register_signatureExpired_reverts() public {
+        Vm.Wallet memory val = vm.createWallet("val");
+        address delegator = makeAddr("delegator");
 
-        uint256 amt = uint256(type(uint64).max) + 1;
-        vm.deal(wallet.addr, amt);
+        uint256 deposit = stake.MIN_REGISTER_DEPOSIT() + 1 ether;
+        vm.deal(delegator, deposit);
 
-        vm.expectRevert("OmniStake: deposit amt too high");
-        vm.prank(wallet.addr);
-        stake.deposit{ value: amt }(pubkey);
+        bytes32 salt = keccak256("salt");
+        uint256 expiry = block.timestamp + 1 days;
+
+        // warp ahead of expiry
+        vm.warp(block.timestamp + expiry + 1 days);
+
+        bytes32 digestHash = stake.validatorRegistrationDigestHash(delegator, _pubkey(val), salt, expiry);
+
+        OmniStake.SignatureWithSaltAndExpiry memory sig =
+            OmniStake.SignatureWithSaltAndExpiry({ signature: _sign(val, digestHash), salt: salt, expiry: expiry });
+
+        vm.expectRevert("OmniStake: expired signature");
+        vm.prank(delegator);
+        stake.register{ value: deposit }(_pubkey(val), sig);
+    }
+
+    /// @dev Test that a valid deposit succeeds
+    function test_deposit_succeeds() public {
+        address delegator = makeAddr("delegator");
+        uint256 amt = 16 ether;
+        vm.deal(delegator, amt);
+
+        vm.expectEmit();
+        emit Deposit(delegator, amt);
+
+        // NOTE: the consensus chain verifies if the delegator has an active validator
+        // if they do not, the deposit is lost. But stake.deposit() will succeed regardless
+
+        vm.prank(delegator);
+        stake.deposit{ value: amt }();
+    }
+
+    /// @dev Test that a deposit below OmniStake.MIN_DEPOSIT reverts
+    function test_deposit_belowMinimum_reverts() public {
+        address delegator = makeAddr("delegator");
+        uint256 amt = stake.MIN_DEPOSIT() - 1;
+        vm.deal(delegator, amt);
+
+        vm.expectRevert("OmniStake: deposit below min");
+        vm.prank(delegator);
+        stake.deposit{ value: amt }();
     }
 
     /// @dev Helper function to convert a wallet to a secp256k1 64 uncompressed public key (without 0x04 prefix)
-    function _pubkey(Vm.Wallet memory wallet) internal pure returns (bytes memory) {
-        return abi.encode(wallet.publicKeyX, wallet.publicKeyY);
+    function _pubkey(Vm.Wallet memory w) internal pure returns (bytes memory) {
+        return abi.encode(w.publicKeyX, w.publicKeyY);
+    }
+
+    /// @dev Helper to sign digest hash with a wallet
+    function _sign(Vm.Wallet memory w, bytes32 hash) internal pure returns (bytes memory) {
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(w.privateKey, hash);
+        return abi.encodePacked(r, s, v);
+    }
+
+    /**
+     * Utils repuposed from OmniStake.sol
+     */
+
+    /// @dev Matches OmniStake.DOMAIN_TYPEHASH
+    bytes32 public constant DOMAIN_TYPEHASH =
+        keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
+
+    /// @dev Matches OmniStake.VALIDATOR_REGISTRATION_TYPEHASH
+    bytes32 public constant VALIDATOR_REGISTRATION_TYPEHASH =
+        keccak256("ValidatorRegistration(address delegator,bytes validatorPubKey,bytes32 salt,uint256 expiry)");
+
+    /// @dev Differs from OmniStake.validatorRegistrationDigestHash(), because domainSeparator() is different
+    function valRegDigestHash_wrongDomain(address delegator, bytes memory valPubKey, bytes32 salt, uint256 expiry)
+        public
+        view
+        returns (bytes32)
+    {
+        bytes32 structHash = keccak256(abi.encode(VALIDATOR_REGISTRATION_TYPEHASH, delegator, valPubKey, salt, expiry));
+        return keccak256(abi.encodePacked("\x19\x01", domainSeparator(), structHash));
+    }
+
+    // @dev Differs from OmniStake.domainSeparator() because uses address(this)
+    function domainSeparator() public view returns (bytes32) {
+        return keccak256(abi.encode(DOMAIN_TYPEHASH, keccak256(bytes("OmniStake")), block.chainid, address(this)));
     }
 }
