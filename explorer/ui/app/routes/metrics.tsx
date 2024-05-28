8bd63908c38c902c24330a866d804882545610d4
Pavel Nikolov <me@pavelnikolov.net>
2024-05-28 22:38:57 +0300

    add up gauge to explorer ui

--- explorer/ui/app/routes/metrics.tsx
+++ explorer/ui/app/routes/metrics.tsx
@@ -3,7 +3,11 @@
  */
 
 import type { LoaderFunction } from '@remix-run/node'
-import { register } from 'prom-client'
+import { register, Gauge  } from 'prom-client'
+
+const gauge = new Gauge({ name: 'up', help: 'up is always set to 1 when the application is running' })
+gauge.set(1)
+register.registerMetric(gauge)
 
 export const loader: LoaderFunction = async () => {
   const data = await register.metrics()
