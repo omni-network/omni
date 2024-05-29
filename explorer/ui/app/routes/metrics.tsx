e61a44a274f91646628762e91d31490d08cef257
Pavel Nikolov <me@pavelnikolov.net>
2024-05-29 07:42:22 +0300

    use empty metrics registry

--- explorer/ui/app/routes/metrics.tsx
+++ explorer/ui/app/routes/metrics.tsx
@@ -1,15 +1,14 @@
-/**
- * Fetch application metrics.
- */
-
 import type { LoaderFunction } from '@remix-run/node'
-import { register, Gauge  } from 'prom-client'
+import { Gauge, Registry } from 'prom-client'
+
+// Create a new blank registry to register the metrics not to expose system metrics
+const reg = new Registry()
 
 const gauge = new Gauge({ name: 'up', help: 'up is always set to 1 when the application is running' })
 gauge.set(1)
-register.registerMetric(gauge)
+reg.registerMetric(gauge)
 
 export const loader: LoaderFunction = async () => {
-  const data = await register.metrics()
+  const data = await reg.metrics()
   return new Response(data)
 }
