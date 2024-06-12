ff70c6296ba069e5a0d06c0fbe0a305f81f43aa8
Dennis <dennis@omni.network>
2024-06-12 12:56:08 +0200

    chore: skip lint in clone cmd

--- cli/cmd/develop.go
+++ cli/cmd/develop.go
@@ -60,7 +60,7 @@ func newForgeProjectTemplate(ctx context.Context, cfg developerForgeProjectConfi
 	}
 
 	// Clone the repository
-	//nolint:gosec
+	//nolint:gosec // This is a command-line tool, not a library
 	cmd := exec.CommandContext(ctx, "git", "clone", "--depth", "1", "https://github.com/omni-network/"+cfg.templateName+".git", destinationPath)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
