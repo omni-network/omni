5d048b90202fab989b7eb6316afefd3e20e369a4
Pavel Nikolov <pavelnikolov@users.noreply.github.com>
2024-06-27 09:24:40 +0200

    chore(lib/netconf): add portal address to static (#1296)
    Add portal address to neconf static.
    
    task: none

--- lib/netconf/static.go
+++ lib/netconf/static.go
@@ -121,8 +121,9 @@ var runid = uuid.New().String()
 //nolint:gochecknoglobals // Static addresses
 var (
 	// Address matches lib/contracts. We do not import to avoid cylic dependencies. Equivalence asserted in tests.
-	omegaAVS   = common.HexToAddress("0xa7b2e7830C51728832D33421670DbBE30299fD92")
-	mainnetAVS = common.HexToAddress("0xed2f4d90b073128ae6769a9A8D51547B1Df766C8")
+	omegaAVS    = common.HexToAddress("0xa7b2e7830C51728832D33421670DbBE30299fD92")
+	mainnetAVS  = common.HexToAddress("0xed2f4d90b073128ae6769a9A8D51547B1Df766C8")
+	omegaPortal = common.HexToAddress("0x602f8a77337d22DC13EAd94fFf23024f27899Ae9")
 )
 
 //nolint:gochecknoglobals // Static mappings.
@@ -151,7 +152,11 @@ var statics = map[ID]Static{
 		AVSContractAddress:   omegaAVS,
 		OmniExecutionChainID: evmchain.IDOmniOmega,
 		MaxValidators:        maxValidators,
-		Portals:              []Deployment{},
+		Portals: []Deployment{
+			{ChainID: evmchain.IDHolesky, Address: omegaPortal, DeployHeight: 1812518},
+			{ChainID: evmchain.IDOpSepolia, Address: omegaPortal, DeployHeight: 13805229},
+			// add arb sepolia when available
+		},
 	},
 	Mainnet: {
 		Network:            Mainnet,
