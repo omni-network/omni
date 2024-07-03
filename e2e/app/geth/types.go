c38e9fe36402b2b8a3f4a9f4bba72b6674b65a02
corver <corver@omni.network>
2024-07-03 15:22:12 +0200

    chore(deps): upgrade geth to v1 14 6 (#1347)
    Replaces https://github.com/omni-network/omni/pull/1337
    
    No noteworthy changes in this version for us.
    
    issue: none

--- e2e/app/geth/types.go
+++ e2e/app/geth/types.go
@@ -14,7 +14,7 @@ import (
 )
 
 // Version defines the geth version deployed to all networks.
-const Version = "v1.14.5"
+const Version = "v1.14.6"
 
 // Config is the configurable options for the standard omni geth config.
 type Config struct {
