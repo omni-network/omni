1d0ed4589c81dd40d9139f5c6c3d1dc0d6dc971d
corver <corver@omni.network>
2024-07-10 18:22:25 +0200

    chore(halo/genutil): disable cometBFT max block bytes (#1389)
    Explicitly disable cometBFT block max bytes. This effectively increases
    maximum block size from 21MB (default) to 100MB.
    
    We MUST include the EVM block, which is limited to `MaxGas` (30M), not
    bytes. In practice, 30M gas results in +-3MB blocks.
    
    issue: none

--- halo/genutil/defaults.go
+++ halo/genutil/defaults.go
@@ -7,6 +7,7 @@ func DefaultConsensusParams() *types.ConsensusParams {
 	resp := types.DefaultConsensusParams()
 	resp.ABCI.VoteExtensionsEnableHeight = 1                             // Enable vote extensions from the start.
 	resp.Validator.PubKeyTypes = []string{types.ABCIPubKeyTypeSecp256k1} // Only k1 keys.
+	resp.Block.MaxBytes = -1                                             // Disable max block bytes, since we MUST include the whole EVM block, which is limited by max gas per block.
 
 	return resp
 }
