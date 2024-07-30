7de3307769d80d8bc76750055d7eb34d23de8cce
Kevin Halliday <kevin@omni.network>
2024-07-30 17:47:03 -0400

    chore(e2e): post omega deployment updates

--- lib/netconf/static.go
+++ lib/netconf/static.go
@@ -131,6 +131,7 @@ var runid = uuid.New().String()
 //nolint:gochecknoglobals // Static addresses
 var (
 	omegaAVS     = common.HexToAddress("0xa7b2e7830C51728832D33421670DbBE30299fD92")
+	omegaPortal  = common.HexToAddress("0x1ce5094EE8b861674D3aD0da116D90592193c7d9")
 	mainnetAVS   = common.HexToAddress("0xed2f4d90b073128ae6769a9A8D51547B1Df766C8")
 	mainnetToken = common.HexToAddress("0x36e66fbbce51e4cd5bd3c62b637eb411b18949d4")
 
@@ -181,7 +182,12 @@ var statics = map[ID]Static{
 		AVSContractAddress:   omegaAVS,
 		OmniExecutionChainID: evmchain.IDOmniOmega,
 		MaxValidators:        maxValidators,
-		Portals:              nil,
+		Portals: []Deployment{
+			{ChainID: evmchain.IDArbSepolia, Address: omegaPortal, DeployHeight: 67804723},
+			{ChainID: evmchain.IDBaseSepolia, Address: omegaPortal, DeployHeight: 13302705},
+			{ChainID: evmchain.IDHolesky, Address: omegaPortal, DeployHeight: 2034739},
+			{ChainID: evmchain.IDOpSepolia, Address: omegaPortal, DeployHeight: 15285567},
+		},
 		ConsensusGenesisJSON: omegaConsensusGenesisJSON,
 		ConsensusSeedTXT:     omegaConsensusSeedsTXT,
 		ExecutionGenesisJSON: omegaExecutionGenesisJSON,
