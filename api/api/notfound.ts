c90a861d4931089b8417daf649d15ea60fa202c6
Kevin Halliday <kevin@omni.network>
2024-07-31 16:05:35 -0400

    chore(api): remove api from monorepo

--- api/api/notfound.ts
+++
@@ -1,5 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-
-export default function handler(_req: VercelRequest, res: VercelResponse) {
-  res.status(404).send('Not Found')
-}
