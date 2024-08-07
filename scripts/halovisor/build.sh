8114561520e2e09cd98b4a3cae0871f61ffe18f2
corver <corver@omni.network>
2024-08-07 15:21:59 +0200

    chore(e2e): fix build

--- scripts/halovisor/build.sh
+++ scripts/halovisor/build.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
-# ./build.sh <halo_genesis_version>
+# ./build.sh <HALO_VERSION_GENESIS>
 # This scripts builds the halovisor docker image
 # Halovisor wraps cosmovisor and multiple halo versions into a single docker image.
 # It allows for docker based deployments that support halo network upgrades.
 
+set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-HALO_GENESIS_VERSION="${1}"
-if [ -z "$HALO_GENESIS_VERSION" ]; then
-  HALO_GENESIS_VERSION=$(git rev-parse --short=7 HEAD)
-  echo "Using head as HALO_GENESIS_VERSION: ${HALO_GENESIS_VERSION}"
+HALO_VERSION_GENESIS="${1}"
+if [ -z "$HALO_VERSION_GENESIS" ]; then
+  HALO_VERSION_GENESIS=$(git rev-parse --short=7 HEAD)
+  echo "Using head as HALO_VERSION_GENESIS: ${HALO_VERSION_GENESIS}"
 fi
 
-IMAGEREF="omniops/halovisor:${HALO_GENESIS_VERSION}"
+IMAGEREF="omniops/halovisor:${HALO_VERSION_GENESIS}"
 IMAGEMAIN="omniops/halovisor:main"
 
 docker build \
-  --build-arg HALO_GENESIS_VERSION="${HALO_GENESIS_VERSION}" \
+  --build-arg HALO_VERSION_GENESIS="${HALO_VERSION_GENESIS}" \
   -t "${IMAGEREF}" \
   -t "${IMAGEMAIN}" \
   "${SCRIPT_DIR}"
