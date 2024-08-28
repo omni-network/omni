59b8e13c432abe69da271d45857a72c968293971
Kevin Halliday <kevin@omni.network>
2024-08-28 11:42:36 -0400

    rebrand fund to gas station

--- contracts/core/src/token/OmniGasPump.sol
+++ contracts/core/src/token/OmniGasPump.sol
@@ -6,33 +6,47 @@ import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/P
 import { XAppUpgradeable } from "src/pkg/XAppUpgradeable.sol";
 import { FeeOracleV1 } from "src/xchain/FeeOracleV1.sol";
 import { ConfLevel } from "src/libraries/ConfLevel.sol";
-import { OmniXFund } from "./OmniXFund.sol";
+import { OmniGasStation } from "./OmniGasStation.sol";
 
 /**
  * @title OmniGasPump
- * @notice A unidirectional cross-chain gas exchange, allowing users to swap native ETH for native OMNI.[]
+ * @notice A unidirectional cross-chain gas exchange, allowing users to swap native ETH for native OMNI.
  */
 contract OmniGasPump is XAppUpgradeable, OwnableUpgradeable, PausableUpgradeable {
+    /// @notice Emitted when the max swap is set
     event MaxSwapSet(uint256 max);
-    event FundSet(address fund);
-    event PctCutSet(uint256 pct);
-    event FillUp(address indexed recipient, uint256 total, uint256 paid, uint256 fee, uint256 cut, uint256 amtOMNI);
 
-    /// @notice Gas limit passed to OmniXFund.settleUp xcall
+    /// @notice Emitted when the gas station is set
+    event GasStationSet(address station);
+
+    /// @notice Emitted when the toll is set
+    event TollSet(uint256 pct);
+
+    /**
+     * @notice Emitted on a fillUp
+     * @param recipient Address on Omni to send OMNI to
+     * @param owed      Total amount owed to `recipient` (sum of historical fillUps()), denominated in OMNI.
+     * @param amtETH    Amount of ETH paid
+     * @param fee       Xcall fee, denominated in ETH
+     * @param toll      Toll taken by this contract, denominated in ETH
+     * @param amtOMNI   Amount of OMNI swapped for
+     */
+    event FilledUp(address indexed recipient, uint256 owed, uint256 amtETH, uint256 fee, uint256 toll, uint256 amtOMNI);
+
+    /// @notice Gas limit passed to OmniGasStation.settleUp xcall
     uint64 internal constant SETTLE_GAS = 100_000;
 
-    /// @notice Denominator for pct cut
-    uint256 internal constant PCT_DENOM = 1000;
+    /// @notice Denominator for toll percentage calculations
+    uint256 internal constant TOLL_DENOM = 1000;
 
-    /// @notice Address of OmniXFund on Omni
-    address public fund;
+    /// @notice Address of OmniGasStation on Omni
+    address public gasStation;
 
     /// @notice Max amt (in native token) that can be swapped in a single tx
     uint256 public maxSwap;
 
-    /// @notice Percentage cut (over PCT_DENOM) taken by this contract for each swap
-    ///     Used to disencentivize spamming
-    uint256 public pctCut;
+    /// @notice Percentage toll taken by this contract for each swap, to disencentivize spamming
+    uint256 public toll;
 
     /// @notice Map recipient to total owed (sum of historical fillUps()), denominated in OMNI.
     mapping(address => uint256) public owed;
@@ -42,17 +56,17 @@ contract OmniGasPump is XAppUpgradeable, OwnableUpgradeable, PausableUpgradeable
     }
 
     struct InitParams {
-        address fund;
+        address gasStation;
         address portal;
         address owner;
         uint256 maxSwap;
-        uint256 pctCut;
+        uint256 toll;
     }
 
     function initialize(InitParams calldata p) external initializer {
-        _setFund(p.fund);
+        _setGasStation(p.gasStation);
         _setMaxSwap(p.maxSwap);
-        _setPctCut(p.pctCut);
+        _setToll(p.toll);
         __XApp_init(p.portal, ConfLevel.Latest);
         __Ownable_init(p.owner);
     }
@@ -63,33 +77,39 @@ contract OmniGasPump is XAppUpgradeable, OwnableUpgradeable, PausableUpgradeable
      *      Takes an xcall fee and a pct cut. Cut taken to disencentivize spamming.
      *      Returns the amount of OMNI swapped for.
      *
-     *      To retry (if OmniXFund transfer fails), call swap() again with the
+     *      To retry (if OmniGasStation transfer fails), call swap() again with the
      *      same `recipient`, and msg.value == swapFee().
      *
      * @param recipient Address on Omni to send OMNI to
      */
     function fillUp(address recipient) public payable whenNotPaused returns (uint256) {
-        uint256 f = fee();
-        require(msg.value >= f, "OmniExchange: insufficient fee");
-
+        // take xcall fee
+        uint256 f = xfee();
+        require(msg.value >= f, "OmniGasPump: insufficient fee");
         uint256 amtETH = msg.value - f;
-        require(amtETH <= maxSwap, "OmniExchange: over max");
 
-        uint256 cut = amtETH * pctCut / PCT_DENOM;
-        amtETH -= cut;
+        // check max
+        require(amtETH <= maxSwap, "OmniGasPump: over max");
+
+        // take toll
+        uint256 t = amtETH * toll / TOLL_DENOM;
+        amtETH -= t;
 
         uint256 amtOMNI = _toOmni(amtETH);
+
+        // update owed
         owed[recipient] += amtOMNI;
 
+        // settle up with the gas station
         xcall({
             destChainId: omniChainId(),
-            to: fund,
+            to: gasStation,
             conf: ConfLevel.Latest,
-            data: abi.encodeCall(OmniXFund.settleUp, (recipient, owed[recipient])),
+            data: abi.encodeCall(OmniGasStation.settleUp, (recipient, owed[recipient])),
             gasLimit: SETTLE_GAS
         });
 
-        emit FillUp(recipient, owed[recipient], msg.value, f, cut, amtOMNI);
+        emit FilledUp(recipient, owed[recipient], msg.value, f, t, amtOMNI);
 
         return amtOMNI;
     }
@@ -100,31 +120,42 @@ contract OmniGasPump is XAppUpgradeable, OwnableUpgradeable, PausableUpgradeable
      *      or not it would succeed, and the revert reason, if any.
      */
     function dryFillUp(uint256 amtETH) public view returns (uint256, bool, string memory) {
-        uint256 f = fee();
+        // take xcall fee
+        uint256 f = xfee();
         if (amtETH < f) return (0, false, "insufficient fee");
-
         amtETH = amtETH - f;
+
+        // check max
         if (amtETH > maxSwap) return (0, false, "over max");
 
-        return (_toOmni(_takeCut(amtETH)), true, "");
+        // take toll
+        amtETH -= amtETH * toll / TOLL_DENOM;
+
+        return (_toOmni(amtETH), true, "");
     }
 
     /// @notice Returns the xcall fee required for fillUp(). Does not include `pctCut`.
-    function fee() public view returns (uint256) {
+    function xfee() public view returns (uint256) {
         // Use max addrs & amount to use no zero byte calldata to ensure max fee
         address recipient = address(type(uint160).max);
         uint256 amt = type(uint256).max;
 
         return feeFor({
             destChainId: omniChainId(),
-            data: abi.encodeCall(OmniXFund.settleUp, (recipient, amt)),
+            data: abi.encodeCall(OmniGasStation.settleUp, (recipient, amt)),
             gasLimit: SETTLE_GAS
         });
     }
 
     /// @notice Returns the amount of ETH needed to swap for `amtOMNI`
     function quote(uint256 amtOMNI) public view returns (uint256) {
-        return _undoCut(_toEth(amtOMNI)) + fee();
+        uint256 amtETH = _toEth(amtOMNI);
+
+        // "undo" toll
+        amtETH += (amtETH * TOLL_DENOM / (TOLL_DENOM - toll));
+
+        // "undo" xcall fee
+        return amtETH + xfee();
     }
 
     /// @notice Converts `amtOMNI` to ETH, using the current conversion rate
@@ -137,16 +168,6 @@ contract OmniGasPump is XAppUpgradeable, OwnableUpgradeable, PausableUpgradeable
     function _toOmni(uint256 amtETH) internal view returns (uint256) {
         FeeOracleV1 o = FeeOracleV1(omni.feeOracle());
         return amtETH * o.toNativeRate(omniChainId()) / o.CONVERSION_RATE_DENOM();
-    }
-
-    /// @notice Returns `amtETH` cut by `pctCut`.
-    function _takeCut(uint256 amtETH) internal view returns (uint256) {
-        return amtETH - (amtETH * pctCut / PCT_DENOM);
-    }
-
-    /// @notice Returns n such `n = _takeCut(amtETH)`
-    function _undoCut(uint256 amtETH) internal view returns (uint256) {
-        return amtETH + (amtETH * PCT_DENOM / (PCT_DENOM - pctCut));
     }
 
     //////////////////////////////////////////////////////////////////////////////
@@ -166,7 +187,7 @@ contract OmniGasPump is XAppUpgradeable, OwnableUpgradeable, PausableUpgradeable
     /// @notice Withdraw collected ETH to `to`
     function withdraw(address to) external onlyOwner {
         (bool success,) = to.call{ value: address(this).balance }("");
-        require(success, "OmniExchange: withdraw failed");
+        require(success, "OmniGasPump: withdraw failed");
     }
 
     /// @notice Set the max swap, denominated in ETh
@@ -174,31 +195,31 @@ contract OmniGasPump is XAppUpgradeable, OwnableUpgradeable, PausableUpgradeable
         _setMaxSwap(max);
     }
 
-    /// @notice Set the address of the OmniXFund, on Omni
-    function setOmniXFund(address omnifund_) external onlyOwner {
-        _setFund(omnifund_);
+    /// @notice Set the address of the OmniGasStation, on Omni
+    function setOmniGasStation(address station) external onlyOwner {
+        _setGasStation(station);
     }
 
-    /// @notice Set the pct cut taken on each swap
-    function setPctCut(uint256 pct) external onlyOwner {
-        _setPctCut(pct);
+    /// @notice Set the toll (as a percentage over PCT_DENOM)
+    function setToll(uint256 pct) external onlyOwner {
+        _setToll(pct);
     }
 
-    function _setPctCut(uint256 pct) internal {
-        require(pct < PCT_DENOM, "OmniExchange: over pct cut");
-        pctCut = pct;
-        emit PctCutSet(pct);
+    function _setToll(uint256 pct) internal {
+        require(pct < TOLL_DENOM, "OmniGasPump: pct too high");
+        toll = pct;
+        emit TollSet(toll);
     }
 
     function _setMaxSwap(uint256 max) internal {
-        require(max > 0, "OmniExchange: zero max");
+        require(max > 0, "OmniGasPump: zero max");
         maxSwap = max;
         emit MaxSwapSet(max);
     }
 
-    function _setFund(address fund_) internal {
-        require(fund_ != address(0), "OmniExchange: zero address");
-        fund = fund_;
-        emit FundSet(fund_);
+    function _setGasStation(address station) internal {
+        require(station != address(0), "OmniGasPump: zero address");
+        gasStation = station;
+        emit GasStationSet(station);
     }
 }
---
+++ contracts/core/src/token/OmniGasStation.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.24;
+
+import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import { XAppUpgradeable } from "src/pkg/XAppUpgradeable.sol";
+import { ConfLevel } from "src/libraries/ConfLevel.sol";
+
+/**
+ * @title OmniGasStation
+ * @notice Pays out all gas owed by way of OmniGasPumps on other chains
+ */
+contract OmniGasStation is XAppUpgradeable, OwnableUpgradeable, PausableUpgradeable {
+    /**
+     * @notice Emitted on settleUp
+     * @param recipient Address
+     * @param chainID   ChainID of the pump
+     * @param owed      Total amount owed to `recipient` by way of `chainID`'s OmniGasPump
+     * @param fueled    Total amount sent to `recipient` by this contract.
+     * @param success   True if the transfer was successful (if true, owed == fueled)
+     */
+    event SettledUp(address indexed recipient, uint64 indexed chainID, uint256 owed, uint256 fueled, bool success);
+
+    /// @notice Emitted when a OmniGasPump is set for a chain
+    event GasPumpAdded(uint64 indexed chainID, address addr);
+
+    //// @notice Map chainID to addr to true, if authorized to withdraw
+    mapping(uint64 => address) public pumps;
+
+    /// @notice Map recipient to chainID to total fueled
+    mapping(address => mapping(uint64 => uint256)) public fueled;
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address portal, address owner) external initializer {
+        __XApp_init(portal, ConfLevel.Finalized);
+        __Ownable_init(owner);
+    }
+
+    /**
+     * @notice Settle up with a recipient. If `owed` is more than they've been fueled, send the difference.
+     * @param recipient Address to receive the funds
+     * @param owed      Total amount owed to `recipient`, by way of xmsg.sourceChainId's OmniGasPump
+     */
+    function settleUp(address recipient, uint256 owed) external xrecv whenNotPaused {
+        require(isXCall() && isPump(xmsg.sourceChainId, xmsg.sender), "GasStation: unauthorized");
+
+        uint256 settled = fueled[recipient][xmsg.sourceChainId];
+
+        // If already settled, revert
+        require(owed >= settled, "GasStation: already funded");
+
+        // Transfer the difference
+        (bool success,) = recipient.call{ value: owed - settled }("");
+
+        // Update books. We do not bother doing so pre-transfer, because isXCall() prevents reentrancy
+        if (success) fueled[recipient][xmsg.sourceChainId] = owed;
+
+        emit SettledUp(recipient, xmsg.sourceChainId, owed, fueled[recipient][xmsg.sourceChainId], success);
+    }
+
+    /// @notice Set the pump addr for a chain
+    function setPump(uint64 chainID, address addr) external onlyOwner {
+        pumps[chainID] = addr;
+        emit GasPumpAdded(chainID, addr);
+    }
+
+    /// @notice Return true if `chainID` has a registered pump at `addr`
+    function isPump(uint64 chainID, address addr) public view returns (bool) {
+        return addr != address(0) && addr == pumps[chainID];
+    }
+
+    /// @notice Pause withdrawals
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    /// @notice Unpause withdrawals
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    receive() external payable { }
+}
