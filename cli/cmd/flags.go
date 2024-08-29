13fe1862c0fdc81dfd977cba387291f1b0176aeb
corver <corver@omni.network>
2024-08-29 12:34:42 +0200

    feat(cli): support archive in init-nodes (#1788)
    Adds a `--archive` flag to `omni operator init-nodes` to join a network
    as an archive node.
    
    This is currently blocked on the fact that our archive nodes are not
    publicly accessible via P2P so the local archive node can't find blocks
    to download.
    
    issue: none

--- cli/cmd/flags.go
+++ cli/cmd/flags.go
@@ -19,6 +19,8 @@ func bindInitConfig(cmd *cobra.Command, cfg *initConfig) {
 	cmd.Flags().StringVar(&cfg.Moniker, "moniker", "", "Human-readable node name used in p2p networking")
 	cmd.Flags().StringVar(&cfg.Home, "home", "", "Home directory. If empty, defaults to: $HOME/.omni/<network>/")
 	cmd.Flags().BoolVar(&cfg.Clean, "clean", cfg.Clean, "Delete contents of home directory")
+	cmd.Flags().BoolVar(&cfg.Archive, "archive", cfg.Archive, "Enable archive mode. Note this requires more disk space")
+	cmd.Flags().BoolVar(&cfg.Debug, "debug", cfg.Debug, "Configure nodes with debug log level")
 }
 
 func bindAVSAddress(cmd *cobra.Command, addr *string) {
