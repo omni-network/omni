3c1e08a3d9e08efa138ebcaaf7a74a63244e4fff
Aayush Rajasekaran <arajasek94@gmail.com>
2024-09-03 16:50:38 -0400

    chore(*): rename to BlockPeriodNs

--- lib/netconf/provider.go
+++ lib/netconf/provider.go
@@ -227,7 +227,7 @@ func toPortalBindings(portals []*rtypes.Portal) []bindings.PortalRegistryDeploym
 			DeployHeight:   p.DeployHeight,
 			Shards:         p.ShardIds,
 			AttestInterval: p.AttestInterval,
-			BlockPeriod:    p.BlockPeriodMs,
+			BlockPeriod:    p.BlockPeriodNs,
 			Name:           p.Name,
 		}
 	}
