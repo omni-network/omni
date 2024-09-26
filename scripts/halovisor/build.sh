c5323ac9253a9407c34cbf0b3a433f2117cb3bca
corver <corver@omni.network>
2024-09-26 10:39:07 +0200

    ci(halovisor): swap to omni cosmovisor fork (#1977)
    Swap the cosmovisor binary inside halovisor to the omni
    [fork](https://github.com/omni-network/cosmovisor).
    
    This ensures that the next version, `v0.8.0`. Will be compatible with
    subsequent e2e app that mounts only a single `/halo` folder to
    halovisor. This ensures we can use `v0.8.0`as LTS "genesis version" and
    backwards compatibility target.
    
    issue: none

--- scripts/halovisor/build.sh
+++ scripts/halovisor/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# ./build.sh <HALO_VERSION_GENESIS>
+# ./build.sh <HALO_VERSION_0_GENESIS>
 # This scripts builds the halovisor docker image
 # Halovisor wraps cosmovisor and multiple halo versions into a single docker image.
 # It allows for docker based deployments that support halo network upgrades.
@@ -8,17 +8,17 @@
 set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-HALO_VERSION_GENESIS="${1}"
-if [ -z "$HALO_VERSION_GENESIS" ]; then
-  HALO_VERSION_GENESIS=$(git rev-parse --short=7 HEAD)
-  echo "Using head as HALO_VERSION_GENESIS: ${HALO_VERSION_GENESIS}"
+HALO_VERSION_0_GENESIS="${1}"
+if [ -z "${HALO_VERSION_0_GENESIS}" ]; then
+  HALO_VERSION_0_GENESIS=$(git rev-parse --short=7 HEAD)
+  echo "Using head as HALO_VERSION_GENESIS: ${HALO_VERSION_0_GENESIS}"
 fi
 
-IMAGEREF="omniops/halovisor:${HALO_VERSION_GENESIS}"
+IMAGEREF="omniops/halovisor:${HALO_VERSION_0_GENESIS}"
 IMAGEMAIN="omniops/halovisor:main"
 
 docker build \
-  --build-arg HALO_VERSION_GENESIS="${HALO_VERSION_GENESIS}" \
+  --build-arg HALO_VERSION_0_GENESIS="${HALO_VERSION_0_GENESIS}" \
   -t "${IMAGEREF}" \
   -t "${IMAGEMAIN}" \
   "${SCRIPT_DIR}"
