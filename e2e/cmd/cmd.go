omni-network/omni

commit 5aee7a6376a984b3afd5f1e7508dae59a784fa40

chore(lib/cmd): improve invalid command errors (#2054)
Improve binary errors when subcommands are invalid or not specified,
reverting to default cobra behaviour.

issue: none

---------

Co-authored-by: Gregor G. <75445744+sideninja@users.noreply.github.com>

lib/cmd/cmd.go
@@ -20,24 +20,20 @@ import (
 	"github.com/spf13/viper"
 )
 
-// Main is the main entry point for the command line tool.
+// Main is the main entry point for the omni application binaries.
 // Usage:
 //
 //	   func main() {
 //		     libcmd.Main(appcmd.New())
 //	   }
 func Main(cmd *cobra.Command) {
+	wrapRunCmd(cmd)
+
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-
-	SilenceErrUsage(cmd)
-
 	err := cmd.ExecuteContext(ctx)
-
 	cancel()
 
 	if err != nil {
-		log.Error(ctx, "!! Fatal error occurred, app died unexpectedly !!", err)
-
 		const errExitCode = 1
 		os.Exit(errExitCode) //nolint:revive // Deep exit is exactly the point of this helper function.
 	}
@@ -45,7 +41,6 @@ func Main(cmd *cobra.Command) {
 
 // NewRootCmd returns a new root cobra command that handles our command line tool.
 // It sets up the general viper config and binds the cobra flags to the viper flags.
-// It also silences the usage printing when commands error during "running".
 func NewRootCmd(appName string, appDescription string, subCmds ...*cobra.Command) *cobra.Command {
 	root := &cobra.Command{
 		Use:   appName,
@@ -53,6 +48,10 @@ func NewRootCmd(appName string, appDescription string, subCmds ...*cobra.Command
 		Args:  cobra.NoArgs,
 		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
 			return initializeConfig(appName, cmd)
+		},
+		RunE: func(*cobra.Command, []string) error {
+			// Callers should either add sub-commands or override RunE.
+			return errors.New("no sub-command specified, see --help")
 		},
 	}
 
@@ -167,4 +166,35 @@ func bindFlags(cmd *cobra.Command, v *viper.Viper) error {
 	})
 
 	return lastErr
+}
+
+// wrapRunCmd wraps the "app run" command to custom fatal error log and silence cobra output.
+func wrapRunCmd(cmd *cobra.Command) {
+	runCmd := getRunCmd(cmd)
+	SilenceErrUsage(runCmd)
+	runFunc := runCmd.RunE
+	runCmd.RunE = func(cmd *cobra.Command, args []string) error {
+		if runFunc == nil {
+			return errors.New("run command RunE nil [BUG]")
+		}
+
+		err := runFunc(cmd, args)
+		if err != nil {
+			log.Error(cmd.Context(), "!! Fatal error occurred, app died !!", err)
+		}
+
+		return err
+	}
+}
+
+// getRunCmd returns the "run" subcommand of the given command or the command itself.
+func getRunCmd(cmd *cobra.Command) *cobra.Command {
+	const name = "run"
+	for _, sub := range cmd.Commands() {
+		if sub.Use == name {
+			return sub
+		}
+	}
+
+	return cmd
 }

halo/cmd/cmd_internal_test.go
@@ -167,6 +167,37 @@ func TestTomlConfig(t *testing.T) {
 	tutil.RequireNoError(t, rootCmd.Execute())
 }
 
+func TestInvalidCmds(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		Name        string
+		Args        []string
+		ErrContains string
+	}{
+		{
+			Name:        "no args",
+			Args:        []string{},
+			ErrContains: "no sub-command specified, see --help",
+		},
+		{
+			Name:        "invalid args",
+			Args:        []string{"invalid"},
+			ErrContains: "unknown command \"invalid\" for \"halo\"",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			t.Parallel()
+			rootCmd := libcmd.NewRootCmd("halo", "", New())
+			rootCmd.SetArgs(test.Args)
+			err := rootCmd.Execute()
+			require.Error(t, err)
+			require.Contains(t, err.Error(), test.ErrContains)
+		})
+	}
+}
+
 // slice is a convenience function for creating string slice literals.
 func slice(strs ...string) []string {
 	return strs

e2e/app/rpc.go
@@ -66,8 +66,8 @@ func waitForHeight(ctx context.Context, testnet *e2e.Testnet, height int64) (*ty
 				}
 
 				result, err := currentBlock(ctx, client)
-				if errors.Is(err, context.DeadlineExceeded) {
-					return nil, nil, errors.Wrap(err, "timeout")
+				if ctx.Err() != nil {
+					return nil, nil, errors.Wrap(err, "parent context canceled")
 				} else if err != nil {
 					continue
 				}

halo/cmd/testdata/TestCLIReference_halo.golden
@@ -1,6 +1,7 @@
 Halo is a consensus client implementation for the Omni Protocol
 
 Usage:
+  halo [flags]
   halo [command]
 
 Available Commands:

commit 048d3b08982553d1bd96e9c1fac65663a93a7d90

fix(lib/cmd): fix missing cli errors (#2088)
Fixes an issue introduced in #2054:
 - Only `run` or root commands logged omni-style app fatal error logs
 - All other commands didn't log anything when crashing.
- Now only cobra cli flags or usage issues do cli style logging,
everything else is omni style.
 
This affects v0.9.0, but only halo subcommands like `halo rollback` or
`halo init` which will not log fatal errors.
 This doesn't affect omni cli thought which has custom logic
 
Also fixes legacy issue with `e2e` app not supporting viper (e2e vars).

issue: none

lib/cmd/cmd.go
@@ -27,7 +27,7 @@ import (
 //		     libcmd.Main(appcmd.New())
 //	   }
 func Main(cmd *cobra.Command) {
-	wrapRunCmd(cmd)
+	wrapRunE(cmd)
 
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	err := cmd.ExecuteContext(ctx)
@@ -39,6 +39,26 @@ func Main(cmd *cobra.Command) {
 	}
 }
 
+// wrapRunE wraps all (nested) RunE functions adding omni logging (stack traces + structured errors).
+// Done here, so cobra command/flag errors are cli style (usage + simple errors).
+func wrapRunE(cmd *cobra.Command) {
+	runE := cmd.RunE
+	cmd.RunE = func(cmd *cobra.Command, args []string) error {
+		err := runE(cmd, args)
+		if err != nil {
+			cmd.SilenceUsage = true
+			cmd.SilenceErrors = true
+			log.Error(cmd.Context(), "!! Fatal error occurred, app died !!", err)
+		}
+
+		return err
+	}
+
+	for _, subCmd := range cmd.Commands() {
+		wrapRunE(subCmd)
+	}
+}
+
 // NewRootCmd returns a new root cobra command that handles our command line tool.
 // It sets up the general viper config and binds the cobra flags to the viper flags.
 func NewRootCmd(appName string, appDescription string, subCmds ...*cobra.Command) *cobra.Command {
@@ -47,6 +67,8 @@ func NewRootCmd(appName string, appDescription string, subCmds ...*cobra.Command
 		Short: appDescription,
 		Args:  cobra.NoArgs,
 		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
+			// Note that callers should wrap this if replacing PersistentPreRunE.
+			// So they get proper toml+env config.
 			return initializeConfig(appName, cmd)
 		},
 		RunE: func(*cobra.Command, []string) error {
@@ -166,35 +188,4 @@ func bindFlags(cmd *cobra.Command, v *viper.Viper) error {
 	})
 
 	return lastErr
-}
-
-// wrapRunCmd wraps the "app run" command to custom fatal error log and silence cobra output.
-func wrapRunCmd(cmd *cobra.Command) {
-	runCmd := getRunCmd(cmd)
-	SilenceErrUsage(runCmd)
-	runFunc := runCmd.RunE
-	runCmd.RunE = func(cmd *cobra.Command, args []string) error {
-		if runFunc == nil {
-			return errors.New("run command RunE nil [BUG]")
-		}
-
-		err := runFunc(cmd, args)
-		if err != nil {
-			log.Error(cmd.Context(), "!! Fatal error occurred, app died !!", err)
-		}
-
-		return err
-	}
-}
-
-// getRunCmd returns the "run" subcommand of the given command or the command itself.
-func getRunCmd(cmd *cobra.Command) *cobra.Command {
-	const name = "run"
-	for _, sub := range cmd.Commands() {
-		if sub.Use == name {
-			return sub
-		}
-	}
-
-	return cmd
 }

e2e/cmd/cmd.go
@@ -31,7 +31,12 @@ func New() *cobra.Command {
 	var def app.Definition
 
 	cmd := libcmd.NewRootCmd("e2e", "e2e network generator and test runner")
+	cachedPreRun := cmd.PersistentPreRunE
 	cmd.PersistentPreRunE = func(cmd *cobra.Command, _ []string) error {
+		if err := cachedPreRun(cmd, nil); err != nil {
+			return err
+		}
+
 		ctx := cmd.Context()
 		if _, err := log.Init(ctx, logCfg); err != nil {
 			return err

halo/cmd/cmd_internal_test.go
@@ -22,13 +22,14 @@ import (
 
 //go:generate go test . -golden -clean
 
-func TestRunCmd(t *testing.T) { //nolint:paralleltest,tparallel // RunCmd modifies global state via setMonikerForT
+func TestRunCmd(t *testing.T) {
 	setMonikerForT(t)
 
 	tests := []struct {
 		Name  string
 		Args  []string
 		Files map[string][]byte
+		Env   map[string]string
 	}{
 		{
 			Name: "defaults",
@@ -45,18 +46,21 @@ func TestRunCmd(t *testing.T) { //nolint:paralleltest,tparallel // RunCmd modifi
 		{
 			Name: "json files",
 			Args: slice("run", "--home=testinput/input2"),
+			Env:  map[string]string{"HALO_NETWORK": "GOTEST"},
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.Name, func(t *testing.T) {
-			t.Parallel()
-
 			cmd := newRunCmd("run", func(_ context.Context, actual app.Config) error {
 				tutil.RequireGoldenJSON(t, actual)
 
 				return nil
 			})
+
+			for k, v := range test.Env {
+				t.Setenv(k, v)
+			}
 
 			rootCmd := libcmd.NewRootCmd("halo", "", cmd)
 			rootCmd.SetArgs(test.Args)

halo/cmd/testdata/TestRunCmd_json_files.golden
@@ -1,6 +1,6 @@
 {
  "HomeDir": "testinput/input2",
- "Network": "",
+ "Network": "GOTEST",
  "EngineJWTFile": "jwt.json",
  "EngineEndpoint": "",
  "RPCEndpoints": null,