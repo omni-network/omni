e8372bf00e9560a5e67bf12b80626ca0ca57e2d6
corver <corver@omni.network>
2024-10-10 10:42:46 +0200

    fix(octane/keeper): retry fecthing evm event logs

--- octane/evmengine/keeper/evmmsgs.go
+++ octane/evmengine/keeper/evmmsgs.go
@@ -7,6 +7,7 @@ import (
 	"sort"
 
 	"github.com/omni-network/omni/lib/errors"
+	"github.com/omni-network/omni/lib/log"
 	"github.com/omni-network/omni/octane/evmengine/types"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -16,17 +17,25 @@ import (
 func (k *Keeper) evmEvents(ctx context.Context, blockHash common.Hash) ([]*types.EVMEvent, error) {
 	var events []*types.EVMEvent
 	for _, proc := range k.eventProcs {
-		ll, err := proc.Prepare(ctx, blockHash)
-		if err != nil {
-			return nil, errors.Wrap(err, "prepare msgs")
-		}
-		for _, log := range ll {
-			if err := log.Verify(); err != nil {
-				return nil, errors.Wrap(err, "verify log")
+		err := retryForever(ctx, func(ctx context.Context) (bool, error) {
+			ll, err := proc.Prepare(ctx, blockHash)
+			if err != nil {
+				log.Warn(ctx, "Failed fetching evm logs (will retry)", err, "proc", proc)
+				return false, nil
 			}
-		}
+			// Verify all logs
+			for _, l := range ll {
+				if err := l.Verify(); err != nil {
+					return false, errors.Wrap(err, "verify log")
+				}
+			}
+			events = append(events, ll...)
 
-		events = append(events, ll...)
+			return true, nil
+		})
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	// Sort by Address > Topics > Data
@@ -35,8 +44,6 @@ func (k *Keeper) evmEvents(ctx context.Context, blockHash common.Hash) ([]*types
 		if cmp := bytes.Compare(events[i].Address, events[j].Address); cmp != 0 {
 			return cmp < 0
 		}
-
-		// TODO: replace this with sort.CompareFunc in next network upgrade which is more performant but has slightly different results
 		topicI := slices.Concat(events[i].Topics...)
 		topicJ := slices.Concat(events[j].Topics...)
 		if cmp := bytes.Compare(topicI, topicJ); cmp != 0 {
