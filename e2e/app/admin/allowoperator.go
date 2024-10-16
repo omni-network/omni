497f374eb6a74f88a7e01f9286871862840953c9
Khalil Claybon <khalil@omni.network>
2024-10-16 09:31:15 -0400

    chore: whitelist operator DAIC

--- e2e/app/admin/allowoperator.go
+++ e2e/app/admin/allowoperator.go
@@ -25,6 +25,7 @@ var omegaOperators = []common.Address{
 	common.HexToAddress("0x641F5938E0d093988d7Cf99509C3152FC7922B88"), // Galaxy
 	common.HexToAddress("0xb86DDe58C05dF3C09a8eB7476152793138D113C9"), // Chorusone
 	common.HexToAddress("0xcf8EB4Ee3cb3C9c14a4b290bD902dC06d2926ec1"), // P-OPS
+	common.HexToAddress("0x27eA917d14d637797FDeb3f9A9824395e7744941"), // DAIC
 }
 
 // AllowOperators ensures that all operators hard-coded in this package is allowed as validators.
