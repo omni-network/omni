a87a10b72a32d7ea21dc90f7fb8920a4afc566a7
corver <corver@omni.network>
2024-10-17 08:00:24 +0200

    wip: ordered submissions

--- relayer/app/sender.go
+++ relayer/app/sender.go
@@ -89,13 +89,22 @@ func NewSender(
 	}, nil
 }
 
-// SendTransaction sends the submission to the destination chain.
-func (s Sender) SendTransaction(ctx context.Context, sub xchain.Submission) error {
+// SendAsync sends the submission to the destination chain asynchronously.
+// It returns a channel that will receive an error if the submission fails or nil when it succeeds.
+// Nonces are however reserved synchronously, so ordering of submissions
+// is preserved.
+func (s Sender) SendAsync(ctx context.Context, sub xchain.Submission) chan error {
+	returnErr := func(err error) chan error {
+		resp := make(chan error, 1)
+		resp <- err
+		return resp
+	}
+
 	if s.txMgr == nil {
-		return errors.New("tx mgr not found", "dest_chain_id", sub.DestChainID)
+		return returnErr(errors.New("tx mgr not found", "dest_chain_id", sub.DestChainID))
 	} else if sub.DestChainID != s.chain.ID {
-		return errors.New("unexpected destination chain [BUG]",
-			"got", sub.DestChainID, "expect", s.chain.ID)
+		return returnErr(errors.New("unexpected destination chain [BUG]",
+			"got", sub.DestChainID, "expect", s.chain.ID))
 	}
 
 	// Get some info for logging
@@ -122,13 +131,13 @@ func (s Sender) SendTransaction(ctx context.Context, sub xchain.Submission) erro
 
 	txData, err := xchain.EncodeXSubmit(xchain.SubmissionToBinding(sub))
 	if err != nil {
-		return err
+		return returnErr(err)
 	}
 
 	// Reserve a nonce here to ensure correctly ordered submissions.
 	nonce, err := s.txMgr.ReserveNextNonce(ctx)
 	if err != nil {
-		return err
+		return returnErr(err)
 	}
 
 	estimatedGas := s.gasEstimator(s.chain.ID, sub.Msgs)
@@ -141,44 +150,50 @@ func (s Sender) SendTransaction(ctx context.Context, sub xchain.Submission) erro
 		Nonce:    &nonce,
 	}
 
-	tx, rec, err := s.txMgr.Send(ctx, candidate)
-	if err != nil {
-		return errors.Wrap(err, "failed to send tx", reqAttrs...)
-	}
+	asyncResp := make(chan error, 1)
+	go func() {
+		tx, rec, err := s.txMgr.Send(ctx, candidate)
+		if err != nil {
+			asyncResp <- errors.Wrap(err, "failed to send tx", reqAttrs...)
+			return
+		}
 
-	submissionTotal.WithLabelValues(srcChain, dstChain).Inc()
-	msgTotal.WithLabelValues(srcChain, dstChain).Add(float64(len(sub.Msgs)))
-	gasEstimated.WithLabelValues(dstChain).Observe(float64(estimatedGas))
+		submissionTotal.WithLabelValues(srcChain, dstChain).Inc()
+		msgTotal.WithLabelValues(srcChain, dstChain).Add(float64(len(sub.Msgs)))
+		gasEstimated.WithLabelValues(dstChain).Observe(float64(estimatedGas))
 
-	receiptAttrs := []any{
-		"valset_id", sub.ValidatorSetID,
-		"status", rec.Status,
-		"nonce", tx.Nonce(),
-		"height", rec.BlockNumber.Uint64(),
-		"gas_used", rec.GasUsed,
-		"tx_hash", rec.TxHash,
-	}
+		receiptAttrs := []any{
+			"valset_id", sub.ValidatorSetID,
+			"status", rec.Status,
+			"nonce", tx.Nonce(),
+			"height", rec.BlockNumber.Uint64(),
+			"gas_used", rec.GasUsed,
+			"tx_hash", rec.TxHash,
+		}
 
-	spendTotal.WithLabelValues(dstChain, string(s.gasToken)).Add(totalSpentGwei(tx, rec))
+		spendTotal.WithLabelValues(dstChain, string(s.gasToken)).Add(totalSpentGwei(tx, rec))
 
-	if rec.Status == 0 {
-		// Try and get debug information of the reverted transaction
-		resp, err := s.rpcClient.CallContract(ctx, callFromTx(s.txMgr.From(), tx), rec.BlockNumber)
+		if rec.Status == 0 {
+			// Try and get debug information of the reverted transaction
+			resp, err := s.rpcClient.CallContract(ctx, callFromTx(s.txMgr.From(), tx), rec.BlockNumber)
 
-		errAttrs := slices.Concat(receiptAttrs, reqAttrs, []any{
-			"call_resp", hexutil.Encode(resp),
-			"call_err", err,
-			"gas_limit", tx.Gas(),
-		})
+			errAttrs := slices.Concat(receiptAttrs, reqAttrs, []any{
+				"call_resp", hexutil.Encode(resp),
+				"call_err", err,
+				"gas_limit", tx.Gas(),
+			})
 
-		revertedSubmissionTotal.WithLabelValues(srcChain, dstChain).Inc()
+			revertedSubmissionTotal.WithLabelValues(srcChain, dstChain).Inc()
 
-		return errors.New("submission reverted", errAttrs...)
-	}
+			asyncResp <- errors.New("submission reverted", errAttrs...)
+			return
+		}
 
-	log.Info(ctx, "Sent submission", receiptAttrs...)
+		log.Info(ctx, "Sent submission", receiptAttrs...)
+		asyncResp <- nil
+	}()
 
-	return nil
+	return asyncResp
 }
 
 func callFromTx(from common.Address, tx *ethtypes.Transaction) ethereum.CallMsg {
