1eaf642c1b95f60be8fb50c9fe335136e6805b28
corver <corver@omni.network>
2024-10-24 18:03:20 +0200

    fix(monitor/routerecon): fix lag metric (#2276)
    Fix routescan recon lag metric. Previously, we compared the live
    submit_offset to a stale (up 30min old) routescan_offset which resulted
    in incorrect insight.
    
    issue: none
    
    ---------
    
    Co-authored-by: Gregor G. <75445744+sideninja@users.noreply.github.com>

--- monitor/routerecon/routescan_internal_test.go
+++ monitor/routerecon/routescan_internal_test.go
@@ -6,12 +6,50 @@ import (
 	"flag"
 	"testing"
 
+	"github.com/omni-network/omni/lib/evmchain"
+	"github.com/omni-network/omni/lib/log"
+	"github.com/omni-network/omni/lib/netconf"
+	"github.com/omni-network/omni/lib/xchain/connect"
+
 	"github.com/stretchr/testify/require"
 )
 
 //go:generate go test . -integration -v -run=TestQueryLatestXChain
 
 var integration = flag.Bool("integration", false, "run routescan integration tests")
+
+func TestReconLag(t *testing.T) {
+	t.Parallel()
+	if !*integration {
+		t.Skip("skipping integration test")
+	}
+
+	ctx := context.Background()
+	conn, err := connect.New(ctx, netconf.Omega)
+	require.NoError(t, err)
+
+	for _, stream := range conn.Network.EVMStreams() {
+		if stream.DestChainID == evmchain.IDArbSepolia || stream.SourceChainID == evmchain.IDArbSepolia {
+			// Skip arb since fetching cross txs from routescan fails
+			continue // TODO(corver): Remove when routescan adds support for arb_sepolia.
+		}
+
+		streamName := conn.Network.StreamName(stream)
+
+		cursor, ok, err := conn.XProvider.GetSubmittedCursor(ctx, stream)
+		require.NoError(t, err, streamName)
+		if !ok {
+			// Skip streams without submissions
+			continue
+		}
+
+		crossTx, err := paginateLatestCrossTx(ctx, queryFilter{Stream: stream})
+		require.NoError(t, err, streamName)
+
+		lag := float64(cursor.MsgOffset) - float64(crossTx.Data.Offset)
+		log.Info(ctx, "Routescan lag", "stream", streamName, "submitted_offset", cursor.MsgOffset, "lag", lag)
+	}
+}
 
 func TestQueryLatestXChain(t *testing.T) {
 	t.Parallel()
