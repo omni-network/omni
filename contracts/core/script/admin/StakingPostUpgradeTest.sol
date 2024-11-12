39262ac3d6c281b63722394a3945b3e53fb38259
Zodomo <zodomo@proton.me>
2024-11-12 16:30:52 -0600

    chore(contracts/core): generate sigs instead of precomputing them (#2466)
    Generate signatures instead of using precomputed values, as chainIds
    vary depending on destination. Precomputed values were causing tests to
    fail.
    
    issue: #2431

--- contracts/core/script/admin/StakingPostUpgradeTest.sol
+++ contracts/core/script/admin/StakingPostUpgradeTest.sol
@@ -68,17 +68,15 @@ contract StakingPostUpgradeTest is Test {
     }
 
     function _testCreateValidator() internal {
+        bytes32 privkey = 0xf0e1605dd50ce33553290b778b0f53b2cde5e47a8794c0e7d2815e456e6da3b9;
         bytes32 x = 0x3b12d750493ed6b12b390447f6dd38f587af12ed04ab8d6858e818cf0c63607c;
         bytes32 y = 0x044e0321a3e57de51e95f2b230b9e4ffed2318578baab1a80652234fe0115d13;
         bytes memory pubkey = Secp256k1.compressPublicKey(x, y);
-        bytes32 digest = 0x6b428e37b951e4f3858fde3c4f025278b0f2e1b8abf34fa295d26be1aeebf6e3;
-        bytes memory signature =
-            hex"ab1fc81491c244466777cc3edfbb5925cb2586c2f8d061edc09022cfc3f9ff6d06e8105a7228b964dd3a22349c9f853994db5ce47f76867ba43a641b7530559c1b";
+        bytes32 digest = staking.getValidatorPubkeyDigest(x, y);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(uint256(privkey), digest);
+        bytes memory signature = abi.encodePacked(r, s, v);
         uint256 deposit = staking.MinDeposit();
         vm.deal(validator, deposit);
-
-        bytes32 validatorPubkeyDigest = staking.getValidatorPubkeyDigest(x, y);
-        assertEq(validatorPubkeyDigest, digest, "validator pubkey digest");
 
         vm.expectEmit();
         emit Staking.CreateValidator(validator, pubkey, deposit);
