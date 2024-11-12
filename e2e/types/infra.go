964336b23045fe838148e78977d16644fc4e1a66
corver <corver@omni.network>
2024-11-12 15:16:40 +0200

    ci(e2e): support canary upgrade regex

--- e2e/types/infra.go
+++ e2e/types/infra.go
@@ -2,10 +2,31 @@ package types
 
 import (
 	"context"
+	"regexp"
+	"strings"
 
 	e2e "github.com/cometbft/cometbft/test/e2e/pkg"
 	"github.com/cometbft/cometbft/test/e2e/pkg/infra"
 )
+
+const (
+	// regexpCanary is a convenient way to specify canary upgrades.
+	regexpCanary = "canary"
+	// regexpNonCanary is a convenient way to specify non-canary upgrades.
+	regexpNonCanary = "non-canary"
+)
+
+// canaries define services included in canary upgrades
+// and excluded from non-canary upgrades.
+var canaries = map[string]bool{
+	"validator01": true,
+	"fullnode01":  true,
+	"archive01":   true,
+	"seed01":      true,
+	"relayer":     true,
+	"monitor":     true,
+	"solver":      true,
+}
 
 func DefaultServiceConfig() ServiceConfig {
 	return ServiceConfig{
@@ -16,6 +37,33 @@ func DefaultServiceConfig() ServiceConfig {
 type ServiceConfig struct {
 	// Regexp to match the service names.
 	Regexp string
+}
+
+// MatchService returns true if the service matches the regexp config.
+func (c ServiceConfig) MatchService(service string) bool {
+	if c.Regexp == "" {
+		return true
+	}
+
+	isCanary := func() bool {
+		for canary := range canaries {
+			if strings.HasPrefix(service, canary) {
+				return true
+			}
+		}
+
+		return false
+	}
+
+	if c.Regexp == regexpCanary {
+		return isCanary()
+	} else if c.Regexp == regexpNonCanary {
+		return !isCanary()
+	}
+
+	ok, _ := regexp.MatchString(c.Regexp, service) // Nothing matches invalid regex
+
+	return ok
 }
 
 type InfraProvider interface {
