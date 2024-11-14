51e914be8fce31d19af7810c75bfdd7721fc8c8d
Kevin Halliday <kevin@omni.network>
2024-11-14 17:30:08 -0500

    feat(e2e/solve): support devapp deposits

--- e2e/solve/devapp/devapp.go
+++ e2e/solve/devapp/devapp.go
@@ -4,6 +4,7 @@ import (
 	"github.com/omni-network/omni/contracts/bindings"
 	"github.com/omni-network/omni/lib/create3"
 	"github.com/omni-network/omni/lib/errors"
+	"github.com/omni-network/omni/lib/evmchain"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
@@ -14,18 +15,24 @@ type App struct {
 	L1Vault common.Address
 	L1Token common.Address
 	L2Token common.Address
+	L1      evmchain.Metadata
+	L2      evmchain.Metadata
 }
 
 var (
 	vaultABI     = mustGetABI(bindings.MockVaultMetaData)
 	tokenABI     = mustGetABI(bindings.MockTokenMetaData)
 	vaultDeposit = mustGetMethod(vaultABI, "deposit")
+	mockL1       = mustChainMeta(evmchain.IDMockL1)
+	mockL2       = mustChainMeta(evmchain.IDMockL2)
 
 	// static is the static devnt app instance.
 	static = App{
 		L1Vault: create3.Address(create3Factory, l1VaultSalt, deployer),
 		L1Token: create3.Address(create3Factory, l1TokenSalt, deployer),
 		L2Token: create3.Address(create3Factory, l2TokenSalt, deployer),
+		L1:      mockL1,
+		L2:      mockL2,
 	}
 )
 
@@ -49,4 +56,13 @@ func mustGetMethod(abi *abi.ABI, name string) abi.Method {
 	}
 
 	return method
+}
+
+func mustChainMeta(chainID uint64) evmchain.Metadata {
+	meta, ok := evmchain.MetadataByID(chainID)
+	if !ok {
+		panic(errors.New("missing chain meta", "chain_id", chainID))
+	}
+
+	return meta
 }
