fccb0c63e2182ccdec7e97ece5ce08410bc6b4f8
Kevin Halliday <kevin@omni.network>
2024-11-15 16:18:57 -0500

    chore(monitor/xfeemngr): tweak gas price tiers

--- monitor/xfeemngr/gasprice/tiers.go
+++ monitor/xfeemngr/gasprice/tiers.go
@@ -4,11 +4,14 @@ package gasprice
 var tiers = []uint64{
 	gwei(0.001),
 	gwei(0.1),
+	gwei(0.5),
 	gwei(1),
 	gwei(5),
 	gwei(10),
-	gwei(25),
+	gwei(20),
+	gwei(35),
 	gwei(50),
+	gwei(75),
 	gwei(100),
 	gwei(200),
 }
@@ -19,7 +22,7 @@ func Tiers() []uint64 {
 
 func Tier(live uint64) uint64 {
 	for _, p := range tiers {
-		if p > live {
+		if p >= live {
 			return p
 		}
 	}
