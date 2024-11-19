a9457cf11b39e50b501e9455513f054a2beb1599
Zodomo <zodomo@proton.me>
2024-11-19 17:22:36 -0600

    feat(contracts/misc): v0 pledge contract (#2515)
    Initial V0 pledge app contracts.
    
    issue: #2514

---
+++ contracts/misc/src/pledge/Pledge.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity =0.8.24;
+
+import { XAppBase } from "core/src/pkg/XAppBase.sol";
+
+/**
+ * @title Pledge
+ * @notice Receives pledges to Ethereum from users on other chains.
+ */
+contract Pledge is XAppBase {
+    /**
+     * @notice Thrown when the caller is not the OmniPortal.
+     */
+    error NotXCall();
+
+    /**
+     * @notice Emitted when a user pledges to Ethereum.
+     * @param from The address of the user who pledged.
+     * @param chainId The chain ID of the source chain.
+     * @param timestamp The timestamp the pledge was received.
+     */
+    event Pledged(address indexed from, uint256 indexed chainId, uint256 indexed timestamp);
+
+    /**
+     * @param omni_ The address of the OmniPortal.
+     */
+    constructor(address omni_) payable {
+        _setOmniPortal(omni_);
+    }
+
+    /**
+     * @notice Initiates a user's pledge to Ethereum.
+     * @dev Only the OmniPortal can call this function, user cannot call it directly.
+     * @dev Function selector is mined to be 0x00000000
+     */
+    function pledge_jwkilcxtschdbaaa() external xrecv {
+        // only accept xcalls
+        if (!isXCall()) revert NotXCall();
+        // emit pledge event, frontend will contextualize
+        emit Pledged(xmsg.sender, xmsg.sourceChainId, block.timestamp);
+    }
+}
