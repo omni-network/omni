omni-network/omni

commit 20d1ef7b96135c8c6aa745853ab7917677c842aa

chore(lib/contracts): output FeeOracleV1 impl and proxy addrs in logs (#2545)
Staging deployment is failing because FeeOracleV1 is failing to deploy.
It is working in devnet deployments and in CI tests, so I am confirming
it isn't an address collision first.

issue: none

lib/contracts/feeoraclev1/deploy.go
@@ -8,6 +8,7 @@ import (
 	"github.com/omni-network/omni/e2e/app/eoa"
 	"github.com/omni-network/omni/lib/errors"
 	"github.com/omni-network/omni/lib/ethclient/ethbackend"
+	"github.com/omni-network/omni/lib/log"
 	"github.com/omni-network/omni/lib/netconf"
 	"github.com/omni-network/omni/lib/tokens/coingecko"
 
@@ -84,6 +85,7 @@ func Deploy(ctx context.Context, network netconf.ID, chainID uint64, destChainID
 	}
 
 	impl, tx, _, err := bindings.DeployFeeOracleV1(txOpts, backend)
+	log.Info(ctx, "Fee oracle impl", "addr", impl.Hex(), "tx", tx.Hash().Hex(), "chain_id", chainID)
 	if err != nil {
 		return common.Address{}, nil, errors.Wrap(err, "deploy fee oracle")
 	}
@@ -94,6 +96,7 @@ func Deploy(ctx context.Context, network netconf.ID, chainID uint64, destChainID
 	}
 
 	proxy, tx, _, err := bindings.DeployTransparentUpgradeableProxy(txOpts, backend, impl, cfg.ProxyAdminOwner, initializer)
+	log.Info(ctx, "Fee oracle proxy", "addr", proxy.Hex(), "tx", tx.Hash().Hex(), "chain_id", chainID)
 	if err != nil {
 		return common.Address{}, nil, errors.Wrap(err, "deploy proxy")
 	}

commit dac3dc0374cc4f842042cff4f7b9e7a64a97bd41

fix(lib/contracts): fix FeeOracleV1 deploy logs (#2549)
Fixing bug introduced by logs in #2545 

issue: none

lib/contracts/feeoraclev1/deploy.go
@@ -85,10 +85,10 @@ func Deploy(ctx context.Context, network netconf.ID, chainID uint64, destChainID
 	}
 
 	impl, tx, _, err := bindings.DeployFeeOracleV1(txOpts, backend)
-	log.Info(ctx, "Fee oracle impl", "addr", impl.Hex(), "tx", tx.Hash().Hex(), "chain_id", chainID)
 	if err != nil {
 		return common.Address{}, nil, errors.Wrap(err, "deploy fee oracle")
 	}
+	log.Debug(ctx, "Fee oracle impl", "addr", impl.Hex(), "tx", tx.Hash().Hex(), "chain_id", chainID)
 
 	_, err = backend.WaitMined(ctx, tx)
 	if err != nil {
@@ -96,10 +96,10 @@ func Deploy(ctx context.Context, network netconf.ID, chainID uint64, destChainID
 	}
 
 	proxy, tx, _, err := bindings.DeployTransparentUpgradeableProxy(txOpts, backend, impl, cfg.ProxyAdminOwner, initializer)
-	log.Info(ctx, "Fee oracle proxy", "addr", proxy.Hex(), "tx", tx.Hash().Hex(), "chain_id", chainID)
 	if err != nil {
 		return common.Address{}, nil, errors.Wrap(err, "deploy proxy")
 	}
+	log.Debug(ctx, "Fee oracle proxy", "addr", proxy.Hex(), "tx", tx.Hash().Hex(), "chain_id", chainID)
 
 	receipt, err := backend.WaitMined(ctx, tx)
 	if err != nil {