cd99b79d5d8d8eaac53e956f180d823dd29c8e41
corver <29249923+corverroos@users.noreply.github.com>
2024-12-03 15:57:59 +0200

    ci(e2e/admin): allow more mainnet operators (#2611)
    Allow more mainnet operators.
    
    issue: none

--- e2e/app/admin/allowoperator.go
+++ e2e/app/admin/allowoperator.go
@@ -28,8 +28,15 @@ var allowedOperators = map[netconf.ID][]common.Address{
 		common.HexToAddress("0x44Fb4c265E551139e4D3956Aba6fe2DEa27AE4De"), // Finoa
 	},
 	netconf.Mainnet: {
-		common.HexToAddress("0x85bD563C4636a6006BCFB3fd367757676e2Dd269"), // RHINO
 		common.HexToAddress("0xc6A510df0F6d2D8CEbD6112FB3132aba4bAc23d1"), // A41
+		// common.HexToAddress(""), // BlockDaemon
+		common.HexToAddress("0x85bD563C4636a6006BCFB3fd367757676e2Dd269"), // RHINO
+		common.HexToAddress("0x7E634AC3186E9ef6fd36C1207bb87445Fd5CAA7d"), // Kingnodes
+		// common.HexToAddress(""), // Galaxy
+		// common.HexToAddress(""), // Chorusone
+		common.HexToAddress("0xd456Db9730a346211797D42de2272f2406936194"), // P-OPS
+		// common.HexToAddress(""), // DAIC
+		common.HexToAddress("0x231a7A2872208A3e3EF75Ae6580a16308D60706b"), // Finoa
 	},
 }
 
