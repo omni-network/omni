03906148090efa4f475a6224bf28fa965c01a2ca
Tsvetan Dimitrov <tsvetan.dimitrov23@gmail.com>
2025-01-14 14:25:59 +0200

    fix: minor improvement

--- cli/cmd/initnodes.go
+++ cli/cmd/initnodes.go
@@ -121,7 +121,11 @@ func InitNodes(ctx context.Context, cfg InitConfig) error {
 		}
 	}
 
-	cfg.HaloFeatureFlags = maybeGetFeatureFlags(ctx, cfg.Network)
+	featureFlags, err := maybeGetFeatureFlags(cfg.Network)
+	if err != nil {
+		return err
+	}
+	cfg.HaloFeatureFlags = featureFlags
 
 	if cfg.FromLatestSnapshot {
 		if err := downloadSnapshot(ctx, cfg.Network, gethClientName); err != nil {
@@ -137,7 +141,7 @@ func InitNodes(ctx context.Context, cfg InitConfig) error {
 		return errors.Wrap(err, "download genesis")
 	}
 
-	err := gethInit(ctx, cfg, filepath.Join(cfg.Home, "geth"))
+	err = gethInit(ctx, cfg, filepath.Join(cfg.Home, gethClientName))
 	if err != nil {
 		return errors.Wrap(err, "init geth")
 	}
@@ -148,7 +152,7 @@ func InitNodes(ctx context.Context, cfg InitConfig) error {
 	}
 
 	err = halocmd.InitFiles(ctx, halocmd.InitConfig{
-		HomeDir:     filepath.Join(cfg.Home, "halo"),
+		HomeDir:     filepath.Join(cfg.Home, haloClientName),
 		Moniker:     cfg.Moniker,
 		Network:     cfg.Network,
 		TrustedSync: !cfg.Archive, // Don't state sync if archive
@@ -373,7 +377,7 @@ func gethInit(ctx context.Context, cfg InitConfig, dir string) error {
 
 	// Write jwtsecret file
 	{
-		secretFile := filepath.Join(dir, "geth", "jwtsecret")
+		secretFile := filepath.Join(dir, gethClientName, gethJWTSecretFileName)
 		if cmtos.FileExists(secretFile) {
 			log.Info(ctx, "Found existing geth jwtsecret file", "path", secretFile)
 		} else {
@@ -422,7 +426,7 @@ func gethInit(ctx context.Context, cfg InitConfig, dir string) error {
 func downloadSnapshot(ctx context.Context, network netconf.ID, clientName string) error {
 	gcpCloudStorageURL := fmt.Sprintf("https://storage.googleapis.com/omni-%s-snapshots/%s_data.tar.lz4", network, clientName)
 
-	log.Info(ctx, "Downloading and restoring latest snapshot...", "network", network, "client", clientName)
+	log.Info(ctx, "Downloading and restoring latest snapshot...", "url", gcpCloudStorageURL)
 	if err := downloadUntarLz4(ctx, gcpCloudStorageURL, clientName); err != nil {
 		return errors.Wrap(err, "download untar lz4 error", "client", clientName)
 	}
@@ -430,17 +434,16 @@ func downloadSnapshot(ctx context.Context, network netconf.ID, clientName string
 	return nil
 }
 
-func maybeGetFeatureFlags(ctx context.Context, network netconf.ID) feature.Flags {
-	if network.IsProtected() {
-		return make([]string, 0) // Protected networks never have feature flags
+func maybeGetFeatureFlags(network netconf.ID) (feature.Flags, error) {
+	if network != netconf.Staging {
+		return make([]string, 0), nil
 	}
 
 	var manifest types.Manifest
 	_, err := toml.Decode(string(manifests.Staging()), &manifest)
 	if err != nil {
-		log.Error(ctx, "Failed to parse the manifest", err)
-		return make([]string, 0)
+		return nil, errors.Wrap(err, "parse manifest")
 	}
 
-	return manifest.FeatureFlags
+	return manifest.FeatureFlags, nil
 }
