c2957b8772911f62960a7961f195e4ac54a32bea
corver <corver@omni.network>
2025-01-17 13:23:21 +0200

    feat(octane/evmengine): proto encode execution payload

--- octane/evmengine/keeper/keeper.go
+++ octane/evmengine/keeper/keeper.go
@@ -9,6 +9,7 @@ import (
 	"github.com/omni-network/omni/halo/comet"
 	"github.com/omni-network/omni/lib/errors"
 	"github.com/omni-network/omni/lib/ethclient"
+	"github.com/omni-network/omni/lib/feature"
 	"github.com/omni-network/omni/lib/k1util"
 	"github.com/omni-network/omni/octane/evmengine/types"
 
@@ -139,16 +140,37 @@ func (k *Keeper) RegisterProposalService(server grpc1.Server) {
 
 // parseAndVerifyProposedPayload parses and returns the proposed payload
 // if comparing it against the latest execution block succeeds.
+//
+//nolint:nestif // Will be removed with feature flag
 func (k *Keeper) parseAndVerifyProposedPayload(ctx context.Context, msg *types.MsgExecutionPayload) (engine.ExecutableData, error) {
 	// Parse the payload.
 	var payload engine.ExecutableData
-	if err := json.Unmarshal(msg.ExecutionPayload, &payload); err != nil {
-		return engine.ExecutableData{}, errors.Wrap(err, "unmarshal payload")
+	if feature.FlagProtoEVMPayload.Enabled(ctx) {
+		if msg.ExecutionPayload != nil {
+			return engine.ExecutableData{}, errors.New("legacy json payload not allowed")
+		}
+		var err error
+		payload, err = types.PayloadFromProto(msg.ExecutionPayloadDeneb)
+		if err != nil {
+			return engine.ExecutableData{}, errors.Wrap(err, "unmarshal proto payload")
+		}
+	} else {
+		if msg.ExecutionPayloadDeneb != nil {
+			return engine.ExecutableData{}, errors.New("proto payload not allowed")
+		}
+		if err := json.Unmarshal(msg.ExecutionPayload, &payload); err != nil {
+			return engine.ExecutableData{}, errors.Wrap(err, "unmarshal payload")
+		}
 	}
 
 	// Ensure no withdrawals are included in the payload.
 	if len(payload.Withdrawals) > 0 {
 		return engine.ExecutableData{}, errors.New("withdrawals not allowed in payload")
+	}
+
+	// Ensure no witness
+	if payload.ExecutionWitness != nil {
+		return engine.ExecutableData{}, errors.New("witness not allowed in payload")
 	}
 
 	// Ensure fee recipient using provider
