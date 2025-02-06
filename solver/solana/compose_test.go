071b202412e0c57135b889f0e93762af0e56d1fe
corver <corver@omni.network>
2025-02-06 09:10:31 +0200

    wip

--- solver/solana/compose_test.go
+++ solver/solana/compose_test.go
@@ -18,10 +18,12 @@ var integration = flag.Bool("integration", false, "Include integration tests")
 // dir is subdirectory to store the docker compose file and solana generated artifacts (excluded from repo).
 const dir = "compose"
 
+var v0 uint64
+
 //nolint:paralleltest // Global docker dir container
 func TestIntegration(t *testing.T) {
 	if !*integration {
-		t.Skip("skipping integration test")
+		// t.Skip("skipping integration test")
 	}
 
 	ctx := context.Background()
@@ -31,43 +33,45 @@ func TestIntegration(t *testing.T) {
 	defer stop()
 	t.Logf("Pubkey: %s", privKey0.PublicKey())
 
-	bal0, err := cl.GetBalance(ctx, privKey0.PublicKey(), rpc.CommitmentConfirmed)
-	require.NoError(t, err)
-	t.Logf("Balance: %d lamports, %d sol", bal0.Value, bal0.Value/solana.LAMPORTS_PER_SOL)
+	t.Run("prefunded 500M SOL", func(t *testing.T) {
+		bal0, err := cl.GetBalance(ctx, privKey0.PublicKey(), rpc.CommitmentConfirmed)
+		require.NoError(t, err)
+		t.Logf("Balance: %d lamports, %d sol", bal0.Value, bal0.Value/solana.LAMPORTS_PER_SOL)
 
-	height, err := cl.GetBlockHeight(ctx, rpc.CommitmentConfirmed)
-	require.NoError(t, err)
-	t.Logf("Finalized Block: %d", height)
-
-	slot, err := cl.GetSlot(ctx, rpc.CommitmentConfirmed)
-	require.NoError(t, err)
-	t.Logf("Processed Slot: %d", slot)
-
-	privKey1, err := solana.NewRandomPrivateKey()
-	require.NoError(t, err)
-
-	bal1, err := cl.GetBalance(ctx, privKey1.PublicKey(), rpc.CommitmentConfirmed)
-	require.NoError(t, err)
-	t.Logf("Balance: %d lamports, %d sol", bal1.Value, bal1.Value/solana.LAMPORTS_PER_SOL)
-
-	txSig, err := cl.RequestAirdrop(ctx, privKey1.PublicKey(), solana.LAMPORTS_PER_SOL, rpc.CommitmentConfirmed)
-	require.NoError(t, err)
-
-	var v0 uint64
-	tx, err := cl.GetTransaction(ctx, txSig, &rpc.GetTransactionOpts{
-		Encoding:                       solana.EncodingBase64,
-		Commitment:                     rpc.CommitmentConfirmed,
-		MaxSupportedTransactionVersion: &v0,
+		require.Equal(t, bal0.Value, solana.LAMPORTS_PER_SOL*500_000_000)
 	})
-	require.NoError(t, err)
-	t.Logf("Airdrop Tx: slot=%d, time=%v, sig=%v", tx.Slot, tx.BlockTime, txSig)
 
-	slot, err = cl.GetSlot(ctx, rpc.CommitmentConfirmed)
-	require.NoError(t, err)
-	t.Logf("Finalized Slot: %d", slot)
+	t.Run("airdrop new key", func(t *testing.T) {
+		privKey1, err := solana.NewRandomPrivateKey()
+		require.NoError(t, err)
 
-	bal1, err = cl.GetBalance(ctx, privKey1.PublicKey(), rpc.CommitmentConfirmed)
-	require.NoError(t, err)
-	t.Logf("Balance: %d lamports, %d sol", bal1.Value, bal1.Value/solana.LAMPORTS_PER_SOL)
-	time.Sleep(time.Minute * 10)
+		bal1, err := cl.GetBalance(ctx, privKey1.PublicKey(), rpc.CommitmentConfirmed)
+		require.NoError(t, err)
+		t.Logf("Balance: %d lamports, %d sol", bal1.Value, bal1.Value/solana.LAMPORTS_PER_SOL)
+		require.Zero(t, bal1.Value)
+
+		const airdropVal = solana.LAMPORTS_PER_SOL // 1 SOL
+		txSig, err := cl.RequestAirdrop(ctx, privKey1.PublicKey(), airdropVal, rpc.CommitmentConfirmed)
+		require.NoError(t, err)
+
+		require.Eventually(t, func() bool {
+			tx, err := cl.GetTransaction(ctx, txSig, &rpc.GetTransactionOpts{
+				Encoding:                       solana.EncodingBase64,
+				Commitment:                     rpc.CommitmentConfirmed,
+				MaxSupportedTransactionVersion: &v0,
+			})
+			if err != nil {
+				return false
+			}
+			t.Logf("Airdrop Tx: slot=%d, time=%v, sig=%v", tx.Slot, tx.BlockTime, txSig)
+			return true
+		}, time.Second*10, time.Second)
+
+		bal1, err = cl.GetBalance(ctx, privKey1.PublicKey(), rpc.CommitmentConfirmed)
+		require.NoError(t, err)
+		t.Logf("Balance: %d lamports, %d sol", bal1.Value, bal1.Value/solana.LAMPORTS_PER_SOL)
+
+		require.EqualValues(t, bal1.Value, airdropVal)
+	})
+
 }
