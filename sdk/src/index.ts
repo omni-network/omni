19a3c0ea4c3e73878497bce892edef4a9868cb7e
ga-reth <hi@gareth-v.com>
2025-02-07 21:03:30 +0000

    feat(sdk): intial order sdk logic (#2995)
    Initial order SDK logic and setup for build
    
    issue: none

---
+++ sdk/src/index.ts
@@ -0,0 +1,6 @@
+////// CONSTS //////
+export { inbox, outbox } from './constants/contracts.js'
+export { typeHash } from './constants/typehash.js'
+
+////// TYPES //////
+export type { Order } from './types/order.js'
