25dc3fd3195154b098d1706593ed8f192ca770a1
corver <corver@omni.network>
2025-02-07 10:40:33 +0200

    chore(solver/solana): add more test cases

--- solver/solana/compose_test.go
+++ solver/solana/compose_test.go
@@ -9,6 +9,7 @@ import (
 	solcompose "github.com/omni-network/omni/solver/solana"
 
 	"github.com/gagliardetto/solana-go"
+	"github.com/gagliardetto/solana-go/programs/memo"
 	"github.com/gagliardetto/solana-go/rpc"
 	"github.com/stretchr/testify/require"
 )
@@ -23,7 +24,7 @@ var v0 uint64
 //nolint:paralleltest // Global docker dir container
 func TestIntegration(t *testing.T) {
 	if !*integration {
-		// t.Skip("skipping integration test")
+		t.Skip("skipping integration test")
 	}
 
 	ctx := context.Background()
@@ -38,7 +39,7 @@ func TestIntegration(t *testing.T) {
 		require.NoError(t, err)
 		t.Logf("Balance: %d lamports, %d sol", bal0.Value, bal0.Value/solana.LAMPORTS_PER_SOL)
 
-		require.Equal(t, bal0.Value, solana.LAMPORTS_PER_SOL*500_000_000)
+		require.Equal(t, solana.LAMPORTS_PER_SOL*500_000_000, bal0.Value)
 	})
 
 	t.Run("airdrop new key", func(t *testing.T) {
@@ -63,7 +64,9 @@ func TestIntegration(t *testing.T) {
 			if err != nil {
 				return false
 			}
+
 			t.Logf("Airdrop Tx: slot=%d, time=%v, sig=%v", tx.Slot, tx.BlockTime, txSig)
+
 			return true
 		}, time.Second*10, time.Second)
 
@@ -71,7 +74,81 @@ func TestIntegration(t *testing.T) {
 		require.NoError(t, err)
 		t.Logf("Balance: %d lamports, %d sol", bal1.Value, bal1.Value/solana.LAMPORTS_PER_SOL)
 
-		require.EqualValues(t, bal1.Value, airdropVal)
+		require.EqualValues(t, airdropVal, bal1.Value)
 	})
 
+	t.Run("send memo", func(t *testing.T) {
+		// Ensure memo program is deployed.
+		memoAccount, err := cl.GetAccountInfo(ctx, memo.ProgramID)
+		require.NoError(t, err)
+		require.NotNil(t, memoAccount.Value)
+		require.True(t, memoAccount.Value.Executable)
+
+		recent, err := cl.GetLatestBlockhash(ctx, rpc.CommitmentFinalized)
+		require.NoError(t, err)
+
+		msg1 := []byte("Hello, Solana!")
+		msg2 := []byte("Hello, Omni!")
+		tx, err := solana.NewTransaction(
+			[]solana.Instruction{
+				memo.NewMemoInstruction(msg1, privKey0.PublicKey()).Build(),
+				memo.NewMemoInstruction(msg2, privKey0.PublicKey()).Build(),
+			},
+			recent.Value.Blockhash,
+			solana.TransactionPayer(privKey0.PublicKey()),
+		)
+		require.NoError(t, err)
+
+		sigs, err := tx.Sign(func(pub solana.PublicKey) *solana.PrivateKey {
+			require.Equal(t, privKey0.PublicKey(), pub)
+			return &privKey0
+		})
+		require.NoError(t, err)
+		require.Len(t, sigs, 1)
+
+		txSig, err := cl.SendTransactionWithOpts(ctx, tx, rpc.TransactionOpts{
+			SkipPreflight: true, // This fails with Memo Program not deployed if false :/
+		})
+		require.NoError(t, err)
+
+		require.Eventually(t, func() bool {
+			tx, err := cl.GetTransaction(ctx, txSig, &rpc.GetTransactionOpts{
+				Encoding:                       solana.EncodingBase64,
+				Commitment:                     rpc.CommitmentConfirmed,
+				MaxSupportedTransactionVersion: &v0,
+			})
+			if err != nil {
+				return false
+			}
+			t.Logf("Memo Tx: slot=%d, time=%v, sig=%v, logs=%#v", tx.Slot, tx.BlockTime, txSig, tx.Meta.LogMessages)
+
+			return true
+		}, time.Second*10, time.Second)
+
+		// Get above Memo Tx (using sender address), and assert memo contains messages.
+		txSigs, err := cl.GetSignaturesForAddressWithOpts(ctx, privKey0.PublicKey(), &rpc.GetSignaturesForAddressOpts{
+			Limit:      ptr(1),
+			Commitment: rpc.CommitmentConfirmed,
+		})
+		require.NoError(t, err)
+		require.Len(t, txSigs, 1)
+		require.Equal(t, txSig, txSigs[0].Signature)
+		require.Contains(t, *txSigs[0].Memo, string(msg1))
+		require.Contains(t, *txSigs[0].Memo, string(msg2))
+
+		// Getting sigs by memo program address results in same txSig
+		memoSigs, err := cl.GetSignaturesForAddressWithOpts(ctx, memo.ProgramID, &rpc.GetSignaturesForAddressOpts{
+			Limit:      ptr(10),
+			Commitment: rpc.CommitmentConfirmed,
+		})
+		require.NoError(t, err)
+		require.Len(t, memoSigs, 1)
+		require.Equal(t, txSig, memoSigs[0].Signature)
+		require.Contains(t, *memoSigs[0].Memo, string(msg1))
+		require.Contains(t, *memoSigs[0].Memo, string(msg2))
+	})
+}
+
+func ptr[A any](a A) *A {
+	return &a
 }
