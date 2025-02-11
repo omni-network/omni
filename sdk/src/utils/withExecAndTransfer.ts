94a9f194cf548eeef7566d9a607a6739f696ba12
ga-reth <hi@gareth-v.com>
2025-02-11 17:43:39 +0000

    feat(sdk): withExecAndTransfer call constructor (#3033)
    Introduce `ExecutionCall` to enable support for execution via middleman
    contract, without leaking implementation to users (mostly motivated by
    the fact the rp gist is incorrect right now without this). Ideally, we'd
    infer this rather than bloating the interface, but onBehalfOf
    functionality is different across protocols.
    
    issue: none

---
+++ sdk/src/utils/withExecAndTransfer.ts
@@ -0,0 +1,50 @@
+import { type Address, encodeFunctionData } from 'viem'
+import { middleman } from '../constants/contracts.js'
+import type { Call } from '../types/order.js'
+
+type WithExecAndTransferParams = {
+  readonly call: Call
+  readonly transfer: {
+    readonly to: Address
+    readonly token: Address
+  }
+}
+
+/**
+ * @description Constructs a call to be executed via a middleware contract, that executes the input
+ * call and transfers the specified token to the recipient.
+ *
+ * @param params - Parameters for the middleware call.
+ * @returns Call
+ *
+ * @example
+ *
+ * const callWithExecAndTransfer = withExecAndTransfer({
+ *  call: {
+ *    target: '0x...',
+ *    abi,
+ *    value: BigInt(1),
+ *    functionName: '0x...',
+ *    value: BigInt(1),
+ *    args: ['0x...', BigInt(1)],
+ *  },
+ *  transfer: {
+ *    to: '0x...',
+ *    token: '0x...',
+ *  }
+ * })
+ */
+export function withExecAndTransfer(params: WithExecAndTransferParams): Call {
+  const { call, transfer } = params
+  const _callData = encodeFunctionData({
+    ...call,
+  })
+
+  return {
+    ...call,
+    abi: middleman.abi,
+    target: middleman.address,
+    functionName: 'executeAndTransfer',
+    args: [transfer.token, transfer.to, call.target, _callData],
+  }
+}
