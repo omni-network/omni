4f6a4908406e926928501e35da2b22efb014f7bb
Paul Le Cam <paul@omni.network>
2025-02-27 15:47:33 +0000

    Remove React plugin from Vitest config

--- sdk/vitest.config.ts
+++ sdk/vitest.config.ts
@@ -1,7 +1,5 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [react()],
   test: {},
 })
