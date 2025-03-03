077b0eb98e72b34a21b0437cce914f847bd63f0a
ga-reth <hi@gareth-v.com>
2025-03-03 17:02:59 +0000

    chore(sdk): update mock to clear

--- sdk/test/unitSetup.ts
+++ sdk/test/unitSetup.ts
@@ -1,9 +1,9 @@
 import { afterEach, beforeEach, vi } from 'vitest'
 
 beforeEach(() => {
-  vi.restoreAllMocks()
+  vi.clearAllMocks()
 })
 
 afterEach(() => {
-  vi.restoreAllMocks()
+  vi.clearAllMocks()
 })
