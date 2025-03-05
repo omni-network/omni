af137735dcb47c36a6443aa3b8e41e5be9207b69
Tsvetan Dimitrov <tsvetan.dimitrov23@gmail.com>
2025-03-05 05:23:55 -0500

    test(e2e/solve): tests covering missing branches (#3275)
    Test orders for missing branches:
    - Reject with `InvalidExpense` due to multiple native expenses.
    - Reject with `InvalidExpense` due to multiple ERC20 expenses.
    
    issue: #3177

--- e2e/solve/test.go
+++ e2e/solve/test.go
@@ -60,6 +60,10 @@ func isInsufficientInventory(o TestOrder) bool {
 	return o.RejectReason == solver.RejectInsufficientInventory.String()
 }
 
+func isInvalidExpense(o TestOrder) bool {
+	return o.RejectReason == solver.RejectInvalidExpense.String()
+}
+
 func Test(ctx context.Context, network netconf.Network, endpoints xchain.RPCEndpoints) error {
 	if network.ID != netconf.Devnet {
 		return errors.New("only devnet")
@@ -205,7 +209,7 @@ func makeOrders() []TestOrder {
 			DestChainID:   evmchain.IDMockL1,
 			Expenses:      nativeExpense(requestAmt),
 			Calls:         nativeTransferCall(requestAmt, user),
-			Deposit:       unsupportedDeposit(depositAmt),
+			Deposit:       unsupportedERC20Deposit(depositAmt),
 			ShouldReject:  true,
 			RejectReason:  solver.RejectUnsupportedDeposit.String(),
 		})
@@ -311,9 +315,34 @@ func makeOrders() []TestOrder {
 		FillDeadline:  time.Now().Add(1 * time.Hour),
 		SourceChainID: evmchain.IDMockL1,
 		DestChainID:   evmchain.IDMockL2,
-		Expenses:      invalidExpense(),
+		Expenses:      invalidExpenseOutOfBounds(),
 		Calls:         nativeTransferCall(big.NewInt(1), users[0]),
 		Deposit:       erc20Deposit(big.NewInt(1), addrs.Token),
+		ShouldReject:  true,
+		RejectReason:  solver.RejectInvalidExpense.String(),
+	})
+
+	// invalid expense with multiple native expenses
+	orders = append(orders, TestOrder{
+		Owner:         users[0],
+		FillDeadline:  time.Now().Add(1 * time.Hour),
+		SourceChainID: evmchain.IDMockL1,
+		DestChainID:   evmchain.IDMockL2,
+		Expenses:      multipleNativeExpenses(validETHSpend),
+		Calls:         nativeTransferCall(validETHSpend, users[0]),
+		Deposit:       nativeDeposit(maxETHSpend),
+		ShouldReject:  true,
+		RejectReason:  solver.RejectInvalidExpense.String(),
+	})
+
+	// invalid expense with multiple ERC20 expenses
+	orders = append(orders, TestOrder{
+		Owner:         users[0],
+		FillDeadline:  time.Now().Add(1 * time.Hour),
+		SourceChainID: evmchain.IDMockL2,
+		DestChainID:   evmchain.IDMockL1,
+		Expenses:      multipleERC20Expenses(validETHSpend),
+		Deposit:       nativeDeposit(maxETHSpend),
 		ShouldReject:  true,
 		RejectReason:  solver.RejectInvalidExpense.String(),
 	})
@@ -367,7 +396,7 @@ func openAll(ctx context.Context, backends ethbackend.Backends, orders []TestOrd
 	var eg errgroup.Group
 	for _, order := range orders {
 		eg.Go(func() error {
-			if isSrcChainInvalid(order) || isDepositTokenInvalid(order) || srcAndDestChainAreSame(order) || isInsufficientInventory(order) {
+			if isInvalidExpense(order) || isSrcChainInvalid(order) || isDepositTokenInvalid(order) || srcAndDestChainAreSame(order) || isInsufficientInventory(order) {
 				return nil
 			}
 
@@ -436,7 +465,6 @@ func testCheckAPI(ctx context.Context, backends ethbackend.Backends, orders []Te
 		if err != nil {
 			return errors.Wrap(err, "do request")
 		}
-
 		if resp.StatusCode != http.StatusOK {
 			return errors.New("bad status", "status", resp.StatusCode)
 		}
