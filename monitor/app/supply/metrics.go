47dbfc8a71bcb0f185d833e951950a5479a9c3fc
Christian Müller <christian@omni.network>
2025-03-10 14:10:41 +0100

    naming

--- monitor/app/supply/metrics.go
+++ monitor/app/supply/metrics.go
@@ -9,21 +9,21 @@ var (
 	cChainSupply = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: "monitor",
 		Subsystem: "supply",
-		Name:      "supply_cchain_gwei",
+		Name:      "cchain_total_gwei",
 		Help:      "Token supply on the consensus chain in gwei",
 	})
 
 	eChainSupply = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: "monitor",
 		Subsystem: "supply",
-		Name:      "total_supply_erc20_gwei",
+		Name:      "l1_erc20_total_gwei",
 		Help:      "ERC20 token supply on Ethereum chain in gwei",
 	})
 
 	bridgeBalance = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: "monitor",
 		Subsystem: "supply",
-		Name:      "bridge_balance_gwei",
+		Name:      "l1_bridge_balance_gwei",
 		Help:      "ERC20 token balance of the bridge contract on Ethereum in gwei",
 	})
 )
