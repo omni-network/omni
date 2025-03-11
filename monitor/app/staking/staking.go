5f15df53e1ca5352e7e465bbbf525af48bdc597c
Christian Müller <christian@omni.network>
2025-03-11 08:16:49 +0100

    Revert "fix(monitor): revert staking metrics (#3351)"
    This reverts commit 0b76998f0c77d1068ad857ad359d0d71a97054a4.

--- monitor/app/staking/staking.go
+++ monitor/app/staking/staking.go
@@ -7,10 +7,15 @@ import (
 
 	"github.com/omni-network/omni/lib/cchain"
 	"github.com/omni-network/omni/lib/cchain/queryutil"
+	"github.com/omni-network/omni/lib/errors"
 	"github.com/omni-network/omni/lib/log"
 
 	"github.com/ethereum/go-ethereum/params"
 )
+
+// maxDelegationsForRewardsEstimation is the max number of random rewards we track across multiple blocks
+// to estimate the average effective staking rewards.
+const maxDelegationsForRewardsEstimation = 4
 
 func MonitorForever(ctx context.Context, cprov cchain.Provider) {
 	ticker := time.NewTicker(time.Hour)
@@ -28,32 +33,42 @@ func MonitorForever(ctx context.Context, cprov cchain.Provider) {
 			}
 
 			instrStakeSizes(allDelegations)
-			instrEffRewards(ctx, cprov, allDelegations)
+
+			if err := instrEffRewards(ctx, cprov, allDelegations); err != nil {
+				log.Warn(ctx, "Effective rewards intrumentation failed", err)
+			}
 		}
 	}
 }
 
 // instrEffRewards instruments effective staking rewards.
-func instrEffRewards(ctx context.Context, cprov cchain.Provider, allDelegations []queryutil.DelegationBalance) {
+func instrEffRewards(ctx context.Context, cprov cchain.Provider, allDelegations []queryutil.DelegationBalance) error {
+	delegations := allDelegations
+	// Since we have no validator commissions, we can use just a couple of random delegations to estimate rewards.
+	// Once we have validator commissions, this code needs to be removed.
+	if len(allDelegations) > maxDelegationsForRewardsEstimation {
+		delegations = allDelegations[:4]
+	}
+
 	// Collect data during multiple blocks.
 	const blocks = uint64(30)
-	rewards, ok, err := queryutil.AvgRewardsRate(ctx, cprov, allDelegations, blocks)
+	rewards, ok, err := queryutil.AvgRewardsRate(ctx, cprov, delegations, blocks)
 	if err != nil {
-		log.Warn(ctx, "Failed to get rewards rate (will retry)", err)
-		return
+		return errors.Wrap(err, "avg rewards")
 	}
 
 	if !ok {
-		return
+		return nil
 	}
 
 	rewardsF64, err := rewards.Float64()
 	if err != nil {
-		log.Warn(ctx, "Failed to convert rewards rate to float64 [BUG]", err)
-		return
+		return errors.Wrap(err, "rewards to float64 conversion")
 	}
 
 	rewardsAvg.Set(rewardsF64)
+
+	return nil
 }
 
 // instrStakeSizes delegations instruments delegations data.
