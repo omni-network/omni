1f37766094f7d337e6d1e06cef032267d7017b6d
Zodomo <zodomo@proton.me>
2025-03-18 08:47:21 -0500

    chore(contracts/core): finalize distributor changes (#3406)
    Finalizing changes to the distributor contract prior to mainnet
    deployment.
    
    issue: #3326

--- contracts/core/script/manual/genesis-staking/MainnetGenesisStake.s.sol
+++ contracts/core/script/manual/genesis-staking/MainnetGenesisStake.s.sol
@@ -44,11 +44,12 @@ contract MainnetGenesisStakeScript is Script, StdCheats {
     GenesisStake internal genesisStake = GenesisStake(0xD2639676dA3dEA5491d27DA19340556b3a7d58B8);
     MerkleDistributorWithDeadline internal merkleDistributor; // = MerkleDistributorWithDeadline(0x5B46d1fA23584c071fa5478D709A189c452Eb050);
 
-    uint256 internal endTime = block.timestamp + 30 days;
+    uint256 internal endTime = 1_746_057_599; // 2025-04-29 23:59:59 UTC
+    uint256 internal index = 7987;
     uint256 internal depositAmount = 100 ether;
-    uint256 internal rewardAmount = 100 ether;
-    bytes32[] internal leaves = new bytes32[](2);
-    bytes32[][] internal proofs = new bytes32[][](2);
+    uint256 internal rewardAmount = 843_524_630_884_109_952;
+    bytes32[] internal leaves = new bytes32[](1);
+    bytes32[][] internal proofs = new bytes32[][](1);
     bytes32 internal root;
 
     function fullDryRun() public {
@@ -67,18 +68,28 @@ contract MainnetGenesisStakeScript is Script, StdCheats {
         if (merkleDistributorDeployed) console2.log("MerkleDistributor deployed");
         else console2.log("MerkleDistributor already deployed");
 
-        _dealApproveStakeAndFund();
+        _fundRewards();
         _upgrade();
     }
 
     function deployGenesisStakeImpl() public {
         vm.startBroadcast();
-        if (msg.sender != 0xA779fC675Db318dab004Ab8D538CB320D0013F42) revert("Unintended deployer");
+        if (msg.sender != deployer) revert("Unintended deployer");
         bool deployed = _deployGenesisStakeImpl();
         vm.stopBroadcast();
 
         if (!deployed) console2.log("GenesisStake implementation already deployed");
         else console2.log("GenesisStake implementation deployed");
+    }
+
+    function deployMerkleDistributor() public {
+        vm.startBroadcast();
+        if (msg.sender != deployer) revert("Unintended deployer");
+        bool deployed = _deployMerkleDistributor();
+        vm.stopBroadcast();
+
+        if (!deployed) console2.log("MerkleDistributor already deployed");
+        else console2.log("MerkleDistributor deployed");
     }
 
     function _preventBroadcast() internal {
@@ -134,16 +145,25 @@ contract MainnetGenesisStakeScript is Script, StdCheats {
     function _prepMerkleTree() internal {
         m = new CompleteMerkle();
 
-        leaves[0] = keccak256(abi.encodePacked(uint256(0), 0xA779fC675Db318dab004Ab8D538CB320D0013F42, rewardAmount));
-        leaves[1] = keccak256(abi.encodePacked(uint256(1), address(0xb00f), rewardAmount));
+        leaves[0] = keccak256(abi.encodePacked(index, deployer, rewardAmount));
+        proofs[0].push(hex"72456ff6c5ae451e3dfe2e76a9c1ae4f316514ef6c5216cd7fc67d84ecbb6474");
+        proofs[0].push(hex"9f68cbac4e4c8977504176df4e02ee45338eaf7be24fce9e0c551da8537e68a5");
+        proofs[0].push(hex"6b47664d11f9d0de4e89cd2f3ffd553048477292ba4e045f78e1d3b0d363866f");
+        proofs[0].push(hex"1682c88343c865daf4dccf035cd064bc5ff350e0ef439d8a21afde30e5e8f117");
+        proofs[0].push(hex"6ed83d2dad1ac9577f67d641d1ca1cd5023b8703e5c839afe77ea7bf95844d3e");
+        proofs[0].push(hex"9af730042a389ed429eeaf204a31b494d76d855911ccc39f8345a7ee9c5f4e4d");
+        proofs[0].push(hex"112d14bdc992bb8b71a7179d91021ef7ebc733925ed5323a05d142af8008f28b");
+        proofs[0].push(hex"c9f2448b67173ca1a5b0564c58bf6249ae64a0d3aadd39d99d34e33f2e2e18cd");
+        proofs[0].push(hex"b2aa63149d0fca20a0586c7e291ee86fcd6a9ab7ce722d4b5d78f20325a26f60");
+        proofs[0].push(hex"aa862cee2ca1c076902205197547d8035cfc82fee22f7722630d0557c3c4f548");
+        proofs[0].push(hex"20818a4fe98b62e2c916c9caf037c10ac22e7971394b06f2dc9b64f53f8ea3ad");
+        proofs[0].push(hex"021461cfd282c77c6915461a1b8a51cdf020b129cbf561ea89f244a501f6b8b1");
+        proofs[0].push(hex"bdcdcfb79c1b7113a7fe38bb82e734e92fc20362c88fc2a0543f89ba49e01edc");
+        proofs[0].push(hex"a6e860fc7ca37e6fbb248f16250f1a3c57e70e4354921fa526372b142429b149");
 
-        proofs[0] = m.getProof(leaves, 0);
-        proofs[1] = m.getProof(leaves, 1);
+        root = hex"5b95c1e6c7fe3ebe64c4b095810a61feb5b219e91261ad181c3149ff26364c3e";
 
-        root = m.getRoot(leaves);
-
-        require(m.verifyProof(root, proofs[0], leaves[0]), "Proof 0 is invalid");
-        require(m.verifyProof(root, proofs[1], leaves[1]), "Proof 1 is invalid");
+        require(m.verifyProof(root, proofs[0], leaves[0]), "Proof is invalid");
     }
 
     function _deployMerkleDistributor() internal returns (bool) {
@@ -170,23 +190,16 @@ contract MainnetGenesisStakeScript is Script, StdCheats {
         return true;
     }
 
-    function _dealApproveStakeAndFund() internal {
-        bool isOpen = genesisStake.isOpen();
-
-        deal(address(omni), deployer, (isOpen ? depositAmount : 0) + rewardAmount);
+    function _fundRewards() internal {
+        deal(address(omni), deployer, rewardAmount);
 
         vm.startPrank(deployer);
-        if (isOpen) {
-            omni.approve(address(genesisStake), type(uint256).max);
-            genesisStake.stake(depositAmount);
-        }
-
         omni.transfer(address(merkleDistributor), rewardAmount);
         vm.stopPrank();
     }
 
     function _upgrade() internal {
         vm.prank(deployer);
-        merkleDistributor.upgradeStake(validator, 0, rewardAmount, proofs[0]);
+        merkleDistributor.upgradeStake(validator, index, rewardAmount, proofs[0]);
     }
 }
