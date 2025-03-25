7a682d925a4764eb3951b44922977fb81506c306
corver <29249923+corverroos@users.noreply.github.com>
2025-03-25 13:01:36 +0200

    chore(solver/job): implement job db (#3501)
    Implement basic solver event log job DB.
    
    issue: #3499

---
+++ solver/job/db.go
@@ -0,0 +1,150 @@
+// Package job provides a simple event log job database.
+package job
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/omni-network/omni/lib/errors"
+	"github.com/omni-network/omni/lib/umath"
+
+	"github.com/ethereum/go-ethereum/core/types"
+
+	ormv1alpha1 "cosmossdk.io/api/cosmos/orm/v1alpha1"
+	"cosmossdk.io/core/store"
+	"cosmossdk.io/orm/model/ormdb"
+	"cosmossdk.io/orm/types/ormerrors"
+	dbm "github.com/cosmos/cosmos-db"
+)
+
+// New returns a new job DB backed by the given cosmos db.
+func New(db dbm.DB) (DB, error) {
+	schema := &ormv1alpha1.ModuleSchemaDescriptor{SchemaFile: []*ormv1alpha1.ModuleSchemaDescriptor_FileEntry{
+		{Id: 1, ProtoFileName: File_solver_job_job_proto.Path()},
+	}}
+
+	storeSvc := dbStoreService{DB: db}
+
+	modDB, err := ormdb.NewModuleDB(schema, ormdb.ModuleDBOptions{KVStoreService: storeSvc})
+	if err != nil {
+		return DB{}, errors.Wrap(err, "create ormdb module db")
+	}
+
+	dbStore, err := NewJobStore(modDB)
+	if err != nil {
+		return DB{}, errors.Wrap(err, "create store")
+	}
+
+	return DB{
+		table: dbStore.JobTable(),
+	}, nil
+}
+
+// DB implements an event log job database.
+// Current iteration simply stores event logs, deduping by chainID, blockHeight, and eventIndex.
+// No specific job scheduling supported, since a single goroutine per job is assumed.
+// That goroutine simply retries until the job is done, then deletes it.
+type DB struct {
+	table JobTable
+}
+
+// All returns all jobs in the database.
+func (db DB) All(ctx context.Context) ([]*Job, error) {
+	iter, err := db.table.List(ctx, JobPrimaryKey{})
+	if err != nil {
+		return nil, errors.Wrap(err, "list all ids")
+	}
+	defer iter.Close()
+
+	var jobs []*Job
+	for iter.Next() {
+		job, err := iter.Value()
+		if err != nil {
+			return nil, errors.Wrap(err, "get value")
+		}
+
+		jobs = append(jobs, job)
+	}
+
+	return jobs, nil
+}
+
+// Delete removes a job from the database.
+func (db DB) Delete(ctx context.Context, id uint64) error {
+	err := db.table.DeleteBy(ctx, JobIdIndexKey{}.WithId(id))
+	if err != nil {
+		return errors.Wrap(err, "delete job")
+	}
+
+	return nil
+}
+
+// Insert adds a new job to the database returning the created job.
+// It is idempotent, and will not insert the same job twice, instead returning the existing job.
+// It however errors if re-inserting reorged events, this isn't supported/expected.
+func (db DB) Insert(ctx context.Context, chainID uint64, elog types.Log) (*Job, error) {
+	if j, ok, err := db.getUnique(ctx, chainID, elog.BlockNumber, elog.Index); err != nil {
+		return nil, err
+	} else if ok {
+		el, err := j.EventLog()
+		if err != nil {
+			return nil, err
+		}
+
+		if el.BlockHash != elog.BlockHash {
+			return nil, errors.New("duplicate job, but hash mismatch (reorg not supported) [BUG]")
+		}
+
+		return j, nil
+	}
+
+	bz, err := json.Marshal(elog)
+	if err != nil {
+		return nil, errors.Wrap(err, "marshal event")
+	}
+
+	index, err := umath.ToUint64(elog.Index)
+	if err != nil {
+		return nil, err
+	}
+
+	j := &Job{
+		ChainId:     chainID,
+		BlockHeight: elog.BlockNumber,
+		EventIndex:  index,
+		EventJson:   bz,
+	}
+
+	id, err := db.table.InsertReturningId(ctx, j)
+	if err != nil {
+		return nil, errors.Wrap(err, "insert job")
+	}
+	j.Id = id
+
+	return j, nil
+}
+
+func (db DB) getUnique(ctx context.Context, chainID uint64, height uint64, index uint) (*Job, bool, error) {
+	indexU64, err := umath.ToUint64(index)
+	if err != nil {
+		return nil, false, err
+	}
+
+	j, err := db.table.GetByChainIdBlockHeightEventIndex(ctx, chainID, height, indexU64)
+	if ormerrors.IsNotFound(err) {
+		return nil, false, nil
+	} else if err != nil {
+		return nil, false, errors.Wrap(err, "get unique job")
+	}
+
+	return j, true, nil
+}
+
+// dbStoreService wraps a cosmos-db instance and provides it via OpenKVStore.
+type dbStoreService struct {
+	dbm.DB
+}
+
+func (db dbStoreService) OpenKVStore(context.Context) store.KVStore {
+	return db.DB
+}
---
+++ solver/job/db_test.go
@@ -0,0 +1,75 @@
+package job_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/omni-network/omni/lib/ethclient"
+	"github.com/omni-network/omni/solver/job"
+
+	"github.com/ethereum/go-ethereum/core/types"
+
+	dbm "github.com/cosmos/cosmos-db"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDB(t *testing.T) {
+	t.Parallel()
+	db, err := job.New(dbm.NewMemDB())
+	require.NoError(t, err)
+
+	fuzzer := ethclient.NewFuzzer(0)
+	fuzzLog := func() types.Log {
+		var resp types.Log
+		fuzzer.Fuzz(&resp)
+
+		return resp
+	}
+
+	ctx := context.Background()
+	elogs := []types.Log{fuzzLog(), fuzzLog(), fuzzLog()}
+
+	for i, elog := range elogs {
+		id := uint64(i + 1)
+		j, err := db.Insert(ctx, id, elog)
+		require.NoError(t, err)
+		require.Equal(t, id, j.GetId())
+		require.Equal(t, id, j.GetChainId())
+
+		// Idempotent
+		j, err = db.Insert(ctx, id, elog)
+		require.NoError(t, err)
+		require.Equal(t, id, j.GetId())
+
+		el, err := j.EventLog()
+		require.NoError(t, err)
+		require.Equal(t, elog, el)
+
+		all, err := db.All(ctx)
+		require.NoError(t, err)
+		require.Len(t, all, i+1)
+		last, err := all[len(all)-1].EventLog()
+		require.NoError(t, err)
+		require.Equal(t, elog, last)
+
+		// Inserting duplicate with different hash fails
+		elog2 := elog
+		elog2.BlockHash[0] = ^elog2.BlockHash[0]
+		_, err = db.Insert(ctx, id, elog2)
+		require.ErrorContains(t, err, "duplicate")
+	}
+
+	for i := range len(elogs) {
+		id := uint64(i + 1)
+		err := db.Delete(ctx, id)
+		require.NoError(t, err)
+
+		all, err := db.All(ctx)
+		require.NoError(t, err)
+		require.Len(t, all, len(elogs)-i-1)
+
+		for _, j := range all {
+			require.NotEqual(t, id, j.GetId())
+		}
+	}
+}
