omni-network/omni

commit a6c0f03b951470148a379bbceb485a8173d39b2b

refactor(solver/app): refactor http handlers and types (#3223)
Refactor solver API handlers:
- single `handlerAdapator` that does all JSON (un)marshalling for all
endpoints.
- Decouple JSON errors from handler response types. Logic now returns
struct and errors (standard go).

issue: none

solver/app/handler.go
@@ -1,0 +1,153 @@
+package app
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/omni-network/omni/lib/contracts"
+	"github.com/omni-network/omni/lib/errors"
+	"github.com/omni-network/omni/lib/log"
+	"github.com/omni-network/omni/solver/types"
+)
+
+func newAPIError(err error, statusCode int) APIError {
+	return APIError{Err: err, StatusCode: statusCode}
+}
+
+// APIError wraps an error with a non-200 HTTP status code.
+type APIError struct {
+	Err        error
+	StatusCode int
+}
+
+func (e APIError) Error() string {
+	return e.Err.Error()
+}
+
+type Handler struct {
+	// Endpoint is the http endpoint path.
+	Endpoint string
+	// ZeroReq returns a zero struct pointer of the request type used for marshaling incoming requests.
+	ZeroReq func() any
+	// HandleFunc is the function that handles the request and returns a response.
+	// The request will be a pointer (same at type returned by ZeroReq).
+	// The response must be a struct and optional error.
+	HandleFunc func(context.Context, any) (any, error)
+}
+
+// newContractsHandler returns a http handler that returns the contract address for `network`.
+func newContractsHandler(addrs contracts.Addresses) Handler {
+	return Handler{
+		Endpoint: endpointContracts,
+		ZeroReq:  func() any { return nil },
+		HandleFunc: func(context.Context, any) (any, error) {
+			return types.ContractsResponse{
+				Portal:    addrs.Portal.Hex(),
+				Inbox:     addrs.SolverNetInbox.Hex(),
+				Outbox:    addrs.SolverNetOutbox.Hex(),
+				Middleman: addrs.SolverNetMiddleman.Hex(),
+			}, nil
+		},
+	}
+}
+
+// newCheckHandler returns a handler for the /check endpoint.
+// It is responsible for http request / response handling, and delegates
+// logic to a checkFunc.
+func newCheckHandler(checkFunc checkFunc) Handler {
+	return Handler{
+		Endpoint: endpointCheck,
+		ZeroReq:  func() any { return &CheckRequest{} },
+		HandleFunc: func(ctx context.Context, request any) (any, error) {
+			req, ok := request.(*CheckRequest)
+			if !ok {
+				return nil, errors.New("invalid request type [BUG]", "type", fmt.Sprintf("%T", request))
+			}
+
+			err := checkFunc(ctx, *req)
+			if r := new(RejectionError); errors.As(err, &r) {
+				return CheckResponse{
+					Rejected:          true,
+					RejectReason:      r.Reason.String(),
+					RejectDescription: r.Err.Error(),
+				}, nil
+			} else if err != nil {
+				return CheckResponse{}, err
+			}
+
+			return CheckResponse{Accepted: true}, nil
+		},
+	}
+}
+
+// newQuoteHandler returns a handler for the /quote endpoint.
+// It is responsible to http request / response handling, and delegates
+// logic to a quoteFunc.
+func newQuoteHandler(quoteFunc quoteFunc) Handler {
+	return Handler{
+		Endpoint: endpointQuote,
+		ZeroReq:  func() any { return &QuoteRequest{} },
+		HandleFunc: func(ctx context.Context, request any) (any, error) {
+			req, ok := request.(*QuoteRequest)
+			if !ok {
+				return nil, errors.New("invalid request type [BUG]", "type", fmt.Sprintf("%T", request))
+			}
+
+			return quoteFunc(ctx, *req)
+		},
+	}
+}
+
+func handlerAdapter(h Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, rr *http.Request) {
+		defer rr.Body.Close()
+		ctx := rr.Context()
+
+		req := h.ZeroReq()
+		if req == nil { //nolint:revive // noop if-block for readability
+			// Skip request unmarshalling if ZeroReq returns nil.
+		} else if err := json.NewDecoder(rr.Body).Decode(req); err != nil {
+			writeErrResponse(ctx, w, newAPIError(err, http.StatusBadRequest))
+			return
+		}
+
+		res, err := h.HandleFunc(ctx, req)
+		if err != nil {
+			writeErrResponse(ctx, w, err)
+			return
+		}
+
+		writeJSONResponse(ctx, w, http.StatusOK, res)
+	}
+}
+
+func writeErrResponse(ctx context.Context, w http.ResponseWriter, err error) {
+	statusCode := http.StatusInternalServerError
+
+	var apiErr APIError
+	if errors.As(err, &apiErr) {
+		statusCode = apiErr.StatusCode
+	}
+
+	log.DebugErr(ctx, "Serving API error", err, "status", statusCode)
+
+	writeJSONResponse(ctx, w, statusCode, JSONErrorResponse{
+		Code:    statusCode,
+		Status:  http.StatusText(statusCode),
+		Message: removeBUG(err.Error()),
+	})
+}
+
+func writeJSONResponse(ctx context.Context, w http.ResponseWriter, status int, resp any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		log.Error(ctx, "Failed writing api response [BUG]", err)
+	}
+}
+
+// removeBUG removes [BUG] from the error messages, so they are not included in responses to users.
+func removeBUG(s string) string { return strings.ReplaceAll(s, "[BUG]", "") }

solver/types/api.go
@@ -2,7 +2,6 @@ package types
 
 import (
 	"math/big"
-	"net/http"
 
 	"github.com/omni-network/omni/lib/contracts/solvernet"
 
@@ -41,21 +40,10 @@ type CheckRequest struct {
 
 // CheckResponse is the response json for the /check endpoint.
 type CheckResponse struct {
-	Accepted          bool               `json:"accepted,omitempty"`
-	Rejected          bool               `json:"rejected,omitempty"`
-	RejectReason      string             `json:"rejectReason,omitempty"`
-	RejectDescription string             `json:"rejectDescription,omitempty"`
-	Error             *JSONErrorResponse `json:"error,omitempty"`
-}
-
-var _ JSONResponse = (*CheckResponse)(nil)
-
-func (r CheckResponse) StatusCode() int {
-	if r.Error != nil {
-		return r.Error.Code
-	}
-
-	return http.StatusOK
+	Accepted          bool   `json:"accepted"`
+	Rejected          bool   `json:"rejected"`
+	RejectReason      string `json:"rejectReason"`
+	RejectDescription string `json:"rejectDescription"`
 }
 
 // QuoteRequest is the expected request body for the /api/v1/quote endpoint.
@@ -96,38 +84,16 @@ func (qu JSONQuoteUnit) Parse() QuoteUnit {
 
 // QuoteResponse is the response json for the /api/v1/quote endpoint.
 type QuoteResponse struct {
-	Deposit JSONQuoteUnit      `json:"deposit"`
-	Expense JSONQuoteUnit      `json:"expense"`
-	Error   *JSONErrorResponse `json:"error,omitempty"`
-}
-
-var _ JSONResponse = (*QuoteResponse)(nil)
-
-func (r QuoteResponse) StatusCode() int {
-	if r.Error != nil {
-		return r.Error.Code
-	}
-
-	return http.StatusOK
+	Deposit JSONQuoteUnit `json:"deposit"`
+	Expense JSONQuoteUnit `json:"expense"`
 }
 
 // ContractsResponse is the response json for the /api/vi/contracts endpoint.
 type ContractsResponse struct {
-	Portal    string             `json:"portal,omitempty"`
-	Inbox     string             `json:"inbox,omitempty"`
-	Outbox    string             `json:"outbox,omitempty"`
-	Middleman string             `json:"middleman,omitempty"`
-	Error     *JSONErrorResponse `json:"error,omitempty"`
-}
-
-var _ JSONResponse = (*ContractsResponse)(nil)
-
-func (r ContractsResponse) StatusCode() int {
-	if r.Error != nil {
-		return r.Error.Code
-	}
-
-	return http.StatusOK
+	Portal    string `json:"portal"`
+	Inbox     string `json:"inbox"`
+	Outbox    string `json:"outbox"`
+	Middleman string `json:"middleman"`
 }
 
 // JSONExpense is a json marshal-able solvernt.Expense.

solver/app/check_internal_test.go
@@ -31,7 +31,7 @@ func TestCheck(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			backends, clients := testBackends(t)
 
-			handler := newCheckHandler(newChecker(backends, solver, inbox, outbox))
+			handler := handlerAdapter(newCheckHandler(newChecker(backends, solver, inbox, outbox)))
 
 			if tt.mock != nil {
 				tt.mock(clients)
@@ -49,7 +49,7 @@ func TestCheck(t *testing.T) {
 			require.NoError(t, err)
 
 			ctx := context.Background()
-			req, err := http.NewRequestWithContext(ctx, http.MethodPost, "api/v1/check", bytes.NewBuffer(body))
+			req, err := http.NewRequestWithContext(ctx, http.MethodPost, endpointCheck, bytes.NewBuffer(body))
 			require.NoError(t, err)
 
 			rr := httptest.NewRecorder()

solver/app/quote_internal_test.go
@@ -27,9 +27,10 @@ func TestQuote(t *testing.T) {
 	omegaOMNIAddr := omniERC20(netconf.Omega).Address
 
 	tests := []struct {
-		name string
-		req  QuoteRequest
-		res  QuoteResponse
+		name   string
+		req    QuoteRequest
+		res    QuoteResponse
+		expErr *JSONErrorResponse
 	}{
 		{
 			name: "quote deposit 1 eth expense",
@@ -106,12 +107,10 @@ func TestQuote(t *testing.T) {
 				Deposit:            JSONQuoteUnit{},
 				Expense:            JSONQuoteUnit{},
 			},
-			res: QuoteResponse{
-				Error: &JSONErrorResponse{
-					Code:    http.StatusBadRequest,
-					Status:  http.StatusText(http.StatusBadRequest),
-					Message: "deposit and expense amount cannot be both zero or both non-zero",
-				},
+			expErr: &JSONErrorResponse{
+				Code:    http.StatusBadRequest,
+				Status:  http.StatusText(http.StatusBadRequest),
+				Message: "deposit and expense amount cannot be both zero or both non-zero",
 			},
 		},
 		{
@@ -122,12 +121,10 @@ func TestQuote(t *testing.T) {
 				Deposit:            JSONQuoteUnit{Amount: parseInt("1000000000000000000")},
 				Expense:            JSONQuoteUnit{Amount: parseInt("1000000000000000000")},
 			},
-			res: QuoteResponse{
-				Error: &JSONErrorResponse{
-					Code:    http.StatusBadRequest,
-					Status:  http.StatusText(http.StatusBadRequest),
-					Message: "deposit and expense amount cannot be both zero or both non-zero",
-				},
+			expErr: &JSONErrorResponse{
+				Code:    http.StatusBadRequest,
+				Status:  http.StatusText(http.StatusBadRequest),
+				Message: "deposit and expense amount cannot be both zero or both non-zero",
 			},
 		},
 		{
@@ -138,12 +135,10 @@ func TestQuote(t *testing.T) {
 				Deposit:            JSONQuoteUnit{Token: common.HexToAddress("0x1234")},
 				Expense:            JSONQuoteUnit{Amount: parseInt("1000000000000000000")},
 			},
-			res: QuoteResponse{
-				Error: &JSONErrorResponse{
-					Code:    http.StatusNotFound,
-					Status:  http.StatusText(http.StatusNotFound),
-					Message: "unsupported deposit token",
-				},
+			expErr: &JSONErrorResponse{
+				Code:    http.StatusNotFound,
+				Status:  http.StatusText(http.StatusNotFound),
+				Message: "unsupported deposit token",
 			},
 		},
 		{
@@ -154,12 +149,10 @@ func TestQuote(t *testing.T) {
 				Deposit:            JSONQuoteUnit{Amount: parseInt("1000000000000000000")},
 				Expense:            JSONQuoteUnit{Token: common.HexToAddress("0x1234")},
 			},
-			res: QuoteResponse{
-				Error: &JSONErrorResponse{
-					Code:    http.StatusNotFound,
-					Status:  http.StatusText(http.StatusNotFound),
-					Message: "unsupported expense token",
-				},
+			expErr: &JSONErrorResponse{
+				Code:    http.StatusNotFound,
+				Status:  http.StatusText(http.StatusNotFound),
+				Message: "unsupported expense token",
 			},
 		},
 		{
@@ -170,12 +163,10 @@ func TestQuote(t *testing.T) {
 				Deposit:            JSONQuoteUnit{},
 				Expense:            JSONQuoteUnit{Amount: parseInt("1000000000000000000")},
 			},
-			res: QuoteResponse{
-				Error: &JSONErrorResponse{
-					Code:    http.StatusBadRequest,
-					Status:  http.StatusText(http.StatusBadRequest),
-					Message: "InvalidDeposit: deposit token must match expense token",
-				},
+			expErr: &JSONErrorResponse{
+				Code:    http.StatusBadRequest,
+				Status:  http.StatusText(http.StatusBadRequest),
+				Message: "InvalidDeposit: deposit token must match expense token",
 			},
 		},
 		{
@@ -186,32 +177,37 @@ func TestQuote(t *testing.T) {
 				Deposit:            JSONQuoteUnit{Amount: parseInt("1000000000000000000")},
 				Expense:            JSONQuoteUnit{},
 			},
-			res: QuoteResponse{
-				Error: &JSONErrorResponse{
-					Code:    http.StatusBadRequest,
-					Status:  http.StatusText(http.StatusBadRequest),
-					Message: "InvalidDeposit: deposit and expense must be of the same chain class (e.g. mainnet, testnet)",
-				},
+			expErr: &JSONErrorResponse{
+				Code:    http.StatusBadRequest,
+				Status:  http.StatusText(http.StatusBadRequest),
+				Message: "InvalidDeposit: deposit and expense must be of the same chain class (e.g. mainnet, testnet)",
 			},
 		},
 	}
 	for _, tt := range tests {
-		handler := newQuoteHandler(quoter)
+		handler := handlerAdapter(newQuoteHandler(quoter))
 
 		body, err := json.Marshal(tt.req)
 		require.NoError(t, err)
 
 		ctx := context.Background()
-		req, err := http.NewRequestWithContext(ctx, http.MethodPost, "api/v1/quote", bytes.NewBuffer(body))
+		req, err := http.NewRequestWithContext(ctx, http.MethodPost, endpointQuote, bytes.NewBuffer(body))
 		require.NoError(t, err)
 
 		rr := httptest.NewRecorder()
 		handler.ServeHTTP(rr, req)
 
-		var res QuoteResponse
+		var res struct {
+			QuoteResponse
+			JSONErrorResponse
+		}
 		require.NoError(t, json.NewDecoder(rr.Body).Decode(&res))
-		require.Equal(t, tt.res.StatusCode(), rr.Code)
-		require.Equal(t, tt.res, res)
+		if rr.Code != http.StatusOK {
+			require.Equal(t, res.Code, rr.Code)
+		} else {
+			require.Empty(t, res.Code)
+		}
+		require.Equal(t, tt.res, res.QuoteResponse)
 	}
 }
 

solver/app/app.go
@@ -50,7 +50,7 @@ func Run(ctx context.Context, cfg Config) error {
 	// if mainnet, just run monitoring and api (/live only)
 	if cfg.Network == netconf.Mainnet {
 		log.Info(ctx, "Serving API", "address", cfg.APIAddr)
-		apiChan := serveAPI(cfg.APIAddr, map[string]http.Handler{})
+		apiChan := serveAPI(cfg.APIAddr)
 
 		select {
 		case <-ctx.Done():
@@ -119,11 +119,11 @@ func Run(ctx context.Context, cfg Config) error {
 	}
 
 	log.Info(ctx, "Serving API", "address", cfg.APIAddr)
-	apiChan := serveAPI(cfg.APIAddr, map[string]http.Handler{
-		"/api/v1/quote":     newQuoteHandler(quoter),
-		"/api/v1/check":     newCheckHandler(newChecker(backends, solverAddr, addrs.SolverNetInbox, addrs.SolverNetOutbox)),
-		"/api/v1/contracts": newContractsHandler(addrs),
-	})
+	apiChan := serveAPI(cfg.APIAddr,
+		newCheckHandler(newChecker(backends, solverAddr, addrs.SolverNetInbox, addrs.SolverNetOutbox)),
+		newContractsHandler(addrs),
+		newQuoteHandler(quoter),
+	)
 
 	if err := approveOutboxes(ctx, network, backends, solverAddr); err != nil {
 		return errors.Wrap(err, "approve outboxes")

solver/app/check.go
@@ -2,16 +2,13 @@ package app
 
 import (
 	"context"
-	"encoding/json"
 	"math/big"
-	"net/http"
 
 	"github.com/omni-network/omni/contracts/bindings"
 	"github.com/omni-network/omni/lib/contracts/solvernet"
 	"github.com/omni-network/omni/lib/errors"
 	"github.com/omni-network/omni/lib/ethclient"
 	"github.com/omni-network/omni/lib/ethclient/ethbackend"
-	"github.com/omni-network/omni/lib/log"
 	"github.com/omni-network/omni/solver/types"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
@@ -119,48 +116,6 @@ func getFillOriginData(req CheckRequest) ([]byte, error) {
 	}
 
 	return fillOriginDataBz, nil
-}
-
-// newCheckHandler returns a handler for the /check endpoint.
-// It is responsible for http request / response handling, and delegates
-// logic to a checkFunc.
-func newCheckHandler(checkFunc checkFunc) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, rr *http.Request) {
-		ctx := rr.Context()
-
-		w.Header().Set("Content-Type", "application/json")
-
-		writeError := func(statusCode int, err error) {
-			log.DebugErr(ctx, "Error handling /check request", err)
-
-			writeJSON(ctx, w, CheckResponse{
-				Error: &JSONErrorResponse{
-					Code:    statusCode,
-					Status:  http.StatusText(statusCode),
-					Message: removeBUG(err.Error()),
-				},
-			})
-		}
-
-		var req CheckRequest
-		if err := json.NewDecoder(rr.Body).Decode(&req); err != nil {
-			writeError(http.StatusBadRequest, errors.Wrap(err, "decode request"))
-			return
-		}
-
-		err := checkFunc(ctx, req)
-		if r := new(RejectionError); errors.As(err, &r) { // RejectionError
-			writeJSON(ctx, w, CheckResponse{
-				Rejected:          true,
-				RejectReason:      r.Reason.String(),
-				RejectDescription: r.Err.Error(),
-			})
-		} else if err != nil { // Error
-			writeError(http.StatusInternalServerError, err)
-		} else {
-			writeJSON(ctx, w, CheckResponse{Accepted: true}) // Success
-		}
-	})
 }
 
 // coversQuote checks if `deposits` match or exceed a `quote` for expenses.

solver/app/quote.go
@@ -1,7 +1,7 @@
 package app
 
 import (
-	"encoding/json"
+	"context"
 	"math/big"
 	"net/http"
 
@@ -17,26 +17,20 @@ type (
 	QuoteResponse = types.QuoteResponse
 	QuoteUnit     = types.QuoteUnit
 
-	quoteFunc func(QuoteRequest) QuoteResponse
+	quoteFunc func(context.Context, QuoteRequest) (QuoteResponse, error)
 )
 
 // quoter is quoteFunc that can be used to quoter an expense or deposit.
 // It is the logic behind the /quoter endpoint.
-func quoter(req QuoteRequest) QuoteResponse {
+func quoter(_ context.Context, req QuoteRequest) (QuoteResponse, error) {
 	deposit := req.Deposit.Parse()
 	expense := req.Expense.Parse()
 
 	isDepositQuote := deposit.Amount == nil || deposit.Amount.Sign() == 0
 	isExpenseQuote := expense.Amount == nil || expense.Amount.Sign() == 0
 
-	returnErr := func(code int, msg string) QuoteResponse {
-		return QuoteResponse{
-			Error: &JSONErrorResponse{
-				Code:    code,
-				Status:  http.StatusText(code),
-				Message: msg,
-			},
-		}
+	returnErr := func(code int, msg string) (QuoteResponse, error) {
+		return QuoteResponse{}, newAPIError(errors.New(msg), code)
 	}
 
 	if isDepositQuote == isExpenseQuote {
@@ -72,7 +66,7 @@ func quoter(req QuoteRequest) QuoteResponse {
 			return returnErr(http.StatusBadRequest, err.Error())
 		}
 
-		return returnQuote(quoted.Amount, expense.Amount)
+		return returnQuote(quoted.Amount, expense.Amount), nil
 	}
 
 	quoted, err := QuoteExpense(expenseTkn, Payment{Token: depositTkn, Amount: deposit.Amount})
@@ -80,34 +74,7 @@ func quoter(req QuoteRequest) QuoteResponse {
 		return returnErr(http.StatusBadRequest, err.Error())
 	}
 
-	return returnQuote(deposit.Amount, quoted.Amount)
-}
-
-// newQuoteHandler returns a handler for the /quote endpoint.
-// It is responsible to http request / response handling, and delegates
-// logic to a quoteFunc.
-func newQuoteHandler(quoteFunc quoteFunc) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, rr *http.Request) {
-		ctx := rr.Context()
-
-		w.Header().Set("Content-Type", "application/json")
-
-		var req QuoteRequest
-		if err := json.NewDecoder(rr.Body).Decode(&req); err != nil {
-			writeJSON(ctx, w, QuoteResponse{
-				Error: &JSONErrorResponse{
-					Code:    http.StatusBadRequest,
-					Status:  http.StatusText(http.StatusBadRequest),
-					Message: err.Error(),
-				},
-			})
-
-			return
-		}
-
-		res := quoteFunc(req)
-		writeJSON(ctx, w, res)
-	})
+	return returnQuote(deposit.Amount, quoted.Amount), nil
 }
 
 // getQuote returns payment in `depositTkns` required to pay for `expenses`.

commit 6f6afdc0222c4241c859d5081fddd76860b64bd8

fix(solver/app): revert to nested API error json (#3537)
Revert solver API errors to nested `{"error":{...}}` which was
introduced in #3223.

Also add some golden files to prevent accidental issues like this.

issue: none

lib/tutil/golden.go
@@ -8,6 +8,7 @@ import (
 	"flag"
 	"os"
 	"path"
+	"path/filepath"
 	"strings"
 	"sync"
 	"testing"
@@ -48,7 +49,7 @@ func RequireGoldenBytes(t *testing.T, data []byte, opts ...func(*string)) {
 			})
 		}
 
-		require.NoError(t, os.MkdirAll("testdata", 0o755))
+		require.NoError(t, os.MkdirAll(filepath.Dir(filename), 0o755))
 
 		_ = os.Remove(filename)
 		require.NoError(t, os.WriteFile(filename, data, 0o644))

solver/app/handler.go
@@ -152,9 +152,11 @@ func writeErrResponse(ctx context.Context, w http.ResponseWriter, err error) {
 	log.DebugErr(ctx, "Serving API error", err, "status", statusCode)
 
 	writeJSONResponse(ctx, w, statusCode, types.JSONErrorResponse{
-		Code:    statusCode,
-		Status:  http.StatusText(statusCode),
-		Message: removeBUG(err.Error()),
+		Error: types.JSONError{
+			Code:    statusCode,
+			Status:  http.StatusText(statusCode),
+			Message: removeBUG(err.Error()),
+		},
 	})
 }
 

solver/app/internal_testutils.go
@@ -52,6 +52,7 @@ type orderTestCase struct {
 	shouldErr    bool
 	mock         func(clients MockClients)
 	order        testOrder
+	testdata     bool
 }
 
 // rejectTestCase is a test case for shouldReject(...)
@@ -73,6 +74,7 @@ type checkTestCase struct {
 	mock         func(clients MockClients)
 	req          types.CheckRequest
 	res          types.CheckResponse
+	testdata     bool
 }
 
 func toCheckTestCase(t *testing.T, tt orderTestCase) checkTestCase {
@@ -91,6 +93,7 @@ func toCheckTestCase(t *testing.T, tt orderTestCase) checkTestCase {
 		name:         tt.name,
 		mock:         tt.mock,
 		disallowCall: tt.disallowCall,
+		testdata:     tt.testdata,
 		req: types.CheckRequest{
 			SourceChainID:      tt.order.srcChainID,
 			DestinationChainID: tt.order.dstChainID,
@@ -275,6 +278,7 @@ func orderTestCases(t *testing.T, solver common.Address) []orderTestCase {
 			mock: func(clients MockClients) {
 				mockNativeBalance(t, clients.Client(t, evmchain.IDBaseSepolia), solver, ether(0))
 			},
+			testdata: true,
 		},
 		{
 			name:   "sufficient native balance",
@@ -292,6 +296,7 @@ func orderTestCases(t *testing.T, solver common.Address) []orderTestCase {
 			mock: func(clients MockClients) {
 				mockNativeBalance(t, clients.Client(t, evmchain.IDOmniOmega), solver, ether(1))
 			},
+			testdata: true,
 		},
 		{
 			name:   "insufficient ERC20 balance",
@@ -326,6 +331,7 @@ func orderTestCases(t *testing.T, solver common.Address) []orderTestCase {
 				mockERC20Balance(t, clients.Client(t, evmchain.IDHolesky), omegaOMNIAddr, ether(1))
 				mockERC20Allowance(t, clients.Client(t, evmchain.IDHolesky), omegaOMNIAddr)
 			},
+			testdata: true,
 		},
 		{
 			name:        "fill reverts",
@@ -445,6 +451,7 @@ func orderTestCases(t *testing.T, solver common.Address) []orderTestCase {
 				calls:    []types.Call{{Value: ether(1)}},
 				expenses: []types.Expense{{Amount: ether(1)}},
 			},
+			testdata: true,
 		},
 		{
 			name: "sufficient deposit",

solver/types/api.go
@@ -15,6 +15,10 @@ import (
 
 // JSONErrorResponse is a json response for http errors (e.g 4xx, 5xx), not used for rejections.
 type JSONErrorResponse struct {
+	Error JSONError `json:"error"`
+}
+
+type JSONError struct {
 	Code    int    `json:"code"`
 	Status  string `json:"status"`
 	Message string `json:"message"`

solver/app/check_internal_test.go
@@ -12,12 +12,15 @@ import (
 	"github.com/omni-network/omni/e2e/app/eoa"
 	"github.com/omni-network/omni/lib/contracts"
 	"github.com/omni-network/omni/lib/netconf"
+	"github.com/omni-network/omni/lib/tutil"
 	"github.com/omni-network/omni/solver/types"
 
 	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/stretchr/testify/require"
 )
+
+//go:generate go test . -run=TestCheck -golden
 
 //nolint:tparallel // subtests use same mock controller
 func TestCheck(t *testing.T) {
@@ -65,13 +68,27 @@ func TestCheck(t *testing.T) {
 			err = json.Unmarshal(respBody, &res)
 			require.NoError(t, err)
 
-			t.Logf("resp_body: %s", respBody)
-
 			require.Equal(t, tt.res.Rejected, res.Rejected)
 			require.Equal(t, tt.res.RejectReason, res.RejectReason)
 			require.Equal(t, tt.res.Accepted, res.Accepted)
 
 			clients.Finish(t)
+
+			if tt.testdata {
+				tutil.RequireGoldenBytes(t, indent(body), tutil.WithFilename(t.Name()+"/req_body.json"))
+				tutil.RequireGoldenBytes(t, indent(respBody), tutil.WithFilename(t.Name()+"/resp_body.json"))
+			}
 		})
 	}
+}
+
+// indent returns the json bytes indented.
+func indent(bz []byte) []byte {
+	var buf bytes.Buffer
+	err := json.Indent(&buf, bz, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+
+	return buf.Bytes()
 }

solver/app/quote_internal_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"io"
 	"math/big"
 	"net/http"
 	"net/http/httptest"
@@ -13,6 +14,7 @@ import (
 	"github.com/omni-network/omni/lib/bi"
 	"github.com/omni-network/omni/lib/evmchain"
 	"github.com/omni-network/omni/lib/netconf"
+	"github.com/omni-network/omni/lib/tutil"
 	"github.com/omni-network/omni/solver/types"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -97,16 +99,19 @@ func addThousandSeparators(num string) string {
 	return strings.Join(result, "")
 }
 
+//go:generate go test . -run=TestQuote -golden
+
 func TestQuote(t *testing.T) {
 	t.Parallel()
 
 	omegaOMNIAddr := omniERC20(netconf.Omega).Address
 
 	tests := []struct {
-		name   string
-		req    types.QuoteRequest
-		res    types.QuoteResponse
-		expErr *types.JSONErrorResponse
+		name     string
+		req      types.QuoteRequest
+		res      types.QuoteResponse
+		expErr   types.JSONError
+		testdata bool
 	}{
 		{
 			name: "quote deposit 1 eth expense",
@@ -120,6 +125,7 @@ func TestQuote(t *testing.T) {
 				Deposit: mockAddrAmt("1003000000000000000"),
 				Expense: mockAddrAmt("1000000000000000000"),
 			},
+			testdata: true,
 		},
 		{
 			name: "quote deposit 10 eth expense",
@@ -174,6 +180,7 @@ func TestQuote(t *testing.T) {
 				Deposit: types.AddrAmt{Amount: parseInt("10000000000000000000"), Token: omegaOMNIAddr},
 				Expense: mockAddrAmt("10000000000000000000"),
 			},
+			testdata: true,
 		},
 		{
 			name: "no deposit of expense amount specified",
@@ -183,11 +190,12 @@ func TestQuote(t *testing.T) {
 				Deposit:            zeroAddrAmt,
 				Expense:            zeroAddrAmt,
 			},
-			expErr: &types.JSONErrorResponse{
+			expErr: types.JSONError{
 				Code:    http.StatusBadRequest,
 				Status:  http.StatusText(http.StatusBadRequest),
 				Message: "deposit and expense amount cannot be both zero or both non-zero",
 			},
+			testdata: true,
 		},
 		{
 			name: "both deposit and expense amount specified",
@@ -197,7 +205,7 @@ func TestQuote(t *testing.T) {
 				Deposit:            mockAddrAmt("1000000000000000000"),
 				Expense:            mockAddrAmt("1000000000000000000"),
 			},
-			expErr: &types.JSONErrorResponse{
+			expErr: types.JSONError{
 				Code:    http.StatusBadRequest,
 				Status:  http.StatusText(http.StatusBadRequest),
 				Message: "deposit and expense amount cannot be both zero or both non-zero",
@@ -211,7 +219,7 @@ func TestQuote(t *testing.T) {
 				Deposit:            types.AddrAmt{Token: common.HexToAddress("0x1234")},
 				Expense:            mockAddrAmt("1000000000000000000"),
 			},
-			expErr: &types.JSONErrorResponse{
+			expErr: types.JSONError{
 				Code:    http.StatusNotFound,
 				Status:  http.StatusText(http.StatusNotFound),
 				Message: "unsupported deposit token",
@@ -225,7 +233,7 @@ func TestQuote(t *testing.T) {
 				Deposit:            mockAddrAmt("1000000000000000000"),
 				Expense:            types.AddrAmt{Token: common.HexToAddress("0x1234")},
 			},
-			expErr: &types.JSONErrorResponse{
+			expErr: types.JSONError{
 				Code:    http.StatusNotFound,
 				Status:  http.StatusText(http.StatusNotFound),
 				Message: "unsupported expense token",
@@ -239,7 +247,7 @@ func TestQuote(t *testing.T) {
 				Deposit:            zeroAddrAmt,
 				Expense:            mockAddrAmt("1000000000000000000"),
 			},
-			expErr: &types.JSONErrorResponse{
+			expErr: types.JSONError{
 				Code:    http.StatusBadRequest,
 				Status:  http.StatusText(http.StatusBadRequest),
 				Message: "InvalidDeposit: deposit token must match expense token",
@@ -253,38 +261,49 @@ func TestQuote(t *testing.T) {
 				Deposit:            mockAddrAmt("1000000000000000000"),
 				Expense:            zeroAddrAmt,
 			},
-			expErr: &types.JSONErrorResponse{
+			expErr: types.JSONError{
 				Code:    http.StatusBadRequest,
 				Status:  http.StatusText(http.StatusBadRequest),
 				Message: "InvalidDeposit: deposit and expense must be of the same chain class (e.g. mainnet, testnet)",
 			},
+			testdata: true,
 		},
 	}
 	for _, tt := range tests {
-		handler := handlerAdapter(newQuoteHandler(quoter))
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			handler := handlerAdapter(newQuoteHandler(quoter))
 
-		body, err := json.Marshal(tt.req)
-		require.NoError(t, err)
+			body, err := json.Marshal(tt.req)
+			require.NoError(t, err)
 
-		ctx := context.Background()
-		req, err := http.NewRequestWithContext(ctx, http.MethodPost, endpointQuote, bytes.NewBuffer(body))
-		require.NoError(t, err)
+			ctx := context.Background()
+			req, err := http.NewRequestWithContext(ctx, http.MethodPost, endpointQuote, bytes.NewBuffer(body))
+			require.NoError(t, err)
 
-		rr := httptest.NewRecorder()
-		handler.ServeHTTP(rr, req)
+			rr := httptest.NewRecorder()
+			handler.ServeHTTP(rr, req)
 
-		// Response is either a QuoteResponse or a JSONErrorResponse
-		var res struct {
-			types.QuoteResponse
-			types.JSONErrorResponse
-		}
-		require.NoError(t, json.NewDecoder(rr.Body).Decode(&res))
-		if rr.Code != http.StatusOK {
-			require.Equal(t, res.Code, rr.Code)
-		} else {
-			require.Empty(t, res.Code)
-		}
-		require.Equal(t, tt.res, res.QuoteResponse)
+			respBody, err := io.ReadAll(rr.Body)
+			require.NoError(t, err)
+
+			// Response is either a QuoteResponse or a JSONErrorResponse
+			var res struct {
+				types.QuoteResponse
+				types.JSONErrorResponse
+			}
+			require.NoError(t, json.Unmarshal(respBody, &res))
+			if rr.Code != http.StatusOK {
+				require.Equal(t, res.Error.Code, rr.Code)
+			}
+			require.Equal(t, tt.res, res.QuoteResponse)
+			require.Equal(t, tt.expErr, res.JSONErrorResponse.Error)
+
+			if tt.testdata {
+				tutil.RequireGoldenBytes(t, indent(body), tutil.WithFilename(t.Name()+"/req_body.json"))
+				tutil.RequireGoldenBytes(t, indent(respBody), tutil.WithFilename(t.Name()+"/resp_body.json"))
+			}
+		})
 	}
 }
 