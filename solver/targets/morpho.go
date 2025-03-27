3be9e4593fddbdd43fa656061557594c2a7b597c
Kevin Halliday <kevin@omni.network>
2025-03-27 12:05:29 -0400

    fix(solver/targets): fix morpho name (#3544)
    Fix morpo name
    
    issue: none

--- solver/targets/morpho.go
+++ solver/targets/morpho.go
@@ -6,7 +6,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-const NameMorpho = "Eigen"
+const NameMorpho = "Morpho"
 
 var (
 	MorphoMainnetSteakhouseVault = addr("0xbeeF010f9cb27031ad51e3333f9aF9C6B1228183")
