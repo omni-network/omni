2f5cab1a4c7c04ac90210e5ea93445d15230b11d
Christian Müller <christian@omni.network>
2025-03-28 17:23:28 +0100

    fix heights

--- octane/evmengine/keeper/abci.go
+++ octane/evmengine/keeper/abci.go
@@ -71,7 +71,7 @@ func (k *Keeper) PrepareProposal(ctx sdk.Context, req *abci.RequestPreparePropos
 	if reqHeight != height { //nolint:nestif // Not an issue
 		// Create a new payload (retrying on network errors).
 		err := retryForever(ctx, func(ctx context.Context) (bool, error) {
-			fcr, err := k.startBuild(ctx, appHash, req.Time)
+			fcr, err := k.startBuild(ctx, appHash, req.Time, req.Height)
 			if err != nil {
 				log.Warn(ctx, "Preparing proposal failed: build new evm payload (will retry)", err)
 				return false, nil // Retry
@@ -248,7 +248,7 @@ func (k *Keeper) PostFinalize(ctx sdk.Context) error {
 	log.Debug(ctx, "Starting optimistic EVM payload build", logAttr)
 
 	// No need to wrap this in retryForever since this is a best-effort optimisation, if it fails, just skip it.
-	fcr, err := k.startBuild(ctx, appHash, timestamp)
+	fcr, err := k.startBuild(ctx, appHash, timestamp, height+1)
 	if err != nil {
 		log.Warn(ctx, "Starting optimistic build failed", err, logAttr)
 		return nil
@@ -270,7 +270,7 @@ func (k *Keeper) PostFinalize(ctx sdk.Context) error {
 
 // startBuild triggers the building of a new execution payload on top of the current execution head.
 // It returns the EngineAPI response which contains a status and payload ID.
-func (k *Keeper) startBuild(ctx context.Context, appHash common.Hash, timestamp time.Time) (engine.ForkChoiceResponse, error) {
+func (k *Keeper) startBuild(ctx context.Context, appHash common.Hash, timestamp time.Time, height int64) (engine.ForkChoiceResponse, error) {
 	head, err := k.getExecutionHead(ctx)
 	if err != nil {
 		return engine.ForkChoiceResponse{}, errors.Wrap(err, "latest execution block")
@@ -300,7 +300,8 @@ func (k *Keeper) startBuild(ctx context.Context, appHash common.Hash, timestamp 
 		FinalizedBlockHash: headHash,
 	}
 
-	withdrawals, err := k.EligibleWithdrawals(ctx)
+	callCtx := sdk.UnwrapSDKContext(ctx).WithBlockHeight(height)
+	withdrawals, err := k.EligibleWithdrawals(callCtx) //nolint:contextcheck // no new ctx
 	if err != nil {
 		return engine.ForkChoiceResponse{}, errors.Wrap(err, "eligible withdrawals")
 	}
