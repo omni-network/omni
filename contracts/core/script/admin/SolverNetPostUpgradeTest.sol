9f01336face3358b0833cfc335758b9c91f5344d
Zodomo <zodomo@proton.me>
2025-03-31 14:52:16 -0500

    feat(contracts): emit OriginData event (#3571)
    SolverNetInbox now emits an `OriginData` event that emits a typed
    `FillOriginData` struct so we can analyze it in Dune. Added validation
    for this to post-upgrade tests.
    
    issue: none

--- contracts/core/script/admin/SolverNetPostUpgradeTest.sol
+++ contracts/core/script/admin/SolverNetPostUpgradeTest.sol
@@ -4,10 +4,12 @@ pragma solidity 0.8.24;
 import { Test } from "forge-std/Test.sol";
 import { VmSafe } from "forge-std/Vm.sol";
 
-import { SolverNetInbox } from "solve/src/SolverNetInbox.sol";
+import { SolverNetInbox, ISolverNetInbox } from "solve/src/SolverNetInbox.sol";
 import { SolverNetOutbox } from "solve/src/SolverNetOutbox.sol";
 import { SolverNetMiddleman } from "solve/src/SolverNetMiddleman.sol";
 import { SolverNetExecutor } from "solve/src/SolverNetExecutor.sol";
+import { IERC7683 } from "solve/src/erc7683/IERC7683.sol";
+import { SolverNet } from "solve/src/lib/SolverNet.sol";
 
 contract SolverNetPostUpgradeTest is Test {
     SolverNetInbox inbox;
@@ -15,13 +17,19 @@ contract SolverNetPostUpgradeTest is Test {
     SolverNetMiddleman middleman;
     SolverNetExecutor executor;
 
+    bytes32 constant ORDERDATA_TYPEHASH = keccak256(
+        "OrderData(address owner,uint64 destChainId,Deposit deposit,Call[] calls,TokenExpense[] expenses)Deposit(address token,uint96 amount)Call(address target,bytes4 selector,uint256 value,bytes params)TokenExpense(address spender,address token,uint96 amount)"
+    );
+
     address owner;
+    address user = makeAddr("user");
 
     function runInbox(address addr) public {
         (VmSafe.CallerMode mode,,) = vm.readCallers();
         require(mode == VmSafe.CallerMode.None, "no broadcast");
 
         _setupInbox(addr);
+        _openOrder();
     }
 
     function runOutbox(address addr) public {
@@ -61,5 +69,41 @@ contract SolverNetPostUpgradeTest is Test {
 
     function _setupExecutor(address addr) internal {
         executor = SolverNetExecutor(payable(addr));
+    }
+
+    function _openOrder() internal {
+        SolverNet.Deposit memory deposit = SolverNet.Deposit({ token: address(0), amount: 1 ether });
+
+        SolverNet.Call[] memory calls = new SolverNet.Call[](1);
+        calls[0] = SolverNet.Call({ target: user, selector: bytes4(hex"00000000"), value: 1 ether, params: "" });
+
+        SolverNet.OrderData memory orderData = SolverNet.OrderData({
+            owner: user,
+            destChainId: uint64(block.chainid == 1 ? 10 : 1),
+            deposit: deposit,
+            calls: calls,
+            expenses: new SolverNet.TokenExpense[](0)
+        });
+
+        IERC7683.OnchainCrossChainOrder memory order = IERC7683.OnchainCrossChainOrder({
+            fillDeadline: type(uint32).max,
+            orderDataType: ORDERDATA_TYPEHASH,
+            orderData: abi.encode(orderData)
+        });
+
+        bytes32 id = inbox.getNextOrderId(user);
+        SolverNet.FillOriginData memory fillOriginData = SolverNet.FillOriginData({
+            srcChainId: uint64(block.chainid),
+            destChainId: uint64(block.chainid == 1 ? 10 : 1),
+            fillDeadline: type(uint32).max,
+            calls: calls,
+            expenses: new SolverNet.TokenExpense[](0)
+        });
+
+        vm.deal(user, 1 ether);
+        vm.prank(user);
+        vm.expectEmit(address(inbox));
+        emit ISolverNetInbox.FillOriginData(id, fillOriginData);
+        inbox.open{ value: 1 ether }(order);
     }
 }
