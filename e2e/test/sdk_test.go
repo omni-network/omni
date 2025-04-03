6d4943f5b7faaecbba4e62b41c597ec14ab2a8df
Paul Le Cam <paul@omni.network>
2025-04-03 14:19:09 +0100

    chore(sdk): add integration tests for contracts

--- e2e/test/sdk_test.go
+++ e2e/test/sdk_test.go
@@ -20,12 +20,29 @@ func TestSDK(t *testing.T) {
 	maybeTestNetwork(t, skipFunc, func(ctx context.Context, t *testing.T, _ NetworkDeps) {
 		t.Helper()
 
+		solverContractsPath, err := filepath.Abs("../../contracts/solve")
+		require.NoError(t, err)
+
 		sdkPath, err := filepath.Abs("../../sdk")
 		require.NoError(t, err)
 
-		exe := func(ctx context.Context, name string, args ...string) {
+		writeABI := func(ctx context.Context, contract string) {
+			outfile, err := os.Create(filepath.Join(sdkPath, "integration-tests/assets", contract+".json"))
+			require.NoError(t, err, "failed to create ABI file for contract: %s", contract)
+			defer outfile.Close()
+
+			cmd := exec.CommandContext(ctx, "forge", "inspect", "--json", "src/"+contract+".sol:"+contract, "abi")
+			cmd.Dir = solverContractsPath
+			cmd.Stdout = outfile
+			cmd.Stderr = os.Stderr
+
+			err = cmd.Run()
+			require.NoError(t, err, "failed to write ABI file for contract: %s", contract)
+		}
+
+		exe := func(ctx context.Context, dir string, name string, args ...string) {
 			cmd := exec.CommandContext(ctx, name, args...)
-			cmd.Dir = sdkPath
+			cmd.Dir = dir
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
 
@@ -33,9 +50,14 @@ func TestSDK(t *testing.T) {
 			require.NoError(t, err, "failed to run command: %s %v", name, args)
 		}
 
-		exe(ctx, "pnpm", "install")
-		exe(ctx, "pnpm", "run", "build")
-		exe(ctx, "pnpm", "run", "test:unit")
-		exe(ctx, "pnpm", "run", "test:integration")
+		exe(ctx, solverContractsPath, "pnpm", "install")
+		writeABI(ctx, "SolverNetInbox")
+		writeABI(ctx, "SolverNetMiddleman")
+		writeABI(ctx, "SolverNetOutbox")
+
+		exe(ctx, sdkPath, "pnpm", "install")
+		exe(ctx, sdkPath, "pnpm", "run", "build")
+		exe(ctx, sdkPath, "pnpm", "run", "test:unit")
+		exe(ctx, sdkPath, "pnpm", "run", "test:integration")
 	})
 }
--- sdk/packages/react/src/constants/abis.ts
+++ sdk/packages/react/src/constants/abis.ts
@@ -19,74 +19,154 @@ export const inboxABI = [
   },
   {
     type: 'function',
-    inputs: [{ name: 'id', internalType: 'bytes32', type: 'bytes32' }],
     name: 'getOrder',
+    inputs: [
+      {
+        name: 'id',
+        type: 'bytes32',
+        internalType: 'bytes32',
+      },
+    ],
     outputs: [
       {
         name: 'resolved',
-        internalType: 'struct IERC7683.ResolvedCrossChainOrder',
         type: 'tuple',
+        internalType: 'struct IERC7683.ResolvedCrossChainOrder',
         components: [
-          { name: 'user', internalType: 'address', type: 'address' },
-          { name: 'originChainId', internalType: 'uint256', type: 'uint256' },
-          { name: 'openDeadline', internalType: 'uint32', type: 'uint32' },
-          { name: 'fillDeadline', internalType: 'uint32', type: 'uint32' },
-          { name: 'orderId', internalType: 'bytes32', type: 'bytes32' },
+          {
+            name: 'user',
+            type: 'address',
+            internalType: 'address',
+          },
+          {
+            name: 'originChainId',
+            type: 'uint256',
+            internalType: 'uint256',
+          },
+          {
+            name: 'openDeadline',
+            type: 'uint32',
+            internalType: 'uint32',
+          },
+          {
+            name: 'fillDeadline',
+            type: 'uint32',
+            internalType: 'uint32',
+          },
+          {
+            name: 'orderId',
+            type: 'bytes32',
+            internalType: 'bytes32',
+          },
           {
             name: 'maxSpent',
-            internalType: 'struct IERC7683.Output[]',
             type: 'tuple[]',
+            internalType: 'struct IERC7683.Output[]',
             components: [
-              { name: 'token', internalType: 'bytes32', type: 'bytes32' },
-              { name: 'amount', internalType: 'uint256', type: 'uint256' },
-              { name: 'recipient', internalType: 'bytes32', type: 'bytes32' },
-              { name: 'chainId', internalType: 'uint256', type: 'uint256' },
+              {
+                name: 'token',
+                type: 'bytes32',
+                internalType: 'bytes32',
+              },
+              {
+                name: 'amount',
+                type: 'uint256',
+                internalType: 'uint256',
+              },
+              {
+                name: 'recipient',
+                type: 'bytes32',
+                internalType: 'bytes32',
+              },
+              {
+                name: 'chainId',
+                type: 'uint256',
+                internalType: 'uint256',
+              },
             ],
           },
           {
             name: 'minReceived',
-            internalType: 'struct IERC7683.Output[]',
             type: 'tuple[]',
+            internalType: 'struct IERC7683.Output[]',
             components: [
-              { name: 'token', internalType: 'bytes32', type: 'bytes32' },
-              { name: 'amount', internalType: 'uint256', type: 'uint256' },
-              { name: 'recipient', internalType: 'bytes32', type: 'bytes32' },
-              { name: 'chainId', internalType: 'uint256', type: 'uint256' },
+              {
+                name: 'token',
+                type: 'bytes32',
+                internalType: 'bytes32',
+              },
+              {
+                name: 'amount',
+                type: 'uint256',
+                internalType: 'uint256',
+              },
+              {
+                name: 'recipient',
+                type: 'bytes32',
+                internalType: 'bytes32',
+              },
+              {
+                name: 'chainId',
+                type: 'uint256',
+                internalType: 'uint256',
+              },
             ],
           },
           {
             name: 'fillInstructions',
-            internalType: 'struct IERC7683.FillInstruction[]',
             type: 'tuple[]',
+            internalType: 'struct IERC7683.FillInstruction[]',
             components: [
               {
                 name: 'destinationChainId',
-                internalType: 'uint64',
                 type: 'uint64',
+                internalType: 'uint64',
               },
               {
                 name: 'destinationSettler',
-                internalType: 'bytes32',
                 type: 'bytes32',
+                internalType: 'bytes32',
               },
-              { name: 'originData', internalType: 'bytes', type: 'bytes' },
+              {
+                name: 'originData',
+                type: 'bytes',
+                internalType: 'bytes',
+              },
             ],
           },
         ],
       },
       {
         name: 'state',
-        internalType: 'struct ISolverNetInbox.OrderState',
         type: 'tuple',
+        internalType: 'struct ISolverNetInbox.OrderState',
         components: [
           {
             name: 'status',
-            internalType: 'enum ISolverNetInbox.Status',
             type: 'uint8',
+            internalType: 'enum ISolverNetInbox.Status',
           },
-          { name: 'timestamp', internalType: 'uint32', type: 'uint32' },
-          { name: 'claimant', internalType: 'address', type: 'address' },
+          {
+            name: 'rejectReason',
+            type: 'uint8',
+            internalType: 'uint8',
+          },
+          {
+            name: 'timestamp',
+            type: 'uint32',
+            internalType: 'uint32',
+          },
+          {
+            name: 'updatedBy',
+            type: 'address',
+            internalType: 'address',
+          },
         ],
+      },
+      {
+        name: 'offset',
+        type: 'uint248',
+        internalType: 'uint248',
       },
     ],
     stateMutability: 'view',
