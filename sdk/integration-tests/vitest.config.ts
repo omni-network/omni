583957b5e51f9b2e083015b8e2e711ec8161d3f8
Kevin Halliday <kevin@omni.network>
2025-04-04 09:27:13 -0400

    fix(sdk): fix open log parsing

--- sdk/integration-tests/vitest.config.ts
+++ sdk/integration-tests/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    testTimeout: 60000, // 1m, orders take time to process
     include: ['./suites/**/*.{ts,tsx}'],
     environment: 'happy-dom',
   },
