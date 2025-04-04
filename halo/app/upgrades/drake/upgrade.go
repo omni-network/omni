287ab1556e7b546a2b3199b7c5e6ce9fc089b0f7
Christian Müller <christian@omni.network>
2025-04-04 16:41:38 +0200

    add unbonding time param upgrade

--- halo/app/upgrades/drake/upgrade.go
+++ halo/app/upgrades/drake/upgrade.go
@@ -4,30 +4,58 @@
 //
 // It includes:
 // - Unstaking of funds,
-// - Processing of rewards withdrawals.
+// - Processing of rewards withdrawals,
+// - Setting the unbonding time to 0.
 package drake
 
 import (
 	"context"
 	"encoding/json"
+	"time"
 
+	"github.com/omni-network/omni/lib/errors"
 	"github.com/omni-network/omni/lib/log"
 
+	"cosmossdk.io/math"
 	storetypes "cosmossdk.io/store/types"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	skeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
+	stypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
 const UpgradeName = "3_drake"
 
 func StoreUpgrades(_ context.Context) *storetypes.StoreUpgrades {
-	return &storetypes.StoreUpgrades{}
+	return &storetypes.StoreUpgrades{
+		Added: []string{},
+	}
 }
 
-func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator) upgradetypes.UpgradeHandler {
+var (
+	// StakingParams are genesis params with UnbondingTime set to 0.
+	StakingParams = stypes.Params{
+		UnbondingTime:     0 * time.Second,
+		MaxValidators:     30,
+		MaxEntries:        7,
+		HistoricalEntries: 10_000,
+		BondDenom:         "stake",
+		MinCommissionRate: math.LegacyNewDec(0),
+	}
+)
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+	staking skeeper.Keeper,
+) upgradetypes.UpgradeHandler {
 	return func(ctx context.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		log.Info(ctx, "Running 3_drake upgrade handler")
+
+		if err := staking.SetParams(ctx, StakingParams); err != nil {
+			return nil, errors.Wrap(err, "set staking params")
+		}
 
 		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
--- halo/app/upgrades/upgrades.go
+++ halo/app/upgrades/upgrades.go
@@ -16,12 +16,14 @@ import (
 	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
 	mintkeeper "github.com/cosmos/cosmos-sdk/x/mint/keeper"
 	slkeeper "github.com/cosmos/cosmos-sdk/x/slashing/keeper"
+	skeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 )
 
 type App interface {
 	GetModuleManager() *module.Manager
 	GetModuleConfigurator() module.Configurator
 	GetSlashingKeeper() slkeeper.Keeper
+	GetStakingKeeper() skeeper.Keeper
 	GetMintKeeper() mintkeeper.Keeper
 	GetAccountKeeper() authkeeper.AccountKeeper
 }
@@ -71,6 +73,7 @@ var Upgrades = []Upgrade{
 			return drake3.CreateUpgradeHandler(
 				a.GetModuleManager(),
 				a.GetModuleConfigurator(),
+				a.GetStakingKeeper(),
 			)
 		},
 		Store:        drake3.StoreUpgrades,
