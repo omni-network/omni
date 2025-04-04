bdb35d988d60dabef7326801800138a36adfdd4e
Christian Müller <christian@omni.network>
2025-04-04 09:24:43 +0200

    adds empty drake upgrade

---
+++ halo/app/upgrades/drake/upgrade.go
@@ -0,0 +1,38 @@
+// Package drake defines the third Omni consensus chain upgrade, named after
+// Sir Francis Drake, an English explorer and privateer best known for making the
+// second circumnavigation of the world in a single expedition between 1577 and 1580.
+//
+// It includes:
+// - Unstaking of funds,
+// - Processing of rewards withdrawals.
+package drake
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/omni-network/omni/lib/log"
+
+	storetypes "cosmossdk.io/store/types"
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
+const UpgradeName = "3_drake"
+
+func StoreUpgrades(_ context.Context) *storetypes.StoreUpgrades {
+	return &storetypes.StoreUpgrades{}
+}
+
+func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator) upgradetypes.UpgradeHandler {
+	return func(ctx context.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		log.Info(ctx, "Running 3_drake upgrade handler")
+
+		return mm.RunMigrations(ctx, configurator, fromVM)
+	}
+}
+
+func GenesisState(codec.JSONCodec) (map[string]json.RawMessage, error) {
+	return map[string]json.RawMessage{}, nil
+}
