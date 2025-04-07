c13fb25cc1245591d841b1d957d88f8b190a96a8
corver <29249923+corverroos@users.noreply.github.com>
2025-04-07 16:37:34 +0200

    fix(ethclient/headerdb): prevent deadlock (#3615)
    Fixes deadlock in `headercachedb` since `cosmos-db.MemDB` deosn't
    support iterating and writing. It can deadlock when iteration is large.
    
    issue: none

--- lib/ethclient/headerdb/headerdb_internal_test.go
+++ lib/ethclient/headerdb/headerdb_internal_test.go
@@ -10,3 +10,8 @@ import (
 func (db *DB) Set(ctx context.Context, h *types.Header) error {
 	return db.set(ctx, h)
 }
+
+// DeleteFrom exports the deleteFrom method for testing.
+func (db *DB) DeleteFrom(ctx context.Context, height uint64) (int, error) {
+	return db.deleteFrom(ctx, height)
+}
