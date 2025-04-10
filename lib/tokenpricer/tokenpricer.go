5a4679b65b62d01ea59bb3886350788dd13cc859
corver <corver@omni.network>
2025-04-10 12:43:48 +0200

    chore(lib/coingecko): support arbitrary prices

--- lib/tokenpricer/tokenpricer.go
+++ lib/tokenpricer/tokenpricer.go
@@ -16,12 +16,12 @@ type Pricer interface {
 	// USDPrices returns the price of each provided token in USD.
 	USDPrices(ctx context.Context, tokens ...tokens.Asset) (map[tokens.Asset]float64, error)
 	// Price returns the price of the base asset denominated in the quote asset.
-	// Price(ctx context.Context, base, quote tokens.Asset) (float64, error)
+	Price(ctx context.Context, base, quote tokens.Asset) (float64, error)
 }
 
 type Cached struct {
 	p     Pricer
-	mu    sync.Mutex
+	mu    sync.RWMutex
 	cache map[pair]float64
 }
 
@@ -35,6 +35,42 @@ func NewCached(p Pricer) *Cached {
 		p:     p,
 		cache: make(map[pair]float64),
 	}
+}
+
+func (c *Cached) get(base, quote tokens.Asset) (float64, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	price, ok := c.cache[pair{Base: base, Quote: quote}]
+
+	return price, ok
+}
+
+func (c *Cached) set(base, quote tokens.Asset, price float64) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.cache[pair{Base: base, Quote: quote}] = price
+}
+
+func (c *Cached) Price(ctx context.Context, base, quote tokens.Asset) (float64, error) {
+	if base == quote {
+		return 1, nil
+	}
+
+	price, ok := c.get(base, quote)
+	if ok {
+		return price, nil
+	}
+
+	price, err := c.p.Price(ctx, base, quote)
+	if err != nil {
+		return 0, err
+	}
+
+	c.set(base, quote, price)
+
+	return price, nil
 }
 
 func (c *Cached) USDPrice(ctx context.Context, token tokens.Asset) (float64, error) {
@@ -52,19 +88,12 @@ func (c *Cached) USDPrice(ctx context.Context, token tokens.Asset) (float64, err
 }
 
 func (c *Cached) USDPrices(ctx context.Context, assets ...tokens.Asset) (map[tokens.Asset]float64, error) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	usdPair := func(asset tokens.Asset) pair {
-		return pair{Base: asset, Quote: tokens.USDC}
-	}
-
 	prices := make(map[tokens.Asset]float64)
 
 	var uncached []tokens.Asset
 
 	for _, asset := range assets {
-		if price, ok := c.cache[usdPair(asset)]; ok {
+		if price, ok := c.get(asset, tokens.USDC); ok {
 			prices[asset] = price
 		} else {
 			uncached = append(uncached, asset)
@@ -82,7 +111,7 @@ func (c *Cached) USDPrices(ctx context.Context, assets ...tokens.Asset) (map[tok
 
 	for token, price := range newPrices {
 		prices[token] = price
-		c.cache[usdPair(token)] = price
+		c.set(token, tokens.USDC, price)
 	}
 
 	return prices, nil
