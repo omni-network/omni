6053fa35ed113e5db139fac5ad574ec3d5768fc2
Paul Le Cam <paul@omni.network>
2025-04-10 15:56:02 +0100

    Add didFillOutbox() function

---
+++ sdk/packages/core/src/contracts/didFillOutbox.ts
@@ -0,0 +1,25 @@
+import type { Address, Chain, Client, Transport } from 'viem'
+import { readContract } from 'viem/actions'
+import { outboxABI } from '../constants/abis.js'
+import type { ParseOpenEventReturn } from './parseOpenEvent.js'
+
+export type DidFillParams<chain extends Chain> = {
+  client: Client<Transport, chain>
+  outboxAddress: Address
+  resolvedOrder: ParseOpenEventReturn
+}
+
+export type DidFillReturn = boolean
+
+export function didFillOutbox<chain extends Chain>({
+  client,
+  outboxAddress,
+  resolvedOrder,
+}: DidFillParams<chain>) {
+  return readContract(client, {
+    address: outboxAddress,
+    abi: outboxABI,
+    functionName: 'didFill',
+    args: [resolvedOrder.orderId, resolvedOrder.fillInstructions[0].originData],
+  })
+}
