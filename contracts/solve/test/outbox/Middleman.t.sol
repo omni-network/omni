1d8887f05568fa3664d11ec859f3a276cb676c55
Zodomo <zodomo@proton.me>
2025-04-15 12:17:08 -0500

    feat(contracts/solve): add executeAndTransfer721 (#3707)
    Added an `executeAndTransfer721` method to `SolverNetMiddleman` that
    performs the same task that `executeAndTransfer` does for ERC20s, but
    for ERC721s instead.
    
    issue: #3706

--- contracts/solve/test/outbox/Middleman.t.sol
+++ contracts/solve/test/outbox/Middleman.t.sol
@@ -5,17 +5,20 @@ import "../TestBase.sol";
 import { MockLST } from "test/utils/MockLST.sol";
 import { Refunder } from "test/utils/Refunder.sol";
 import { Reverter } from "test/utils/Reverter.sol";
+import { MockERC721 } from "test/utils/MockERC721.sol";
 
 contract SolverNet_Outbox_Middleman_Test is TestBase {
     MockLST lst;
     Refunder refunder;
     Reverter reverter;
+    MockERC721 milady;
 
     function setUp() public override {
         super.setUp();
         lst = new MockLST();
         refunder = new Refunder();
         reverter = new Reverter();
+        milady = new MockERC721("Milady Maker", "MILADY", "https://www.miladymaker.net/milady/json/");
     }
 
     function test_executeAndTransfer_reverts() public {
@@ -23,6 +26,13 @@ contract SolverNet_Outbox_Middleman_Test is TestBase {
         vm.prank(solver);
         vm.expectRevert(SolverNetMiddleman.CallFailed.selector);
         middleman.executeAndTransfer{ value: 1 ether }(address(0), user, address(reverter), "");
+    }
+
+    function test_executeAndTransfer721_reverts() public {
+        vm.deal(solver, 1 ether);
+        vm.prank(solver);
+        vm.expectRevert(SolverNetMiddleman.CallFailed.selector);
+        middleman.executeAndTransfer721{ value: 1 ether }(address(0), 0, user, address(reverter), "");
     }
 
     function test_executeAndTransfer_erc20_succeeds() public {
@@ -39,5 +49,13 @@ contract SolverNet_Outbox_Middleman_Test is TestBase {
         vm.prank(solver);
         middleman.executeAndTransfer{ value: 1 ether }(address(0), user, address(refunder), "");
         assertEq(user.balance, 1 ether, "user should have received 1 ether of native tokens from middleman");
+    }
+
+    function test_executeAndTransfer721_succeeds() public {
+        vm.prank(solver);
+        middleman.executeAndTransfer721(
+            address(milady), 1, user, address(milady), abi.encodeWithSelector(MockERC721.mint.selector)
+        );
+        assertEq(milady.ownerOf(1), user, "user should have received the Milady NFT from middleman");
     }
 }
---
+++ contracts/solve/test/utils/MockERC721.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.24;
+
+import { ERC721 } from "solady/src/tokens/ERC721.sol";
+import { LibString } from "solady/src/utils/LibString.sol";
+
+contract MockERC721 is ERC721 {
+    using LibString for string;
+    using LibString for uint256;
+
+    string private _NAME;
+    string private _SYMBOL;
+    string private _BASE_URI;
+
+    uint256 public totalSupply;
+
+    constructor(string memory _name, string memory _symbol, string memory _baseURI) {
+        _NAME = _name;
+        _SYMBOL = _symbol;
+        _BASE_URI = _baseURI;
+    }
+
+    function name() public view override returns (string memory) {
+        return _NAME;
+    }
+
+    function symbol() public view override returns (string memory) {
+        return _SYMBOL;
+    }
+
+    function baseURI() public view returns (string memory) {
+        return _BASE_URI;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return baseURI().concat(tokenId.toString());
+    }
+
+    function mint() external {
+        _mint(msg.sender, ++totalSupply);
+    }
+}
