b4d5a60509cbdde591beecadba819aa3688c871f
ga-reth <hi@gareth-v.com>
2025-04-22 15:06:25 +0100

    chore(sdk): encode bigint prior to stringify

--- sdk/packages/react/src/hooks/util.ts
+++ sdk/packages/react/src/hooks/util.ts
@@ -1,6 +1,33 @@
 import { toHex } from 'viem'
 
-export const toJSON = (value: unknown) =>
-  JSON.stringify(value, (_, value) =>
-    typeof value === 'bigint' ? toHex(value) : value,
-  )
+const convertBigIntsToHex = (value: unknown): unknown => {
+  if (typeof value === 'bigint') {
+    return toHex(value)
+  }
+
+  if (Array.isArray(value)) {
+    return value.map(convertBigIntsToHex)
+  }
+
+  if (typeof value === 'object' && value !== null) {
+    const newValue: Record<string, unknown> = {}
+    for (const key in value) {
+      if (Object.prototype.hasOwnProperty.call(value, key)) {
+        newValue[key] = convertBigIntsToHex(
+          (value as Record<string, unknown>)[key],
+        )
+      }
+    }
+    return newValue
+  }
+
+  return value
+}
+
+export const toJSON = (value: unknown) => {
+  const res = convertBigIntsToHex(value)
+  // stringify w/out the replacer callback since we've seen browser
+  // extensions bypass the replacer by attaching a toJSON method to BigInt
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify#:~:text=Attempting%20to%20serialize,by%20the%20user
+  return JSON.stringify(res)
+}
