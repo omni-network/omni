958b83217382976500eae528d5f0e527c7616530
ga-reth <hi@gareth-v.com>
2025-04-23 13:25:36 +0100

    chore(sdk): move to parse inbox status

--- sdk/packages/react-next/src/hooks/useInboxStatus.ts
+++ sdk/packages/react-next/src/hooks/useInboxStatus.ts
@@ -1,35 +1,15 @@
-import { type InboxStatus, getInboxStatus } from '@omni-network/core'
-import { type UseQueryResult, useQuery } from '@tanstack/react-query'
+import { type InboxStatus, parseInboxStatus } from '@omni-network/core'
 import type { Hex } from 'viem'
-import { useClient } from 'wagmi'
-import { invariant } from '../utils/invariant.js'
-import { useOmniContracts } from './useOmniContracts.js'
-
-export type UseInboxStatusParams = {
-  chainId: number
-  orderId?: Hex
-}
-
-export type UseInboxStatusReturn = UseQueryResult<InboxStatus>
+import { useGetOrder } from './useGetOrder.js'
 
 export function useInboxStatus({
   chainId,
   orderId,
-}: UseInboxStatusParams): UseInboxStatusReturn {
-  const { data: contracts } = useOmniContracts()
-  const client = useClient({ chainId })
-
-  return useQuery({
-    queryKey: ['inboxStatus', chainId, orderId],
-    queryFn: async () => {
-      invariant(!!client && !!contracts && !!orderId)
-      return await getInboxStatus({
-        client,
-        inboxAddress: contracts.inbox,
-        orderId,
-      })
-    },
-    enabled: !!client && !!contracts && !!orderId,
-    refetchInterval: 1000,
-  })
+}: {
+  chainId?: number
+  orderId?: Hex
+}): InboxStatus {
+  const order = useGetOrder({ chainId, orderId })
+  // TODO propagate error if getOrder fails / data not found
+  return parseInboxStatus({ order: order?.data })
 }
