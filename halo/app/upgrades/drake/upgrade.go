c06231610075b5fad09e30b96cddabbb4ae439eb
corver <corver@omni.network>
2025-04-28 09:36:28 +0200

    chore(halo/upgrade): improve drake comments

--- halo/app/upgrades/drake/upgrade.go
+++ halo/app/upgrades/drake/upgrade.go
@@ -2,9 +2,11 @@
 // Sir Francis Drake, an English explorer best known for making the second
 // circumnavigation of the world in a single expedition between 1577 and 1580.
 //
-// It includes:
-// - Unstaking of funds,
-// - Processing of rewards withdrawals.
+// It includes the following logic changes:
+// - User requested undelegations (via staking.MsgUndelegate in evmstaking module)
+// - User requested reward withdrawals (via distribution.MsgWithdraw in evmdistribution package)
+// - Automatic evm withdrawal creation (via halo/withdrawal bank module wrapper)
+// - Processing of evm withdrawals (via evmengine module)
 package drake
 
 import (
@@ -22,7 +24,7 @@ import (
 const UpgradeName = "3_drake"
 
 func StoreUpgrades(_ context.Context) *storetypes.StoreUpgrades {
-	return &storetypes.StoreUpgrades{}
+	return &storetypes.StoreUpgrades{} // No modules updated
 }
 
 func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator) upgradetypes.UpgradeHandler {
@@ -33,6 +35,7 @@ func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator) 
 	}
 }
 
+// GenesisState returns nothing, since this upgrade includes no state changes, only logic.
 func GenesisState(codec.JSONCodec) (map[string]json.RawMessage, error) {
 	return nil, nil //nolint:nilnil // map is for reading only
 }
