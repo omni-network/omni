f7b10ff8b129a3c354dda7be85c385e27958d32b
dkvector <dkvector@users.noreply.github.com>
2025-04-30 09:51:58 -0700

    cleanup: remove staking from V2

--- contracts/core/src/interfaces/IGenesisStakeV2.sol
+++ contracts/core/src/interfaces/IGenesisStakeV2.sol
@@ -3,19 +3,6 @@ pragma solidity 0.8.24;
 
 interface IGenesisStakeV2 {
     /**
-     * @notice Stake `amount` tokens.
-     * @param amount    The amount of tokens to stake.
-     */
-    function stake(uint256 amount) external;
-
-    /**
-     * @notice Stake `amount` tokens for `recipient`, paid by the caller.
-     * @param recipient The recipient to stake tokens for.
-     * @param amount    The amount of tokens to stake.
-     */
-    function stakeFor(address recipient, uint256 amount) external;
-
-    /**
      * @notice Migrate a user's stake to the rewards distributor.
      * @param addr The address of the user to migrate.
      * @return The amount of tokens migrated.
