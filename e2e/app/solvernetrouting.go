276429ff709867eee4488c5abc90186dce8d242c
Zodomo <zodomo@proton.me>
2025-05-02 16:16:16 -0500

    clean up

--- e2e/app/solvernetrouting.go
+++ e2e/app/solvernetrouting.go
@@ -11,9 +11,11 @@ import (
 	"github.com/omni-network/omni/lib/ethclient/ethbackend"
 	"github.com/omni-network/omni/lib/netconf"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 )
 
+// SetSolverNetRoutes sets the SolverNet routes for all chains in a given network.
 func SetSolverNetRoutes(ctx context.Context, network netconf.Network, backends ethbackend.Backends) error {
 	addrs, err := contracts.GetAddresses(ctx, network.ID)
 	if err != nil {
@@ -21,33 +23,10 @@ func SetSolverNetRoutes(ctx context.Context, network netconf.Network, backends e
 	}
 
 	chainIDs := network.EVMChains()
-	manager := eoa.MustAddress(network.ID, eoa.RoleManager)
-
 	for _, chain := range chainIDs {
-		var remoteChainIDs []uint64
-		var remoteOutboxes []common.Address
-		var remoteInboxConfigs []bindings.ISolverNetOutboxInboxConfig
-		for _, c := range chainIDs {
-			if c.ID != chain.ID {
-				var inbox common.Address
-				var outbox common.Address
-				provider := solvernet.None
-				if !solvernet.IsDisabled(chain.ID) && !solvernet.IsDisabled(c.ID) {
-					inbox = addrs.SolverNetInbox
-					outbox = addrs.SolverNetOutbox
-					provider, err = solvernet.Provider(c.ID)
-					if err != nil {
-						return errors.Wrap(err, "get provider", "chain", c.Name)
-					}
-				}
-
-				remoteChainIDs = append(remoteChainIDs, c.ID)
-				remoteOutboxes = append(remoteOutboxes, outbox)
-				remoteInboxConfigs = append(remoteInboxConfigs, bindings.ISolverNetOutboxInboxConfig{
-					Inbox:    inbox,
-					Provider: provider,
-				})
-			}
+		remoteChainIDs, remoteOutboxes, remoteInboxConfigs, err := getRoutes(chain, chainIDs, addrs.SolverNetInbox, addrs.SolverNetOutbox)
+		if err != nil {
+			return errors.Wrap(err, "get routes", "chain", chain.Name)
 		}
 
 		backend, err := backends.Backend(chain.ID)
@@ -55,49 +34,114 @@ func SetSolverNetRoutes(ctx context.Context, network netconf.Network, backends e
 			return errors.Wrap(err, "get backend", "chain", chain.Name)
 		}
 
-		isDeployed, err := contracts.IsDeployed(ctx, backend, addrs.SolverNetInbox)
+		isDeployed, err := isDeployed(ctx, backend, addrs.SolverNetInbox, addrs.SolverNetOutbox)
 		if !isDeployed || err != nil {
-			return errors.Wrap(err, "solvernetinbox not deployed", "chain", chain.Name)
-		}
-		isDeployed, err = contracts.IsDeployed(ctx, backend, addrs.SolverNetOutbox)
-		if !isDeployed || err != nil {
-			return errors.Wrap(err, "solvernetoutbox not deployed", "chain", chain.Name)
+			return errors.Wrap(err, "isDeployed", "chain", chain.Name)
 		}
 
-		txOpts, err := backend.BindOpts(ctx, manager)
+		txOpts, err := backend.BindOpts(ctx, eoa.MustAddress(network.ID, eoa.RoleManager))
 		if err != nil {
 			return errors.Wrap(err, "bind opts", "chain", chain.Name)
 		}
 
-		inbox, err := bindings.NewSolverNetInbox(addrs.SolverNetInbox, backend)
+		err = configureInbox(ctx, backend, txOpts, addrs.SolverNetInbox, remoteChainIDs, remoteOutboxes)
 		if err != nil {
-			return errors.Wrap(err, "bind inbox", "chain", chain.Name)
+			return errors.Wrap(err, "configure inbox", "chain", chain.Name)
 		}
 
-		tx, err := inbox.SetOutboxes(txOpts, remoteChainIDs, remoteOutboxes)
+		err = configureOutbox(ctx, backend, txOpts, addrs.SolverNetOutbox, remoteChainIDs, remoteInboxConfigs)
 		if err != nil {
-			return errors.Wrap(err, "set outboxes on inbox", "chain", chain.Name)
+			return errors.Wrap(err, "configure outbox", "chain", chain.Name)
 		}
+	}
 
-		_, err = backend.WaitMined(ctx, tx)
-		if err != nil {
-			return errors.Wrap(err, "wait mined set outboxes on inbox", "chain", chain.Name)
-		}
+	return nil
+}
 
-		outbox, err := bindings.NewSolverNetOutbox(addrs.SolverNetOutbox, backend)
-		if err != nil {
-			return errors.Wrap(err, "bind outbox", "chain", chain.Name)
-		}
+// getRoutes returns the remote chain IDs, outboxes, and inbox configs for a given chain.
+func getRoutes(chain netconf.Chain, allChains []netconf.Chain, inbox common.Address, outbox common.Address) ([]uint64, []common.Address, []bindings.ISolverNetOutboxInboxConfig, error) {
+	var remoteChainIDs []uint64
+	var remoteOutboxes []common.Address
+	var remoteInboxConfigs []bindings.ISolverNetOutboxInboxConfig
 
-		tx, err = outbox.SetInboxes(txOpts, remoteChainIDs, remoteInboxConfigs)
-		if err != nil {
-			return errors.Wrap(err, "set inboxes on outbox", "chain", chain.Name)
-		}
+	for _, c := range allChains {
+		if c.ID != chain.ID {
+			var solverNetInbox common.Address
+			var solverNetOutbox common.Address
+			provider := solvernet.None
+			var err error
 
-		_, err = backend.WaitMined(ctx, tx)
-		if err != nil {
-			return errors.Wrap(err, "wait mined set inboxes on outbox", "chain", chain.Name)
+			if !solvernet.IsDisabled(chain.ID) && !solvernet.IsDisabled(c.ID) {
+				solverNetInbox = inbox
+				solverNetOutbox = outbox
+				provider, err = solvernet.Provider(c.ID)
+				if err != nil {
+					return nil, nil, nil, errors.Wrap(err, "get provider", "chain", c.Name)
+				}
+			}
+
+			remoteChainIDs = append(remoteChainIDs, c.ID)
+			remoteOutboxes = append(remoteOutboxes, solverNetOutbox)
+			remoteInboxConfigs = append(remoteInboxConfigs, bindings.ISolverNetOutboxInboxConfig{
+				Inbox:    solverNetInbox,
+				Provider: provider,
+			})
 		}
+	}
+
+	return remoteChainIDs, remoteOutboxes, remoteInboxConfigs, nil
+}
+
+// isDeployed returns true if the SolverNet inbox and outbox are deployed on a given chain.
+func isDeployed(ctx context.Context, backend *ethbackend.Backend, inbox common.Address, outbox common.Address) (bool, error) {
+	isDeployed, err := contracts.IsDeployed(ctx, backend, inbox)
+	if !isDeployed || err != nil {
+		return false, errors.Wrap(err, "solvernetinbox not deployed", "chain", backend.Name())
+	}
+
+	isDeployed, err = contracts.IsDeployed(ctx, backend, outbox)
+	if !isDeployed || err != nil {
+		return false, errors.Wrap(err, "solvernetoutbox not deployed", "chain", backend.Name())
+	}
+
+	return true, nil
+}
+
+// configureInbox configures the routes on a SolverNetInbox contract for a given chain.
+func configureInbox(ctx context.Context, backend *ethbackend.Backend, txOpts *bind.TransactOpts, inbox common.Address, remoteChainIDs []uint64, remoteOutboxes []common.Address) error {
+	solverNetInbox, err := bindings.NewSolverNetInbox(inbox, backend)
+	if err != nil {
+		return errors.Wrap(err, "bind inbox", "chain", backend.Name())
+	}
+
+	tx, err := solverNetInbox.SetOutboxes(txOpts, remoteChainIDs, remoteOutboxes)
+	if err != nil {
+		return errors.Wrap(err, "set outboxes on inbox", "chain", backend.Name())
+	}
+
+	_, err = backend.WaitMined(ctx, tx)
+	if err != nil {
+		return errors.Wrap(err, "wait mined set outboxes on inbox", "chain", backend.Name())
+	}
+
+	return nil
+}
+
+// configureOutbox configures the routes on a SolverNetOutbox contract for a given chain.
+func configureOutbox(ctx context.Context, backend *ethbackend.Backend, txOpts *bind.TransactOpts, outbox common.Address, remoteChainIDs []uint64, remoteInboxConfigs []bindings.ISolverNetOutboxInboxConfig) error {
+	solverNetOutbox, err := bindings.NewSolverNetOutbox(outbox, backend)
+	if err != nil {
+		return errors.Wrap(err, "bind outbox", "chain", backend.Name())
+	}
+
+	tx, err := solverNetOutbox.SetInboxes(txOpts, remoteChainIDs, remoteInboxConfigs)
+	if err != nil {
+		return errors.Wrap(err, "set inboxes on outbox", "chain", backend.Name())
+	}
+
+	_, err = backend.WaitMined(ctx, tx)
+	if err != nil {
+		return errors.Wrap(err, "wait mined set inboxes on outbox", "chain", backend.Name())
 	}
 
 	return nil
