bf6a21151df6d1cd8ce33b1d4c251b7fe68fd976
Kevin Halliday <kevin@omni.network>
2025-05-07 19:54:40 -0400

    test(solver/rebalanace): add threshold reference test (#3943)
    Threshold reference test. 
    
    issue: none

---
+++ solver/rebalance/threshold_test.go
@@ -0,0 +1,39 @@
+package rebalance_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/omni-network/omni/lib/evmchain"
+	"github.com/omni-network/omni/lib/tutil"
+	"github.com/omni-network/omni/solver/rebalance"
+)
+
+//go:generate go test . -golden -clean
+
+func TestThresholdReference(t *testing.T) {
+	t.Parallel()
+
+	golden := make(map[string]map[string]string)
+
+	for _, token := range rebalance.Tokens() {
+		thresh := rebalance.GetFundThreshold(token)
+
+		key := fmt.Sprintf("%s:%s", evmchain.Name(token.ChainID), token.Symbol)
+
+		golden[key] = map[string]string{
+			"min":     token.FormatAmt(thresh.Min()),
+			"target":  token.FormatAmt(thresh.Target()),
+			"minSwap": token.FormatAmt(thresh.MinSwap()),
+			"maxSwap": token.FormatAmt(thresh.MaxSwap()),
+		}
+
+		if thresh.NeverSurplus() {
+			golden[key]["surplus"] = "inf"
+		} else {
+			golden[key]["surplus"] = token.FormatAmt(thresh.Surplus())
+		}
+	}
+
+	tutil.RequireGoldenJSON(t, golden, tutil.WithFilename("thresholds.json"))
+}
