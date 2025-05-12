6c269185a515148ef6b8c6573574d0a6d5552f5a
Zodomo <zodomo@proton.me>
2025-05-12 16:29:49 -0500

    add markFilled test for no portal address edge case

--- contracts/solve/test/inbox/MarkFilled.t.sol
+++ contracts/solve/test/inbox/MarkFilled.t.sol
@@ -49,6 +49,37 @@ contract SolverNet_Inbox_MarkFilled_Test is TestBase {
             address(inbox),
             abi.encodeWithSelector(ISolverNetInbox.markFilled.selector, orderId, bytes32(0), address(0))
         );
+
+        // portal address must be set for remote orders
+        address impl = address(new SolverNetInbox(address(0), address(mailboxes[uint32(srcChainId)])));
+        inbox = SolverNetInbox(address(new TransparentUpgradeableProxy(impl, proxyAdmin, bytes(""))));
+        inbox.initialize(address(this), solver);
+
+        uint64[] memory chainIds = new uint64[](2);
+        chainIds[0] = srcChainId;
+        chainIds[1] = destChainId;
+        address[] memory outboxes = new address[](2);
+        outboxes[0] = address(outbox);
+        outboxes[1] = address(outbox);
+        inbox.setOutboxes(chainIds, outboxes);
+
+        orderId = inbox.getOrderId(user, inbox.getUserNonce(user));
+        (orderData, order) = getNativeForNativeVaultOrder(defaultAmount, defaultAmount);
+        IERC7683.ResolvedCrossChainOrder memory resolvedOrder = inbox.resolve(order);
+        bytes32 fillhash = fillHash(orderId, resolvedOrder.fillInstructions[0].originData);
+        assertTrue(inbox.validate(order), "order should be valid");
+
+        fundUser(orderData);
+        vm.prank(user);
+        inbox.open{ value: defaultAmount }(order);
+
+        vm.expectRevert(ISolverNetInbox.WrongSourceChain.selector);
+        portal.mockXCall(
+            destChainId,
+            address(outbox),
+            address(inbox),
+            abi.encodeWithSelector(ISolverNetInbox.markFilled.selector, orderId, fillhash, solver)
+        );
     }
 
     function test_markFilled_succeeds() public {
