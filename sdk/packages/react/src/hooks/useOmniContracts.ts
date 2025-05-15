30c88b039e906f3b5d4133ca1b2c7147e59b0243
Paul Le Cam <paul@omni.network>
2025-05-15 13:43:00 +0100

    feat(sdk): add queryOpts support to useOmniContracts

--- sdk/packages/react/src/hooks/useOmniContracts.ts
+++ sdk/packages/react/src/hooks/useOmniContracts.ts
@@ -1,11 +1,27 @@
 import type { OmniContracts } from '@omni-network/core'
-import { type UseQueryResult, useQuery } from '@tanstack/react-query'
+import {
+  type UseQueryOptions,
+  type UseQueryResult,
+  useQuery,
+} from '@tanstack/react-query'
 import { useOmniContext } from '../context/omni.js'
 import { getOmniContractsQueryOptions } from '../utils/getContracts.js'
 
+export type UseOmniContractsParameters = {
+  queryOpts?: Omit<
+    UseQueryOptions<OmniContracts>,
+    'queryKey' | 'queryFn' | 'enabled'
+  >
+}
+
 export type UseOmniContractsResult = UseQueryResult<OmniContracts>
 
-export function useOmniContracts(): UseOmniContractsResult {
+export function useOmniContracts({
+  queryOpts,
+}: UseOmniContractsParameters = {}): UseOmniContractsResult {
   const config = useOmniContext()
-  return useQuery(getOmniContractsQueryOptions(config))
+  return useQuery({
+    ...getOmniContractsQueryOptions(config),
+    ...queryOpts,
+  })
 }
