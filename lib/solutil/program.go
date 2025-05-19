ca3ead6c8ed9074087a8c365db57f17b7381ffbe
corver <29249923+corverroos@users.noreply.github.com>
2025-05-19 11:53:13 +0200

    chore(lib/soltuil): move solana utils (#4075)
    Move solana utils to `lib/solutil`, enable tests in CI.
    
    issue: none

---
+++ lib/solutil/program.go
@@ -0,0 +1,31 @@
+package solutil
+
+import "github.com/gagliardetto/solana-go"
+
+// Program represents a Solana executable program (smart contract).
+type Program struct {
+	Name         string
+	SharedObject []byte // Compiled BPF shared object
+	KeyPairJSON  []byte
+}
+
+func (p Program) SOFile() string {
+	return p.Name + ".so"
+}
+
+func (p Program) KeyPairFile() string {
+	return p.Name + "-keypair.json"
+}
+
+func (p Program) MustPrivateKey() solana.PrivateKey {
+	pk, err := solana.PrivateKeyFromSolanaKeygenFileBytes(p.KeyPairJSON)
+	if err != nil {
+		panic(err)
+	}
+
+	return pk
+}
+
+func (p Program) MustPublicKey() solana.PublicKey {
+	return p.MustPrivateKey().PublicKey()
+}
