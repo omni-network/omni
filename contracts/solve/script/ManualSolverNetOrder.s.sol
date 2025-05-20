375a74c7f016b6f404d1de4dfc6529338ff27d82
Zodomo <zodomo@proton.me>
2025-05-20 15:31:07 -0500

    new order

--- contracts/solve/script/ManualSolverNetOrder.s.sol
+++ contracts/solve/script/ManualSolverNetOrder.s.sol
@@ -5,7 +5,7 @@ import { Script } from "forge-std/Script.sol";
 // import { console2 } from "forge-std/console2.sol";
 import { SolverNet } from "src/lib/SolverNet.sol";
 import { SafeTransferLib } from "solady/src/utils/SafeTransferLib.sol";
-// import { FixedPointMathLib } from "solady/src/utils/FixedPointMathLib.sol";
+import { FixedPointMathLib } from "solady/src/utils/FixedPointMathLib.sol";
 import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import { IERC7683 } from "src/erc7683/IERC7683.sol";
 import { ISolverNetInbox } from "src/interfaces/ISolverNetInbox.sol";
@@ -16,28 +16,39 @@ contract ManualSolverNetOrderScript is Script {
     ISolverNetInbox internal mainnetInbox = ISolverNetInbox(0x8FCFcd0B4Fa2cc2965a3c7F27995B0A43F210dB8);
     ISolverNetInbox internal omegaInbox = ISolverNetInbox(0x7EA0CeB70D5Df75a730E6cB3EADeC12EfdFe80a1);
 
-    ISolverNetInbox internal inbox;
+    address internal mainnetExecutor = 0xf92Dd37ae11F2CCb4de9355BEcEd42Deb4158815;
+    address internal omegaExecutor = 0x2b6bf280897ccCBEf827E8546CbD4d28367a8196;
 
-    uint256 internal callAmount = 0.0077 ether;
+    ISolverNetInbox internal inbox;
+    address internal executor;
+
+    uint256 internal callAmount = 0;
     uint256 internal msgValue = 0;
-    address internal depositToken = 0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85;
-    uint96 internal depositAmount = 14_000_000;
-    address internal target = 0xC5759Dd58057808568C698fFDc4Ad5548D17e75a;
+    address internal depositToken = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address internal expenseToken = 0x09Bc4E0D864854c6aFB6eB9A9cdF58aC190D0dF9;
+    uint96 internal expenseAmount = 5_000_000;
+    uint96 internal depositAmount = uint96(FixedPointMathLib.fullMulDivUp(expenseAmount, 10_030, 10_000));
+    address internal target = 0x09Bc4E0D864854c6aFB6eB9A9cdF58aC190D0dF9;
     address internal owner = 0xA779fC675Db318dab004Ab8D538CB320D0013F42;
-    uint64 internal destChainId = 1;
+    uint64 internal destChainId = 5000;
 
     bytes32 internal constant ORDERDATA_TYPEHASH = keccak256(
         "OrderData(address owner,uint64 destChainId,Deposit deposit,Call[] calls,TokenExpense[] expenses)Deposit(address token,uint96 amount)Call(address target,bytes4 selector,uint256 value,bytes params)TokenExpense(address spender,address token,uint96 amount)"
     );
 
     function setUp() public {
-        if (block.chainid == 1 || block.chainid == 10 || block.chainid == 8453 || block.chainid == 11_155_420) {
+        if (
+            block.chainid == 1 || block.chainid == 10 || block.chainid == 5000 || block.chainid == 8453
+                || block.chainid == 11_155_420
+        ) {
             inbox = mainnetInbox;
+            executor = mainnetExecutor;
         } else if (
             block.chainid == 17_000 || block.chainid == 84_532 || block.chainid == 421_614
                 || block.chainid == 11_155_111 || block.chainid == 11_155_420
         ) {
             inbox = omegaInbox;
+            executor = omegaExecutor;
         }
     }
 
@@ -60,27 +71,25 @@ contract ManualSolverNetOrderScript is Script {
     }
 
     function _getSolverNetOrder() internal view returns (IERC7683.OnchainCrossChainOrder memory) {
-        IScatterNFT.Auth memory auth = IScatterNFT.Auth({ key: bytes32(0), proof: new bytes32[](0) });
-        uint256 quantity = 1;
-        address affiliate = address(0);
-        bytes memory signature = new bytes(0);
-
         SolverNet.Deposit memory deposit = SolverNet.Deposit({ token: depositToken, amount: depositAmount });
 
         SolverNet.Call[] memory call = new SolverNet.Call[](1);
         call[0] = SolverNet.Call({
             target: target,
-            selector: IScatterNFT.mintTo.selector,
-            value: callAmount,
-            params: abi.encode(auth, quantity, owner, affiliate, signature)
+            selector: IERC20.transfer.selector,
+            value: 0,
+            params: abi.encode(owner, expenseAmount)
         });
+
+        SolverNet.TokenExpense[] memory expenses = new SolverNet.TokenExpense[](1);
+        expenses[0] = SolverNet.TokenExpense({ spender: address(0), token: expenseToken, amount: expenseAmount });
 
         SolverNet.OrderData memory orderData = SolverNet.OrderData({
             owner: owner,
             destChainId: destChainId,
             deposit: deposit,
             calls: call,
-            expenses: new SolverNet.TokenExpense[](0)
+            expenses: expenses
         });
 
         return IERC7683.OnchainCrossChainOrder({
